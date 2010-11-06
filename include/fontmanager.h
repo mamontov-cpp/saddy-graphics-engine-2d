f5e56de3ad17363ae5773f25a13f63bae7db199e
Kolesov <Kolesov@localhost>
2010-11-06 23:47:00 +0300

    ??????????? ?????? ???? ? ????????????.

--- include/fontmanager.h
+++ include/fontmanager.h
@@ -12,61 +12,74 @@
 #include "gl\gl.h"
 #include "gl\glu.h"
 
+
 /*!	\class This class work with one font: load, render and free it.
 	Also class get and keep settings for render, size, colour, place...
 */
-class FontManager
+class Font
 {
 public:
 	/*! Destructor free loaded font
 	*/
-	~FontManager();
+	~Font();
 	/*!	Default constructor, no load anything
 	*/
-	FontManager();
+	Font();
 	/*! Copying constructor, copy all properties of getting object
 		\param[in] other object for copying, this object will be copy of other
 	*/
-	FontManager(FontManager &other);
-	/*! Constructor load font and size, no load settings.
+	Font(Font &other);
+	/*! Constructor load font and size
 		\param[in] file_name name (and path, if need) of file with true type font
-		\param[in] size size of font
 	*/
-	FontManager(hString &file_name, int size);
-	/*!	Constructor load font and settings.
-		\param[in] file_name name (and path, if need) of file with true type font
-		\param[in] size size of font
-		\param[in] colour color of font
-		\param[in] place place on surface, for putting text
-	*/
-	FontManager(hString &filename, int size, hColor color, hRectF place);
-	/*!	function set setting for font, independent of existing previous settings
-		\param[in] colour colour of font
-		\param[in] place place on surface, for putting text
-	*/
-	void setSettings(int colour, hRectF place);
-	/*!	function free current font and load new font, with settings
+	Font(hString &file_name);
+	/*!	function free current font and load new font
 	\param[in] file_name name (and path, if need) of file with true type font
-		\param[in] size size of font
-		\param[in] colour color of font
-		\param[in] place place on surface, for putting text
 	*/
-	void reloadFont(hString &filename, int size, hColor color, hRectF place);
+	void reloadFont(hString &filename);
 	/*!	function return last error in human redable form
 		\return last error as string
 	*/
 	hString getLastError();
-	/*!	function put text on surface of screen
-		\param screen surface of screen
-		\param text text for putting
+	/*!	function create texture with text and return it
+		\param[in] text text to drawing on texture
+		\param[in] size size of font
+		\param[in] color color of text
+		\param[out] lenght lenght of texture
+		\param[out] height height of texture
+		\return texture with drawing text as GLuint*
 	*/
-	void putText(GLuint *screen, hString text);
+	GLuint *drawText(hString &text, int size, hColor color, int *lenght, int *height);
 
 protected:
 	hString m_last_error;///<last error in human readable form
-	hColor color;///<saved color for text
-	hRectF place;///<saved place for putting text
-	HFONT font;///<loaded font
+	//<type> m_font;///<loaded font
+};
+
+/*!	\class manage fonts classes: keep and return pointers to font classes and free it
+*/
+class FontManager
+{
+public:
+	/*!	function create font object and return pointer
+		\param[in] file_name name of file with font, it will be key for this font object
+		\return pointer to font object
+	*/
+	Font *createFont(hString &file_name);
+	/*!	function return font object with key
+		\param[in] key key for object for returning
+		\return pointer to font object
+	*/
+	Font *getFont(hString &key);
+	/*!	function delete font object
+		\param[in] key key of deleting object
+	*/
+	void removeFont(hString &key);
+	/*! destructor delete all fonts object keeping in this manager
+	*/
+	~FontManager();
+protected:
+	hhash<hString, Font*> m_fonts;
 };
 
 #endif //FONTMANAGER_H__
