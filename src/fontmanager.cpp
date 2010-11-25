e26098f446ff7e63e796fb5bd66a244abbf041b3
hsx <hsx@mail.ru>
2010-11-25 17:38:16 +0300

    Splitted definition of sad::Font. Abandoned it.  Revisited a sad::FontManager class.  Started developing sad::TMFont

--- src/fontmanager.cpp
+++ src/fontmanager.cpp
@@ -1,121 +1,51 @@
 
 #include "fontmanager.h"
 
-/*	function create font object and return pointer
-	\param[in] name name of file with font, it will be key for this font object
-	\return pointer to font object
-*/
-pFont FontManager::loadFont(hString &name, int size) {
-	pFont temp;
-	if (!m_fonts.contains(name)) {
-		temp =  new Font(name, size);
-		m_fonts.insert(name, temp);
-	} else {
-		temp = NULL;
-	}
-	return temp;
+using namespace sad;
+
+FontManager FontManager::m_instance;
+
+BasicFont::~BasicFont()
+{
 
 }
-/*	function return font object with key
-	\param[in] key key for object for returning
-	\return pointer to font object
-*/
-pFont FontManager::getFont(hString &key) {
-	pFont result = NULL;
-	if (m_fonts.contains(key)) {
-		result = m_fonts[key];
-	}
-	return result;
+FontManager::FontManager()
+{
+
 }
-/*	function delete font object
-	\param[in] key key of deleting object
-*/
-void FontManager::removeFont(hString &key) {
-	if (m_fonts.contains(key)) {
-		m_fonts.remove(key);
-	}
+
+void FontManager::add(sad::BasicFont * font,const hst::string & name )
+{
+	if (m_instance.m_fonts.contains(name))
+		delete m_instance.m_fonts[name];
+	m_instance.m_fonts.insert(name,font);
+}
+
+sad::BasicFont* FontManager::get(const hst::string &key) 
+{	
+	if (m_instance.m_fonts.contains(key)) 
+		return m_instance.m_fonts[key]; 
+	else 
+		return NULL;
+}
+void FontManager::remove(const hst::string &key) 
+{
+	if (m_instance.m_fonts.contains(key))  
+	{ 
+		delete m_instance.m_fonts[key];
+		m_instance.m_fonts.remove(key);
+	}	
 }
 /* destructor delete all fonts object keeping in this manager
 */
-FontManager::~FontManager() {
-	pFont temp;
-	hhash<hString, Font*>::iterator font_iterator;
+FontManager::~FontManager() 
+{
+	hhash<hString, sad::BasicFont*>::iterator font_iterator;
 	font_iterator = m_fonts.begin();
-	while (font_iterator != m_fonts.end()) {
-		temp = font_iterator.value();
-		delete temp;
+	while (font_iterator != m_fonts.end()) 
+	{
+		delete font_iterator.value();
 		++font_iterator;
 	}
 }
 
-//Чтобы менеджер не ругался на анрезолвед экстерналы следующие функции описаны пустотельно, временно.
-
-/*! Destructor free loaded font
-*/
-Font::~Font() {
-	glDeleteLists(m_font, 96);//удалить все 96 списков отображения, т.е. загруженных символов
-}
-/*!	Default constructor, no load anything
-*/
-Font::Font() {
-	m_font_loaded = false;
-}
-/*! Copying constructor, copy all properties of getting object
-	\param[in] other object for copying, this object will be copy of other
-*/
-Font::Font(Font &other) {
-	m_font_loaded = other.m_font_loaded;
-	m_font = other.m_font;
-#ifdef WIN32
-	m_hdc = other.m_hdc;
-#endif
-}
-/*! Constructor load font and size
-	\param[in] name name (and path, if need) of file with true type font
-*/
-Font::Font(hString &name, int size) {
-	loadFont(name, size);
-}
-#ifdef WIN32
-/*!	Default constructor, no load anything
-	\param hdc HDC
-*/
-Font::Font(HDC hdc) {
-	m_hdc = hdc;
-}
-/*! Constructor load font and size
-	\param[in] file_name name (and path, if need) of file with true type font
-	\param hdc HDC
-*/
-Font::Font(hString &name, int size, HDC hdc) {
-	m_hdc = hdc;
-	loadFont(name, size);
-}
-#endif
-/*! function load font from file with name
-	\param[in] name name of file with font
-	\return font pointer
-*/
-void Font::loadFont(hString &name, int size) {
-#ifdef WIN32
-	tfont temp_font = CreateFont(
-		-size, //Размер шрифта, знак минус означает: считать размер символа а не ячейки
-		0, //ширина шрифта
-		0, //угол отношения
-		0, //угол наклона
-		FW_NORMAL, //жирность шрифта
-		false, //курсив
-		false, //подчеркнуты
-		false, //зачеркнутый
-		ANSI_CHARSET, //идентификатор набора символов
-		OUT_TT_PRECIS, //предпочитать ttf шрифты, если есть несколько с одним именем
-		CLIP_DEFAULT_PRECIS, //точность отсечения
-		ANTIALIASED_QUALITY, //кач-во вывода
-		FF_DONTCARE|DEFAULT_PITCH, //семейство и шаг
-		name.data() //имя шрифта
-		);
-	m_font = glGenLists(96);
-	SelectObject(m_hdc, temp_font);//выбор этого фонта
-	wglUseFontBitmaps(m_hdc, 32, 96, m_font);//Построить списки отображения для 96 символов начиная с пробела
-#endif
-}
