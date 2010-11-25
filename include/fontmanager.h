a3e886f47c9a93731045d250d55b2bd5ffb5f748
hsx <hsx@mail.ru>
2010-11-25 17:38:16 +0300

    Splitted definition of sad::Font. Abandoned it.  Revisited a sad::FontManager class.  Started developing sad::TMFont

--- include/fontmanager.h
+++ include/fontmanager.h
@@ -1,5 +1,5 @@
 /*!	\file fontmanager.h
-	\author Kolesov Dmitiy
+	\author Kolesov Dmitiy, HiddenSeeker
 	\brief Declaration of FontManager class
 
 	This file contain declaration of FontManager class, which work with font
@@ -8,106 +8,84 @@
 
 #ifndef FONTMANAGER_H__
 #define FONTMANAGER_H__
-#include <windows.h>
-#include <WinDef.h>
-#include "types.h"
-#include "gl\gl.h"
-#include "gl\glu.h"
 
 #ifdef WIN32
+
+#include <windows.h>
+#include <WinDef.h>
+
+#endif
+#include "types.h"
+#include "texture.h"
+
+#ifdef WIN32
+
 typedef HFONT tfont;
 #endif
 
-/*!	\class This class work with one font: load, render and free it.
-	Also class get and keep settings for render, size, colour, place...
-*/
-class Font
+namespace sad
 {
-public:
-	/*! Destructor free loaded font
-	*/
-	~Font();
-	/*!	Default constructor, no load anything
-	*/
-	Font();
-	/*! Copying constructor, copy all properties of getting object
-		\param[in] other object for copying, this object will be copy of other
-	*/
-	Font(Font &other);
-	/*! Constructor load font and size
-		\param[in] name name (and path, if need) of file with true type font
-	*/
-	Font(hString &name, int size);
-	/*!	function free current font and load new font
-	\param[in] name name (and path, if need) of file with true type font
-	*/
-#ifdef WIN32
-	/*!	Default constructor, no load anything
-		\param hdc HDC
-	*/
-	Font(HDC hdc);
-	/*! Constructor load font and size
-		\param[in] name name (and path, if need) of file with true type font
-		\param hdc HDC
-	*/
-	Font(hString &name, int size, HDC hdc);
-#endif
-	void reloadFont(hString &filename);
-	/*!	function return last error in human redable form
-		\return last error as string
-	*/
-	hString getLastError();
-	/*!	function create texture with text and return it
-		\param[in] text text to drawing on texture
-		\param[in] size size of font
-		\param[in] color color of text
-		\param[out] lenght lenght of texture
-		\param[out] height height of texture
-		\return texture with drawing text as GLuint*
-	*/
-	GLuint *drawText(hString &text, int size, hColor color, int *lenght, int *height);
 
-protected:
-	/*! function load font from file with name
-		\param[in] name name of file with font
-		\return font pointer
-	*/
-	void loadFont(hString &name, int size);
-	hString m_last_error;///<last error in human readable form
-	GLuint m_font;///<loaded font
-	bool m_font_loaded;
-#ifdef WIN32
-	HDC m_hdc;
-#endif
+/*! Basic font class
+*/
+class BasicFont
+{
+ private:
+ public:
+	 /*! Renders a string
+	     \param[in] str string
+		 \param[in] rect bounding rectangle. If cannot render in it, renders starting from upper left rendering
+	 */
+	 virtual void render(const hst::string & str,const hRectF & rect)=0; 
+	 /*! Destroys an exemplar
+	 */
+	 virtual ~BasicFont();
 };
 
-typedef Font* pFont;
 
-/*!	\class manage fonts classes: keep and return pointers to font classes and free it
-*/
-class FontManager
+}
+
+#ifdef WIN32
+	#include "win32font.h"
+#endif
+
+
+namespace sad
 {
-public:
-	/*!	function create font object and return pointer
-		\param[in] name name of file with font, it will be key for this font object
-		\return pointer to font object, if font already exist return NULL
+	/*!	\class FontManager
+		Keeps a collection of fonts, and provides accessing interface
 	*/
-	pFont loadFont(hString &name, int size);
-	/*!	function return font object with key
-		\param[in] key key for object for returning
-		\return pointer to font object
-	*/
-	pFont getFont(hString &key);
-	/*!	function delete font object
-		\param[in] key key of deleting object
-	*/
-	void removeFont(hString &key);
-	/*! destructor delete all fonts object keeping in this manager
-	*/
-	~FontManager();
-protected:
-	hhash<hString, pFont> m_fonts;
-};
+	class FontManager
+	{
+	 private:
+			 hst::hash<hst::string, sad::BasicFont *> m_fonts;
+			 
+			 static FontManager m_instance;
+
+			 FontManager();
+			 FontManager(const FontManager & o);
+			 FontManager & operator=(const FontManager & o);
+	 public:
+		    /*! Adds a font and associates a key to it
+			    \param[in] font font pointer
+				\param[in] name associated name
+			*/
+		    void add(sad::BasicFont * font,const hst::string & name );
+			/*!	Returns an object by a key
+				\param[in] key associated key
+				\return pointer to font object
+			*/
+		    static sad::BasicFont * get(const hst::string & key);
+		    /*!	Deletes a font by a key
+			    \param[in] key 
+		    */
+			static void remove(const hst::string &key);
+	     	/*! Destructor
+		    */
+			~FontManager();
+	};
+
+}
 
 #endif //FONTMANAGER_H__
 	
---
+++ src/win32font.cpp
@@ -0,0 +1,78 @@
+#include "fontmanager.h"
+
+
+#ifndef WIN32
+#error   "Why you are compiling it, dumbass?"
+#endif
+
+//Чтобы менеджер не ругался на анрезолвед экстерналы следующие функции описаны пустотельно, временно.
+
+/*! Destructor free loaded Win32Font
+*/
+Win32Font::~Win32Font() {
+	glDeleteLists(m_Win32Font, 96);//удалить все 96 списков отображения, т.е. загруженных символов
+}
+/*!	Default constructor, no load anything
+*/
+Win32Font::Win32Font() {
+	m_Win32Font_loaded = false;
+}
+/*! Copying constructor, copy all properties of getting object
+	\param[in] other object for copying, this object will be copy of other
+*/
+Win32Font::Win32Font(Win32Font &other) {
+	m_Win32Font_loaded = other.m_Win32Font_loaded;
+	m_Win32Font = other.m_Win32Font;
+#ifdef WIN32
+	m_hdc = other.m_hdc;
+#endif
+}
+/*! Constructor load Win32Font and size
+	\param[in] name name (and path, if need) of file with true type Win32Font
+*/
+Win32Font::Win32Font(hString &name, int size) {
+	loadWin32Font(name, size);
+}
+#ifdef WIN32
+/*!	Default constructor, no load anything
+	\param hdc HDC
+*/
+Win32Font::Win32Font(HDC hdc) {
+	m_hdc = hdc;
+}
+/*! Constructor load Win32Font and size
+	\param[in] file_name name (and path, if need) of file with true type Win32Font
+	\param hdc HDC
+*/
+Win32Font::Win32Font(hString &name, int size, HDC hdc) {
+	m_hdc = hdc;
+	loadWin32Font(name, size);
+}
+#endif
+/*! function load Win32Font from file with name
+	\param[in] name name of file with Win32Font
+	\return Win32Font pointer
+*/
+void Win32Font::loadWin32Font(hString &name, int size) {
+#ifdef WIN32
+	tfont temp_Win32Font = CreateFont(
+		-size, //Размер шрифта, знак минус означает: считать размер символа а не ячейки
+		0, //ширина шрифта
+		0, //угол отношения
+		0, //угол наклона
+		FW_NORMAL, //жирность шрифта
+		false, //курсив
+		false, //подчеркнуты
+		false, //зачеркнутый
+		ANSI_CHARSET, //идентификатор набора символов
+		OUT_TT_PRECIS, //предпочитать ttf шрифты, если есть несколько с одним именем
+		CLIP_DEFAULT_PRECIS, //точность отсечения
+		ANTIALIASED_QUALITY, //кач-во вывода
+		FF_DONTCARE|DEFAULT_PITCH, //семейство и шаг
+		name.data() //имя шрифта
+		);
+	m_Win32Font = glGenLists(96);
+	SelectObject(m_hdc, temp_Win32Font);//выбор этого фонта
+	wglUseFontBitmaps(m_hdc, 32, 96, m_Win32Font);//Построить списки отображения для 96 символов начиная с пробела
+#endif
+}
