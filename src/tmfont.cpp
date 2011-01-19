8203c108a4ae96873103f34c1c963181533cb1c7
hiddenseeker <hiddenseeker@neutro-ubuntu>
2011-01-19 09:05:40 +0300

    Added support for X11

--- src/tmfont.cpp
+++ src/tmfont.cpp
@@ -26,10 +26,12 @@ bool TMFont::load(
 		return false;
 	}
 	m_tex->setAlpha(255,bk,80);
+        printf("Started to build mipmaps\n");
 	m_tex->buildMipMaps();
 	//Loading mapped file
 	FILE * fl=fopen(cfg.data(),"rt");
 
+	printf("Started to load CFG file\n");
 	if (fl)
 	{
 		int count=0;
