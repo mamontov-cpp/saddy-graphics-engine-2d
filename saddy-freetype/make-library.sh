199973bd0351ce4c454db90338b7df06697f4c6f
hsx <hsx@mail.ru>
2011-03-18 10:11:56 +0300

    Changed library locations

--- saddy-freetype/make-library.sh
+++ saddy-freetype/make-library.sh
@@ -1,2 +1,3 @@
+../mkbuild.sh
 g++ ftfont.cpp -c -o ftfont.o  -L".."  -I"/usr/include/freetype2"  -DLINUX 
-ar cq ../libsaddy-freetype.a ftfont.o ../build/fontmanager.o
+ar cq ../lib/libsaddy-freetype.a ftfont.o ../build/fontmanager.o
