62705d71b73efa291d201e39f2e8270609f06080
hsx <hsx@mail.ru>
2011-09-21 12:28:25 +0400

    Committed background to library. Changed it to be less specific

--- include/extra/background.h
+++ include/extra/background.h
@@ -11,8 +11,14 @@ class Background: public Sprite
  SAD_NODE
  public:
 	    /*! Creates background from texture with name
+			\param[in] name name of texture
+			\param[in] r rectangle
 	    */
-	    Background(const hst::string & name);	
+	    Background(const hst::string & name,const hRectF & r);	
+		/*! Creates background from texture with name
+			\param[in] name name of texture
+	    */
+		Background(const hst::string & name);
 		/*! Destructor
 		*/
 		~Background();
--- src/extra/background.cpp
+++ src/extra/background.cpp
@@ -1,27 +1,27 @@
 #include "background.h"
-
-#ifndef LINUX
-
-#include <windows.h>
-#include <gl/gl.h>														
-#include <gl/glu.h>	
-#else
-
-#include <GL/gl.h>														
-#include <GL/glu.h>	
-
-#endif
+#include "../renderer.h"
 
 SAD_DECLARE(Background,Sprite)
 
-Background::Background(const hst::string &name): 
+#define SCREEN_HEIGHT  (sad::Renderer::instance().settings().height())
+#define SCREEN_WIDTH   (sad::Renderer::instance().settings().width())
+
+Background::Background(const hst::string &name,const hRectF & r): 
 Sprite(
 	   sad::TextureManager::instance()->get(name),
-	   hst::rect< ::s3d::point>(::s3d::point(0,480,0),::s3d::point(640,0,0)),
-	   hRectF(hPointF(0.0f,0.0f),hPointF(512.0f,512.0f))
-	  )
+	   hst::rect< ::s3d::point>(::s3d::point(0,SCREEN_HEIGHT,0),::s3d::point(SCREEN_WIDTH,0,0)),
+	   r)
 {
 }
+#define TEX_PTR (sad::TextureManager::instance()->get(name))
+Background::Background(const hst::string &name): 
+Sprite(
+	   TEX_PTR,
+	   hst::rect< ::s3d::point>(::s3d::point(0,SCREEN_HEIGHT,0),::s3d::point(SCREEN_WIDTH,0,0)),
+	   hRectF(hPointF(0,0),hPointF(TEX_PTR->width(),TEX_PTR->height())))
+{
+}
+
 
 Background::~Background()
 {
