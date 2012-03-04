3477fc4bfec0154245ef13449763a4346904895b
HiddenSeeker <HiddenSeeker@localhost>
2012-03-04 20:00:40 +0400

    Added implementation file for config, implemented constructor and destructor of observer.

---
+++ src/config/sprite2dconfig.cpp
@@ -0,0 +1,4 @@
+#include "sprite2dobserver.h"
+#include "sprite2dconfig.h"
+#include <assert.h>
+
--- src/config/sprite2dobserver.cpp
+++ src/config/sprite2dobserver.cpp
@@ -1,5 +1,31 @@
 #include "sprite2dobserver.h"
 #include "sprite2dconfig.h"
+#include <assert.h>
 
+Sprite2DConfigObserver::Sprite2DConfigObserver(const hst::string & spritegroup, int index, 
+											   Sprite2DConfig * config,
+										       Sprite2DConfigObserverPolicy policy
+											  )
+{
+	assert( config!=NULL ); 
+	
+	m_spritegroup=spritegroup;
+	m_index=index;
+	m_config=config;
+	m_policy=policy;
 
+	//By default sprite is not created
+	m_sprite=NULL;
 
+	//Register observer
+	config->addObserver(this);
+}
+
+Sprite2DConfigObserver::~Sprite2DConfigObserver()
+{
+	if (m_sprite)
+	{
+		delete m_sprite;
+	}
+	m_config->removeObserver(this);
+}
