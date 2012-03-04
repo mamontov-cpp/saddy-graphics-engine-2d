736ffd476c67a6c2516cd580a4e2eeb82baf7c14
HiddenSeeker <HiddenSeeker@localhost>
2012-03-04 22:17:47 +0400

    Implemented container for observers

--- src/config/sprite2dconfig.cpp
+++ src/config/sprite2dconfig.cpp
@@ -8,3 +8,25 @@ sad::TextureContainer * Sprite2DConfig::getTextures()
 	return sad::TextureManager::instance()->getContainer(m_texturecontainername);
 }
 
+bool Sprite2DObserverContainer::fireSpriteConsistencyCheck(const Sprite2DTemplateContainer & container)
+{
+	bool ok=true;
+	for (hst::hash<Sprite2DConfigObserver *,Sprite2DConfigObserver *>::iterator it=m_observers.begin();
+		 it!=m_observers.end();
+		 it++)
+	{
+		ok=ok && it.value()->canBeUpdatedFrom(container);
+	}
+	return ok;
+}
+
+void Sprite2DObserverContainer::fireReloadSprite()
+{
+ for (hst::hash<Sprite2DConfigObserver *,Sprite2DConfigObserver *>::iterator it=m_observers.begin();
+	  it!=m_observers.end();
+	  it++)
+ {
+	 it.value()->notifyConfigChanged();
+ }
+}
+
