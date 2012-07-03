bd302f93753ae6ea154e40cea18b0351fe4e344e
HiddenSeeker <HiddenSeeker@localhost>
2012-07-03 23:10:43 +0400

    Added ImageWriter.

--- plugins/atlasgen/imageglue.rb
+++ plugins/atlasgen/imageglue.rb
@@ -1,10 +1,14 @@
 # A class, which merges a textures in specified order, changing rect and filling output texture
+require 'cairo'
+
+# Merges a textures into one
 class ImageGlue
     # Merges all images into one
     # * param images Array of Texture
     # * param GlueOrder array of GlueOrder
     # * param output output data
+    # * param Config  config, which will be updated for writing
     # * return Array (surface, context)
-    def glue(images, order, output)
+    def glue(images, order, output, config)
     end
 end
---
+++ plugins/atlasgen/imagewriter.rb
@@ -0,0 +1,12 @@
+# Writes an image into file
+require 'cairo'
+
+# Writes an image into file
+class ImageWriter
+    # Writes an image into file
+    # * param texture Array (surface, context)
+    # * param filename String name of output file
+    # * return Boolena name of file
+    def write(texture, filename)
+    end
+end
