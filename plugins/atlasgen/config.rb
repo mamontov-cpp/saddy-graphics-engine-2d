02e03299e2be9ebf292a06c11575285bffe60793
HiddenSeeker <HiddenSeeker@localhost>
2012-07-03 20:42:19 +0400

    Added basic implementation of ConfigReader for AtlasGen tool.

---
+++ plugins/atlasgen/config.rb
@@ -0,0 +1,8 @@
+# An input config as described in input file
+require 'cairo'
+
+# An input config, as described in file, that user inputs to program
+# Also contains input textures.
+class Config
+
+end
