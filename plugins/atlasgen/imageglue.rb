2b5280166ce17e5f2e7725aa91818bb322c7ef99
HiddenSeeker <HiddenSeeker@localhost>
2012-07-03 23:05:52 +0400

    Added ImageGlue.

---
+++ plugins/atlasgen/imageglue.rb
@@ -0,0 +1,10 @@
+# A class, which merges a textures in specified order, changing rect and filling output texture
+class ImageGlue
+    # Merges all images into one
+    # * param images Array of Texture
+    # * param GlueOrder array of GlueOrder
+    # * param output output data
+    # * return Array (surface, context)
+    def glue(images, order, output)
+    end
+end
