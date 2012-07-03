85fcc9d8b7dba9f7f1160475aa08b6f4ea6ab06c
HiddenSeeker <HiddenSeeker@localhost>
2012-07-03 22:31:21 +0400

    Fixed syntax. Added TextureArray and ConfigEntry.

--- plugins/atlasgen/spriteconfig.rb
+++ plugins/atlasgen/spriteconfig.rb
@@ -1,6 +1,74 @@
 # An input config as described in input file
 require 'cairo'
 
+load 'texturearray.rb'
+
+# An entry for config array
+class ConfigEntry
+
+    public 
+    
+    # int Index of array, can be null if tag is omitted 
+    attr_accessor :index
+    # Array size of sprite can be null at input, but must be set with value, when output time is reached
+    attr_accessor :size
+    # String string texture name . Can't be null
+    attr_accessor :inputTextureName
+    # String name of config entry
+    attr_accessor :name
+    # Array list of, can be transparent entry. Set to null if omitted
+    attr_accessor :transparent
+    
+    # String Output texture name, must be set when output
+    attr_accessor :outputTextureName
+    # Array  texture rectangle data
+    attr_accessor :textureRectangle
+    
+    # Inits as non-valid node
+    def initialize()
+        @index = Integer.nil
+        @size = Array.nil
+        @inputTextureName = String.nil
+        @name = String.nil
+        @transparent = Array.nil
+        @outputTextureName = Array.nil
+        @textureRectangle = Array.nil
+    end
+    
+    # Determines, whether reading of entry was successfull and it's valid
+    # * return Boolean true if valid
+    def isValid()
+        if ((@inputTextureName.nil?()) || (@name.nil?()))
+            return false
+        end
+        return true
+    end
+    
+    # Determines, whether we can output some entry to an xml node
+    # * return Boolean true if valid
+    def canOutput()
+        if ((@outputTextureName.nil?()) || (@textureRectangle.nil?()) || (@size.nil?()))
+            return false
+        end
+        return true
+    end
+    
+    # Reads an element to entry
+    # * param element XMLElement
+    def read(element)
+        raise 'Not implemented'
+    end
+    
+    # Writes an element to config
+    # * return XMLElement
+    def write()
+        if (!canOutput())
+            raise 'Attempted to write invalid config'
+        end
+    end
+end
+
+
 # An input config, as described in file, that user inputs to program
 # Also contains input textures.
 class SpriteConfig
