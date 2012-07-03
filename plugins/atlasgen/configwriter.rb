74302906cc3ce17d411c6f87ae421e27caac393e
HiddenSeeker <HiddenSeeker@localhost>
2012-07-03 23:14:04 +0400

    Added ConfigWriter.

---
+++ plugins/atlasgen/configwriter.rb
@@ -0,0 +1,14 @@
+# Writes a new config into file
+require 'cairo'
+require 'rexml/document'
+require 'rexml/element'
+
+# Writes a new config into file
+class ConfigWriter
+    # Writes an config into file
+    # * param config   Config output config data
+    # * param filename String name of output file
+    # * return Boolen  whether writing was successfull
+    def write(config, filename)
+    end
+end
