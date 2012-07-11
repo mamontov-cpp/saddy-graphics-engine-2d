1d9e14c1913d2a940d5de6d4f84c8553ea8f56a0
hiddenseeker <hiddenseeker@localhost>
2012-07-11 08:56:52 +0400

    Added ImageGlue::copy.

--- plugins/atlasgen/imageglue.rb
+++ plugins/atlasgen/imageglue.rb
@@ -1,9 +1,15 @@
 # A class, which merges a texture into output texture
-require 'cairo'
+require 'devil'
 
 # Creates a POT texture with specified size and copies specified texture entries 
-# into it
+# into it, returning some result
 class ImageGlue
+
+	# Copies a source texture into a rectangle described in textureRectangle property (Array of [x,y,w,h]) of destTexture
+	# * param Texture      srcTexture  a source texture entry (see texturearray.rb) for description
+	# * param DevIL::Image destTexture destination texture, where it should be copied.
+	def copy(srcTexture, destTexture)
+	end
     # Merges all images into one
     # * param images Array of Texture
     # * param GlueOrder array of GlueOrder
