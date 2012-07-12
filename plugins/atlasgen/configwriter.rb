f5341ee3526b0dfb4e6f0b7cc2c512ed789b2b57
HiddenSeeker <HiddenSeeker@localhost>
2012-07-12 22:32:08 +0400

    Implemented main.rb and writed some stuff into ConfigWriter.

--- plugins/atlasgen/configwriter.rb
+++ plugins/atlasgen/configwriter.rb
@@ -5,10 +5,22 @@ require 'rexml/element'
 
 # Writes a new config into file
 class ConfigWriter
+
+    def initialize()
+        @errors = []
+    end
     # Writes an config into file
     # * param config   Config output config data
     # * param filename String name of output file
-    # * return Boolen  whether writing was successfull
-    def write(config, filename)
+    # * param outputTextureName String output texture name
+    # * return Boolean  whether writing was successfull
+    def write(config, filename, outputTextureName)
+        # Prepare config for output
+        config.prepareForOutput(outputTextureName)
+        
+    end
+    
+    def getErrors()
+        return @errors
     end
 end
