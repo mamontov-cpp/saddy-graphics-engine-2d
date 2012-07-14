647a3b5bf1952bc54a5a44cbef80f6ca7138c96d
HiddenSeeker <HiddenSeeker@localhost>
2012-07-14 14:52:35 +0400

    Added documentatio for SpriteConfig.

--- plugins/atlasgen/spriteconfig.rb
+++ plugins/atlasgen/spriteconfig.rb
@@ -1,27 +1,42 @@
+##
+# :title: spriteconfig.rb
+# Describes an input and output config some data
 require_relative 'texturearray.rb'
 
-# An entry for config array
+
+##
+# :category: Public classes
+# An entry of config, describing some sprite template
 class ConfigEntry
 
     public 
-    
-    # int Index of array, can be null if tag is omitted 
+    ##
+    # _Fixnum_. Index of entry in sprite template group. Can be null (element with 0)
     attr_accessor :index
-    # Array size of sprite can be null at input, but must be set with value, when output time is reached
+    ##
+    # _Array_ of _Fixnum_ size of sprite on a screen as Array[width, height]. Can be nil in input config
+    # But if nil in output time - must be set by +SpriteConfig::prepareForOutput+ to a size of texture
     attr_accessor :size
-    # String string texture name . Can't be null
+    ##
+    # _String_ source image file name. Can't be nil.
     attr_accessor :inputTextureName
-    # String name of config entry
+    ##
+    # _String_ name of sprite template group, where it belongs.
     attr_accessor :name
-    # Array list of, can be transparent entry. Set to null if omitted
+    ##
+    # _Array_ of _Fixnum_  Mask color for transparency as array [red,green,blue]. Can be nil if not specified
     attr_accessor :transparent
     
-    # String Output texture name, must be set when output
+    ##
+    # _String_ Output texture file name. Must be set by +SpriteConfig::prepareForOutput+ 
     attr_accessor :outputTextureName
-    # Array  texture rectangle data
+    ##
+    # _Array_ of _Fixnum_  a position of source image in an output texture as rectangle [x,y,width, height]
     attr_accessor :textureRectangle
     
-    # Inits as non-valid node
+    ##
+    # :category: Public interface
+    # Inits as non-valid node with nil properties
     def initialize()
         @index = nil
         @size = nil
@@ -31,9 +46,10 @@ class ConfigEntry
         @outputTextureName = nil
         @textureRectangle = nil
     end
-    
+    ##
+    # :category: Public interface
     # Determines, whether reading of entry was successfull and it's valid
-    # * return Boolean true if valid
+    # [return]  _TrueClass_ or _FalseClass_ true, if valid
     def isValid()
         if ((@inputTextureName.nil?()) || (@name.nil?()))
             return false
@@ -42,8 +58,10 @@ class ConfigEntry
     end
     
     
-    # Determines, whether we can output some entry to an xml node
-    # * return Boolean true if valid
+    ##
+    # :category: Public interface
+    # Determines, whether we can output an entry to an XML document
+    # [return]  _TrueClass_ or _FalseClass_ true, if can
     def canOutput()
         if ((@outputTextureName.nil?()) || (@textureRectangle.nil?()) || (@size.nil?()))
             return false
@@ -51,21 +69,29 @@ class ConfigEntry
         return true
     end
     
-    
+    ##
+    # :category: Public interface
+    # Returns an index of sprite template in sprite template group
+    # [return]  _Fixnum_ index of sprite in a template group
     def index()
         if (@index.nil?())
             return 0
         end
         return @index
     end
-    
+    ##
+    # :category: Public interface
+    # Returns a full qualified name of sprite as name:index
+    # [return]  _String_ full name
     def getFullName()
         return @name + ":" + index().to_s()
     end
-    # Reads an element to entry
-    # * param element XMLElement
-    # * param textures SpriteConfig, where could be entries added
-    # * returns array of errors, empty if nothing found
+    ##
+    # :category: Public interface
+    # Reads an XML element to entry. Does not add self to a config, instead loads a source image if possible
+    # [element] _REXML::Element_  an element, which is mapped to entry
+    # [config]  _SpriteConfig_    a resulting config
+    # [return]  _Array_ of _String_ a errors, empty array if nothing found
     def read(element,config)
        errors = []
        # Parse name and index
@@ -108,14 +134,21 @@ class ConfigEntry
        end
        return errors
     end
-    # Converts integer array to a valid XML config string
+    ##
+    # :category: Implementation. DEPRECATED to use
+    # Converts an array of numbers to a semicolon-separated string for output an attributes like +ConfigEntry::size+,
+    # +ConfigEntry::textureRectangle+ and +ConfigEntry::transparent+
+    # [array] _Array_ of _Fixnum_ an input array
+    # [return] _String_  resulting XML string
     def array_to_string(array)
         strings = array.collect{ |item| item.to_s() }
         return strings.join(";")
     end
-    # Writes an element to config
-    # * param root root element
-    # * return XMLElement
+    ##
+    # :category: Public interface
+    # Writes an element into XML element. Does not appends self into a root element.
+    # [root]   _REXML::Element_ root element of document
+    # [return] _REXML::Element_ resulting element of entry to be appended to root element.
     def write(root)
         if (self.canOutput() == false)
             raise 'Attempted to write invalid config'
@@ -135,57 +168,80 @@ class ConfigEntry
 end
 
 
-# An input config, as described in file, that user inputs to program
-# Also contains input textures.
+##
+# :category: Public classes
+# An input and output config, which can be read and written
 class SpriteConfig
 
     private
-    
-    # TextureArray array of textures, that will be merged
+    ##
+    # _TextureArray_.  Array of source images
     attr_writer  :textureArray
-    # Array array of config entries, that must be fixed and replaced in output config
+    ##
+    # _Array_ of _ConfigEntry_ an array of sprite templates
     attr_writer  :configArray
-     # Array array of errors
+    ##
+    # _Array_ of _String_  an array of errors.
     attr_writer :errors
     
     public
-    
+    ##
+    # :category: Public interface
+    # Checks, whether sprite template has been loaded already. Used to check for unique.
+    # [name]   _String_  name of sprite template group
+    # [index]  _Fixnum_  index of element in sprite template group
+    # [return] _TrueClass_ or _FalseClass_ true, if already loaded
     def hasEntry(name, index)
-        return (@configArray.index{ |entry|    (entry.name == name) && (entry.index == index) } != nil)
+        return (@configArray.index{ |entry|    (entry.name == name) && (entry.index() == index) } != nil)
     end
-    
+    ##
+    #  :category: Public interface
+    #  Adds a new sprite tempate to a list
+    # [entry] _ConfigEntry_ source sprite template
     def pushEntry(entry)
         @configArray = @configArray << entry
     end
-    # Inits new empty config
+    ##
+    # :category: Public interface
+    # Inits new empty config.
     def initialize()
         @textureArray = TextureArray.new()
         @configArray = Array.new()
     end
-    
-    # Returns a texture array
-    # * return TextureArray. An array of textures entry
+    ##
+    # :category: Public interface
+    # Returns an array of source images
+    # [return] _TextureArray_  An array of source images
     def getTextures()
         return @textureArray
     end
-    # Returns a config array
-    # * return Array. A config entries array
+    ##
+    # :category: Public interface
+    # Returns an array of sprite templates
+    # [return] _Array_  of _Texture_ An array of sprite templates
     def getConfigArray()
         return @configArray
     end
-    # Sets a texture array
-    # * param textureArray TextureArray. An array of textures entry
+    ##
+    # :category: Public interface
+    # Sets an array of source images
+    # [textureArray] _TextureArray_  A soure array
     def setTextures(textureArray)
         @textureArray = textureArray
     end
-    # Sets a config array
-    # * param configArray Array. A config entries array
+    ##
+    # :category: Public interface
+    # Sets an array of sprite templates
+    # [configArray] _Array_  of _Texture_ An array of sprite templates
     def setConfigArray(configArray)
         @configArray = configArray
     end
-    # Tried to load a texture, if absent in base, otherwise return true. If fails, return false. Adds a new error, if has some
-    # * param filename name of texture
-    # * return true on success, otherwise false.
+    ##
+    # :category: Public interface
+    # Tried to load a texture, if absent in base, otherwise return true. 
+    # If fails, return false. Adds a new error, if loading was failed.
+    # [filename] _String_                    name of file with source texture
+    # [return]   _TrueClass_ or _FalseClass_ true on success, otherwise false.
     def queryLoadTexture(filename)
         if (@textureArray.getTexture(filename)!=nil)
             return true
@@ -199,20 +255,31 @@ class SpriteConfig
         end
         return false
     end
-    # Copies a texture rectangle from part data, size if not set and sets an output texture name from string
+    ##
+    # :category: Public interface
+    # Prepares a config for output to XML documents
+    # Copies a +textureRectangle+ from source +Texture+ data, +size+ if not explicitly specified 
+    # and sets an +outputTextureName+ from passed string
+    # [outputTexName] _String_ filename of output texture.
     def prepareForOutput(outputTexName)
         @configArray.each{
             |entry|
+            # Get texture
             texture = @textureArray.getTexture(entry.inputTextureName)
+            # Get rectangle
             entry.textureRectangle = texture.textureRectangle
+            # Get size if need
             if (entry.size.nil?)
                 entry.size = texture.size()
             end
+            # Get output texture
             entry.outputTextureName = outputTexName
         }
     end
-    # Returns a list of errors
-    # * return Array list of errors
+    ##
+    # :category: Public interface
+    # Returns an array of errors
+    # [return] _Array_ of _String_ array of errors
     def getErrors()
         return @errors
     end
