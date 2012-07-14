6e431b6564ca4ef20efdffc6b9dc3f3f98b0b0e2
HiddenSeeker <HiddenSeeker@localhost>
2012-07-14 15:02:41 +0400

    Added documentation for Texture class.

--- plugins/atlasgen/texturearray.rb
+++ plugins/atlasgen/texturearray.rb
@@ -1,30 +1,40 @@
-# A texture array data, used for displaying and merging textures
+##
+# :title: texturearray.rb
+# Describes an array of textures with handling of unique names
 require 'devil'
 
-# A texture, as presented here, consists from name and associated cairo data
+##
+# :category: Public classes
+# A source image texture with name of source file stored and rectangle in output texture
 class Texture
     private
     
-    # DevIL image
+    ##
+    # _DevIL::Image_. a source image surface, which can be copied
     attr_accessor :image
 
     
     public
-    # Array  texture rectangle data
+    ##
+    # _Array_ of _Fixnum_.  Position of texture in output data as [x,y,width,height]
     attr_accessor :textureRectangle
-    # String name a name of texture
+    ##
+    # _String_. Name of source file for texture
     attr_accessor :name
-    
-    # Initializes texture with some name
-    # * String name of textures
+    ##
+    # :category: Public interface
+    # Initializes texture with some soure filename
+    # [name] _String_ source file name of image
     def initialize(name)
         @name = name
     end
-    
-    # Tries to load some texture from file
-    # * return Boolean true on success, false otherwise
+    ##
+    # :category: Public interface
+    # Tries to load some texture from file specified by +Texture::name+
+    # [return] _TrueClass_ or _FalseClass_ true on success, false otherwise
     def load()
         res = true
+        # Sometimes, library rise some strange errors, so we capture them.
         begin       
             @image = Devil.load_image(@name)
             if (@image.nil?())
@@ -35,9 +45,10 @@ class Texture
         end
         return res
     end
-    
-    # Returns an image of texture
-    # * return DevIL::Image an image of texture
+    ##
+    # :category: Public interface
+    # Returns a source image from texture
+    # [return] _DevIL::Image_ an image of texture
     def getImage()
         if (@image.nil?())
             raise 'Attempt to work with invalid texture'
@@ -45,16 +56,19 @@ class Texture
         return @image
     end
     
-    
-    # Size of texture returns
-    # * return Array size of texture as [w,h]
+    ##
+    # :category: Public interface
+    # Returns size of texture
+    # [return] _Array_ of _FixNum_ size of texture as [width,height]
     def size()
         return [ image.width(), image.height() ]
     end
 end
 
 
-# An array of textures
+##
+# :category: Public classes
+# A an array of textures, which handles unique names for textures
 class TextureArray < Array
 
     # Pushes unique texture
