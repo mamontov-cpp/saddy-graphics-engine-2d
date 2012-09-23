43d5119885bb504eae079aef33ad8f64c5abd7d9
HiddenSeeker <HiddenSeeker@localhost>
2012-09-23 21:29:25 +0400

    Added a default test config.

---
+++ plugins/ifaceed/ifaceed/core/spritedatabase.h
@@ -0,0 +1,13 @@
+/* \file spritedatabase.h
+   \author HiddenSeeker
+
+   A database for holding all of the sprites
+*/
+#include "xmlconfigloader.h"
+
+/** Database of sprites and data
+ */
+class AbstractSpriteDatabase
+{
+
+};
