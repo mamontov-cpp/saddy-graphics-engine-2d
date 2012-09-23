aff65f3d435f7900e04644cef0a574452ee6fba4
HiddenSeeker <HiddenSeeker@localhost>
2012-09-23 21:58:09 +0400

    Implemented a simple image copying.

--- plugins/ifaceed/ifaceed/core/spritedatabase.h
+++ plugins/ifaceed/ifaceed/core/spritedatabase.h
@@ -4,10 +4,18 @@
    A database for holding all of the sprites
 */
 #include "xmlconfigloader.h"
-
+#include <QImage>
+#pragma once
 /** Database of sprites and data
  */
 class AbstractSpriteDatabase
 {
 
 };
+
+/** Extract image from big texture
+	\param[in] im image data
+	\param[in] t  template 
+	\return image data
+ */
+QImage extractImage(const QImage & im, Sprite2DTemplate & t);
