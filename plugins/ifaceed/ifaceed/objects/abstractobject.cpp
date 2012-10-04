a415961128cd2afff7a53c15b4a16475973f5274
HiddenSeeker <HiddenSeeker@localhost>
2012-10-04 21:57:17 +0400

    Implemented AbstractObject

--- plugins/ifaceed/ifaceed/objects/abstractobject.cpp
+++ plugins/ifaceed/ifaceed/objects/abstractobject.cpp
@@ -3,3 +3,23 @@
 
 SAD_DECLARE(AbstractObject,sad::BasicNode)
 
+
+AbstractObject::~AbstractObject()
+{
+}
+
+
+void AbstractObject::render()
+{
+	if (m_active && m_visible)
+		_render();
+}
+
+ScreenTemplate * AbstractObject::screenTemplate()
+{
+	SerializableContainer * parent = this->parent();
+	if (!parent)
+		return NULL;
+	return static_cast<ScreenTemplate *>(parent);
+}
+
--- plugins/ifaceed/ifaceed/objects/abstractobject.h
+++ plugins/ifaceed/ifaceed/objects/abstractobject.h
@@ -6,15 +6,84 @@
 #include "scene.h"
 #include "marshal/serializableobject.h"
 #include "templates/refcountable.h"
+#include "primitives/hpoint.h"
 #pragma once
 
 class ScreenTemplate;
-
+class InterlockedScene;
 /** Describes an abstract object of screen template
  */
 class AbstractObject: public sad::BasicNode, public SerializableObject, public RefCountable
 {
  SAD_NODE
-
+ protected:
+	/** Whether object is active
+	 */
+	bool m_active;
+	/** Whether object is visible
+	 */ 
+	bool m_visible;
+	/** A scene with object
+	 */
+	InterlockedScene * m_scene;
+	/** Renders an object. NOTE: Overload this to do actual render of object
+	 */
+	virtual void _render()=0;
+ public:
+	/** Returns parent object of template
+	 */
+	ScreenTemplate * screenTemplate();
+	/** Describes a scene with data
+	 */
+	inline void setScene(InterlockedScene * scene)
+	{
+		m_scene = scene;
+	}
+	/** Returns a scene for object
+		\return object scene
+	 */
+	inline InterlockedScene * scene()
+	{
+		return m_scene;
+	}
+	/** Whether object is active
+		\return object is active
+	 */
+	inline bool active() 
+	{
+		return m_active;
+	}
+	/** Whether object is visible
+		\return object visible
+	 */
+	inline bool visible() 
+	{
+		return m_visible;
+	}
+	/** Sets activity of object
+		\param[in] a activity
+	 */
+	inline void setActive(bool a) 
+	{
+		m_active = a;
+	}
+	/** Sets visibility of object
+		\param[in] a activity
+	 */
+	inline void setVisible(bool a) 
+	{
+		m_visible = a;
+	}
+	/** Renders an abstract object if active and visible
+	 */
+	virtual void render();
+	/** Returns a region, where object is placed
+	    \param[in] p point
+		\return whether point is within region
+     */
+	virtual bool isWithin(const hPointF & p)=0;
+	/** Deletes a object
+	 */
+	virtual ~AbstractObject();
 };
 
