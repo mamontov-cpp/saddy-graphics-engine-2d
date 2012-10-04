d48089241cc347212d8a9fe75bee6cc4d30786ea
HiddenSeeker <HiddenSeeker@localhost>
2012-10-04 23:04:56 +0400

    Implemented EditorBehaviourState

---
+++ plugins/ifaceed/ifaceed/editorcore/editorbehaviourstate.cpp
@@ -0,0 +1,54 @@
+#include "editorbehaviourstate.h"
+
+EditorBehaviourState::EditorBehaviourState()
+{
+	m_behaviour = NULL;
+}
+
+EditorBehaviourState::~EditorBehaviourState()
+{
+}
+
+void EditorBehaviourState::enter()
+{
+}
+
+void EditorBehaviourState::leave()
+{
+}
+
+void EditorBehaviourState::onMouseMove(const sad::Event & ev)
+{
+
+}
+
+void EditorBehaviourState::onMouseDown(const sad::Event & ev)
+{
+
+}
+
+void EditorBehaviourState::onMouseUp(const sad::Event & ev)
+{
+
+}
+
+void EditorBehaviourState::onWheelUp(const sad::Event & ev)
+{
+
+}
+
+void EditorBehaviourState::onWheelDown(const sad::Event & ev)
+{
+
+}
+
+void EditorBehaviourState::onKeyDown(const sad::Event & ev)
+{
+
+}
+
+void EditorBehaviourState::onKeyUp(const sad::Event & ev)
+{
+
+}
+
