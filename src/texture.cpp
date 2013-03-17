fda31a6172bd56b2e8154d883639ec66c87b4101
HiddenSeeker <HiddenSeeker@localhost>
2013-03-17 21:36:10 +0400

    Made images not so blurry.

--- src/texture.cpp
+++ src/texture.cpp
@@ -83,8 +83,8 @@ void Texture::buildMipMaps()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, what);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, what);
 
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);	
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);	
     
 	// Build Mip Maps	
 	GLint res;
