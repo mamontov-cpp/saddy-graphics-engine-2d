3a2cc8ce2722559483ee33b6a299b5f888b07e9f
hiddenseeker <hiddenseeker@localhost>
2013-03-20 12:26:43 +0400

    implemented basic LogMessage

---
+++ include/log/log.h
@@ -0,0 +1,108 @@
+/*! \file log.h
+	\author HiddenSeeker
+	
+	Describes a basic log and log parameters
+ */
+#include "../3rdparty/format/format.h"
+#include "../templates/hstring.h"
+#include <ctime>
+#include <string.h>
+#pragma once
+
+namespace sad
+{
+	namespace log
+	{
+		/*! Priotity of message of log
+		 */
+		enum Priority
+		{
+			FATAL = 0,
+			CRITICAL = 1,
+			WARNING = 2,
+			MESSAGE = 3,
+			DEBUG = 4,
+			USER = 5
+		};
+		/*! A colors table for coloring some text in console 
+		 */
+		enum Color
+		{
+			NONE =  0,
+			LIGHT_RED = 1,
+			LIGHT_GREEN = 2,
+			LIGHT_BLUE = 3,
+			DARK_RED = 4,
+			DARK_GREEN = 5,
+			DARK_BLUE = 6,
+			WHITE = 7,
+			LIGHT_GREY = 8,
+			DARK_GREY = 9,
+			BLACK = 10,
+			LIGHT_YELLOW = 11,
+			DARK_YELLOW = 12,
+			LIGHT_MAGENTA = 13,
+			DARK_MAGENTA = 14,
+			LIGHT_CYAN = 15,
+			DARK_CYAN = 16
+		};
+		/*! Describes a logging message 
+		 */
+		class Message
+		{
+			private:	
+				hst::string m_subsystem; //!< Subsystem
+				hst::string m_message;   //!< Message data
+				time_t      m_time;		 //!< Current time
+				sad::log::Priority m_priority; //!< Information about message priority
+				hst::string        m_user_priority; //!<  User priority, not used if non USER
+				const char   *     m_file;          //!<  Compiled file, null if nothing
+				int                m_line;          //!<  A line of code, null if nothing
+			public:
+				/*! Creates a new message
+					\param[in] message message text
+					\param[in] priority priority type
+					\param[in] file file information
+					\param[in] line line info
+					\param[in] subsystem subsystem info
+					\param[in]  user_priority user priority information
+				 */
+				Message(const hst::string & message,
+						sad::log::Priority priority,
+						const char * file = NULL,
+						int line = 0,
+						const hst::string & subsystem = hst::string(),
+						const hst::string & user_priority = hst::string()
+					   );
+					   
+				inline const hst::string & subsystem() const
+				{ return m_subsystem; } //!< Returns a subsystem
+				inline const hst::string & message() const
+				{ return m_message; } //!< Returns a message					
+				const char * stime() const; //!< A string-alike time
+				inline const time_t & time() const
+				{ return m_time; }   //!< Returns a time
+				inline sad::log::Priority priority() const { return m_priority; } //!< A priority
+				inline const hst::string & upriority() const
+				{ return m_user_priority; } //!< Returns a user priority
+				/*! Returns string like "name of file", "line"				
+				 */
+				hst::string fileline();
+				/*! Returns a stringified priority
+				 */
+				const char * spriority() const;
+				/*! A file constant
+				 */
+				inline const char * file()  const { return m_file; }
+			    /*! A line constant
+				 */
+				inline int line()  const { return m_line; }
+		};
+	};
+	
+	
+	
+
+
+};
+ 
---
+++ src/log/log.cpp
@@ -0,0 +1,59 @@
+#include "../../include/log/log.h"
+
+static const char * logmesgs[6] = {
+	"FATAL",
+	"CRITICAL",
+	"WARNING",
+	"MESSAGE",
+	"DEBUG",
+	"USER"
+};
+
+const char * sad::log::Message::spriority() const
+{
+	if (m_priority != sad::log::USER || m_user_priority.length() == 0)
+	{
+		return logmesgs[(int)m_priority];
+	}
+	return m_user_priority.data();
+}
+
+
+const char * sad::log::Message::stime() const
+{
+	return asctime(localtime(&m_time));
+}
+
+sad::log::Message::Message(const hst::string & message,
+					   	   sad::log::Priority priority,
+						   const char * file,
+						   int line ,
+						   const hst::string & subsystem ,
+						   const hst::string & user_priority 
+					      )
+{
+	m_message = message;
+	m_priority = priority;
+	m_file = file;
+	m_line = line;
+	m_subsystem = subsystem;
+	m_user_priority = user_priority;
+	m_time = ::time(NULL);
+}
+
+
+hst::string sad::log::Message::fileline()
+{
+	if (!m_file)
+		return hst::string();
+	hst::string o;
+	char ch = '/';
+#ifndef _WIN32
+	ch = '//';
+#endif	
+	o =  (strrchr(m_file, ch) ? strrchr(m_file, ch) + 1 : m_file);
+	o << ", ";
+	o << hst::string::number(m_line);
+	return o;
+}
+
