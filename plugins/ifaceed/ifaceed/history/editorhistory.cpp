a7babf571aa85d6d8d77d3e0cce566c76b855c71
HiddenSeeker <HiddenSeeker@localhost>
2013-03-23 14:24:20 +0400

    Fixed bugs in applying changed to fonts

--- plugins/ifaceed/ifaceed/history/editorhistory.cpp
+++ plugins/ifaceed/ifaceed/history/editorhistory.cpp
@@ -1,4 +1,6 @@
 #include "editorhistory.h"
+#include <3rdparty/format/format.h>
+#include <log/log.h>
 
 EditorHistory::EditorHistory(ActionContext * c)
 {
@@ -16,19 +18,53 @@ EditorHistory::~EditorHistory()
 
 void EditorHistory::commit(CommandChangeObserver * ob)
 {
+	SL_SCOPE("EditorHistory::commit");
+	SL_DEBUG(
+		str(
+			fmt::Format("At position {0} with {1} commands")
+			<< m_current << m_commands.count()
+		)
+	);
 	if (m_commands.count() && m_current!=m_commands.count()-1)
 	{
+		SL_DEBUG( 
+			str(
+				fmt::Format("Commiting command {0}")
+				<< m_current + 1
+			)
+		);
 		m_commands[m_current+1]->commit(m_c, ob);
 		++m_current;
+	}
+	else
+	{
+		SL_DEBUG("Nothing to commit");
 	}
 }
 
 void EditorHistory::rollback(CommandChangeObserver * ob)
 {
+	SL_SCOPE("EditorHistory::rollback");
+	SL_DEBUG(
+		str(
+			fmt::Format("At position {0} with {1} commands")
+			<< m_current << m_commands.count()
+		)
+	);
 	if (m_commands.count() && m_current!=-1)
 	{
+		SL_DEBUG( 
+			str(
+				fmt::Format("Rolling back command {0}")
+				<< m_current
+			)
+		);
 		m_commands[m_current]->rollback(m_c, ob);
 		--m_current;
+	}
+	else
+	{
+		SL_DEBUG("Nothing to rollback");
 	}
 }
 
