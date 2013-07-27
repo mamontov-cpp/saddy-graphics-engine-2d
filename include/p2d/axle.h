1be97d0a5f75ceb86e240f2bd4a40ec32b769724
HiddenSeeker <HiddenSeeker@localhost>
2013-07-28 01:06:50 +0400

    Partially finished refactoring of geometry2d header

--- include/p2d/axle.h
+++ include/p2d/axle.h
@@ -4,12 +4,50 @@
 	Defines axle as a case of 2d vector
  */
 #pragma once
-#include "../primitives/hpoint.h"
+#include "vector.h"
+#include "../templates/hpair.hpp"
 
 namespace p2d 
 {
-	typedef hPointF Vector;
 	/*! An axle is a special case of a vector
 	 */
 	typedef p2d::Vector Axle;
+	/*! A one-dimensional cutter is a pair of doubles
+	 */
+	typedef hst::pair<double, double> Cutter1D;
+	/*! A two-dimensional cutter is a pair of points
+	 */
+	typedef hst::pair<hPointF, hPointF> Cutter2D;
+	/*! Makes an axle from two points by sequentially creating
+		a vector out of result of substraction of points and
+		making unit ount of result
+		\param[in] p1 first point
+		\param[in] p2 second point
+		\return resulting axle
+	 */
+	p2d::Axle axle(const hPointF & p1, const hPointF & p2);
+	/*! Projects a cutter to an axle
+		\param[in] c cutter
+		\param[in] a axle
+		\return one-dimensional cutter
+	 */
+	p2d::Cutter1D project(const p2d::Cutter2D & c, 
+						  const p2d::Axle & a);
+	/*! Projects a cutter to an axle
+		\param[in] p1 first point of cutter
+		\param[in] p1 second point of cutter
+		\param[in] a axle
+		\return one-dimensional cutter
+	 */
+	p2d::Cutter1D project(const hPointF & p1,
+						  const hPointF & p2,
+						  const p2d::Axle & a);
+	/*! Tests, whether two one-dimensional cutters collide
+		\param[in] c1 first cutter
+		\param[in] c2 second cutter
+		\return whether they are colliding
+	 */
+	bool collides(const p2d::Cutter1D & c1, 
+			      const p2d::Cutter1D & c2);
+
 }
