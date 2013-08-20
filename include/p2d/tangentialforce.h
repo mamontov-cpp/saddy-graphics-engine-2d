bef1d469661a2e73d89fb64a9cf62b7801eafd79
HiddenSeeker <HiddenSeeker@localhost>
2013-08-20 15:48:13 +0400

    Created tickable, due to similar behaviour in TangentialForce, GhostOptions and Force. Made it common ancestor for those.

---
+++ include/p2d/tangentialforce.h
@@ -0,0 +1,30 @@
+/*! \file tangentialforce.h
+	\author HiddenSeeker
+
+	Describes a tangential force, acting on object
+ */
+#pragma once
+#include "tickable.h"
+
+namespace p2d
+{
+
+/*! A class for getting default value for a tickable state
+ */
+template<>
+class TickableDefaultValue<double>
+{
+ public:
+	 static inline double get() { return 0; }
+};
+
+/*! A force as a tickable state
+ */
+typedef p2d::TickableState<double> TangentialForce;
+/*! A flow for a force
+ */
+typedef p2d::TickableFlow<double> TangentialForceFlow;
+
+
+}
+
