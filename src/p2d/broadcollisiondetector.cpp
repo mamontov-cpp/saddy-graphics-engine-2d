a01dbc3f4270d83f949fc7eef76831cf7dd6a276
HiddenSeeker <HiddenSeeker@localhost>
2013-08-22 14:54:48 +0400

    Implemented complex test with complex step in p2d::World. Fixed bug in p2d::BroadCollisionDetector

--- src/p2d/broadcollisiondetector.cpp
+++ src/p2d/broadcollisiondetector.cpp
@@ -7,8 +7,8 @@ p2d::MaybeTime p2d::BroadCollisionDetector::collides(p2d::Body * b1,
 	p2d::ConvexHull  s11 = b1->at(0).toHull();
 	p2d::ConvexHull  s12 = b1->at(limit).toHull();
 	
-	p2d::ConvexHull  s21 = b1->at(0).toHull();
-	p2d::ConvexHull  s22 = b1->at(limit).toHull();
+	p2d::ConvexHull  s21 = b2->at(0).toHull();
+	p2d::ConvexHull  s22 = b2->at(limit).toHull();
 	
 	p2d::ConvexHull s1  = p2d::ConvexHull::getUnion(s11, s12);
 	p2d::ConvexHull s2  = p2d::ConvexHull::getUnion(s21, s22);
--- tests/p2d/worldtest.cpp
+++ tests/p2d/worldtest.cpp
@@ -44,6 +44,31 @@ namespace p2dworld
 	typedef p2d::MovementDeltaListener<p2dworld::UserObject2, p2d::Vector> move2_t;
 	typedef p2d::MovementDeltaListener<p2dworld::UserObject1, double> rotate1_t;
 	typedef p2d::MovementDeltaListener<p2dworld::UserObject2, double> rotate2_t;
+
+	struct StateMachine
+	{
+		   int state;
+		   p2d::Body * b1;
+		   p2d::Body * b2;
+
+		   void step(const p2d::BasicCollisionEvent & ev)
+		   {		
+				this->state +=1;
+				if (this->state == 1)
+				{
+					b1->world()->splitTimeStepAt(ev.m_time);
+					b1->sheduleTangentialVelocityAt(p2d::Vector(0.0, 6.0), ev.m_time);
+					b2->setCurrentPosition(p2d::Vector(3.0, 3.0));
+				}
+				if (this->state == 2)
+				{
+					b1->world()->splitTimeStepAt(ev.m_time);
+					b1->sheduleTangentialVelocityAt(p2d::Vector(-4.0, 0.0), ev.m_time);
+					b2->setCurrentPosition(p2d::Vector(7.0, 1.5));
+				}
+		   }
+
+	};
 }
 DECLARE_SOBJ(p2dworld::UserObject1);
 DECLARE_SOBJ(p2dworld::UserObject2);
@@ -89,7 +114,9 @@ struct WorldTest : tpunit::TestFixture
 	   TEST(WorldTest::testRemove),
 	   TEST(WorldTest::testClear),
 	   TEST(WorldTest::testRemoveFromCallback),
-	   TEST(WorldTest::testClearFromCallback)
+	   TEST(WorldTest::testClearFromCallback),
+	   TEST(WorldTest::testListeners),
+	   TEST(WorldTest::testMultipleSteps)
    ) {}
 
    int eventperformed;
@@ -183,6 +210,8 @@ struct WorldTest : tpunit::TestFixture
 	   ASSERT_TRUE( this->eventperformed == 2 );
 	   ASSERT_TRUE( ::eventperformed == 2 );
 	   delete w;
+	   delete u11;
+	   delete u21;
    }
 
    void testCollisionCallbacksForU1U2()
@@ -220,6 +249,8 @@ struct WorldTest : tpunit::TestFixture
 	   ASSERT_TRUE( this->eventperformed == 2 );
 	   ASSERT_TRUE( ::eventperformed == 2 );
 	   delete w;
+	   delete u1;
+	   delete u2;
    }
 
    void testRemove()
@@ -309,6 +340,91 @@ struct WorldTest : tpunit::TestFixture
 	   w->step(1.0);
 	   w->step(1.0);
 	   ASSERT_TRUE( ::eventperformed == 2 );
+	   delete w;
+   }
+
+   void testListeners()
+   {
+	   p2dworld::UserObject1  * u1 = new p2dworld::UserObject1();
+	   p2dworld::UserObject2  * u2 = new p2dworld::UserObject2();
+
+	   this->eventperformed = 0;
+	   ::eventperformed = 0;
+	   p2d::Body * b1 = new p2d::Body();
+	   p2d::Circle * c1 = new p2d::Circle();
+	   c1->setRadius(1.0);
+	   b1->setShape(c1);
+	   b1->setCurrentTangentialVelocity(p2d::Vector(1.0, 0.0));
+	   b1->setCurrentAngularVelocity(1.0);
+	   b1->setUserObject(u1);
+	   b1->addMoveListener( new p2dworld::move1_t(u1, &p2dworld::UserObject1::notifyMove) );
+	   b1->addRotateListener( new p2dworld::rotate1_t(u1, &p2dworld::UserObject1::notifyRotate) );
+
+	   p2d::Body * b2 = new p2d::Body();
+	   p2d::Circle * c2 = new p2d::Circle();
+	   c2->setRadius(1.0);
+	   b2->setShape(c2);
+	   b2->setUserObject(u2);
+	   b2->setCurrentTangentialVelocity(p2d::Vector(1.0, 0.0));
+	   b2->setCurrentAngularVelocity(1.0);
+	   b2->addMoveListener( new p2dworld::move2_t(u2, &p2dworld::UserObject2::notifyMove) );
+	   b2->addRotateListener( new p2dworld::rotate2_t(u2, &p2dworld::UserObject2::notifyRotate) );
+	   b2->setCurrentPosition(p2d::Point(6.0, 0.0));
+	   
+	   p2d::World * w = new p2d::World();
+	   
+	   w->addBody(b1);
+	   w->addBody(b2);
+	   
+	   w->step(1.0);
+	   w->step(1.0);
+	   
+	   ASSERT_TRUE( is_fuzzy_equal(u1->position().x(), 2.0) );
+	   ASSERT_TRUE( is_fuzzy_equal(u2->position().x(), 8.0) );
+	   ASSERT_TRUE( is_fuzzy_equal(u1->position().y(), 0.0) );
+	   ASSERT_TRUE( is_fuzzy_equal(u2->position().y(), 0.0) );
+
+	   ASSERT_TRUE( is_fuzzy_equal(u1->angle(), 2.0) );
+	   ASSERT_TRUE( is_fuzzy_equal(u2->angle(), 2.0) );
+
+	   delete w;
+	   delete u1;
+	   delete u2;
+   }
+
+   void testMultipleSteps()
+   {
+	   p2dworld::StateMachine sm;
+
+	   p2d::Body * b1 = new p2d::Body();
+	   p2d::Circle * c1 = new p2d::Circle();
+	   c1->setRadius(1.0);
+	   b1->setShape(c1);
+	   b1->setCurrentTangentialVelocity(p2d::Vector(6.0, 0.0));
+	   
+	   p2d::Body * b2 = new p2d::Body();
+	   p2d::Circle * c2 = new p2d::Circle();
+	   c2->setRadius(1.0);
+	   b2->setShape(c2);
+	   b2->setCurrentPosition(p2d::Point(3.0, 0.0));
+	
+	   sm.state = 0;
+	   sm.b1 = b1;
+	   sm.b2 = b2;
+
+	   p2d::BroadCollisionDetector * det = new p2d::BroadCollisionDetector();
+	   p2d::World * w = new p2d::World();
+	   w->setDetector(det);
+	   w->addHandler(&sm, &p2dworld::StateMachine::step);
+	   w->addBody(b1);
+	   w->addBody(b2);
+	   
+	   w->step(1.0);
+	   ASSERT_TRUE( sm.state == 2 );
+	   ASSERT_FLOAT_EQUAL( b1->position().x(), 2);
+	   ASSERT_FLOAT_EQUAL( b1->position().y(), 1.5);
+	   ASSERT_FLOAT_EQUAL( b2->position().x(), 7);
+	   ASSERT_FLOAT_EQUAL( b2->position().y(), 1.5);
 	   delete w;
    }
 
