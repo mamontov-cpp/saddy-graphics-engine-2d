ebca78dda6bc7ec4df5ee861d8f3c7c1034e61e3
HiddenSeeker <HiddenSeeker@localhost>
2013-08-23 09:04:58 +0400

    Added enemy bullet

---
+++ examples/game/enemybullet.h
@@ -0,0 +1,19 @@
+/*! \file playerbullet.h
+	\author HiddenSeeker
+
+	Describes a bullet owned by player
+ */
+#include "gameobject.h"
+#include "gameobjectconstants.h"
+#pragma once
+
+/*! As a default, player bullet is a simple object with settings from
+    constant
+ */
+class EnemyBullet: public GameObject
+{
+ SAD_OBJECT
+ public:
+	 EnemyBullet();
+};
+
