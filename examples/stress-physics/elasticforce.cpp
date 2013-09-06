7a5a3863150f0427d301b5a517acdeacdfcc9c67
HiddenSeeker <HiddenSeeker@localhost>
2013-09-06 07:49:28 +0400

    1. Fixed comments in elastic force 2. Made world initialization more simple

--- examples/stress-physics/elasticforce.cpp
+++ examples/stress-physics/elasticforce.cpp
@@ -1,11 +1,19 @@
 #include "elasticforce.h"
 #include <log/log.h>
 
-ElasticForce::ElasticForce(p2d::Body * first, p2d::Body * second)
+ElasticForce::ElasticForce(
+						   p2d::Body * first, 
+						   p2d::Body * second,
+						   double elasticity, 
+						   double resistance
+						  )
 {
 	m_first = first;
 	m_second = second;
-	m_defaultdistance = m_second->currentShape()->center().distanceTo(m_first->currentShape()->center());
+	m_defaultdistance = m_second->currentShape()->center()
+						.distanceTo(m_first->currentShape()->center());
+	m_elasticity = elasticity;
+	m_resistance = resistance;
 }
 
 const p2d::Vector & ElasticForce::value() const
@@ -13,8 +21,8 @@ const p2d::Vector & ElasticForce::value() const
 	ElasticForce * force = const_cast<ElasticForce *>(this);
 	p2d::Point s1 = m_first->currentShape()->center();	
 	p2d::Point s2 = m_second->currentShape()->center();
-	double k = 20;
-	double b = 0.005;
+	double k = m_elasticity;
+	double b = m_resistance;
 	double distance = s1.distanceTo(s2);
 	double delta = distance - m_defaultdistance;
 
