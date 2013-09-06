0bccc5b1a64ea21f12c6d20fedb531f2da548f10
HiddenSeeker <HiddenSeeker@localhost>
2013-09-06 14:34:54 +0400

    Started implementing BounceSolver

--- examples/stress-physics/bouncesolver.h
+++ examples/stress-physics/bouncesolver.h
@@ -5,17 +5,32 @@
 #include <p2d/findcontactpoints.h>
 #pragma once
 
+/*! A bounce solver allows to easily solve problems with 
+	bouncing of various objects. 
+	
+	Due to hard problems with force detection it is HIGHLY recommended, that
+	objects must be checked against with p2d::CollisionTest, before calling a
+	bounce. 
+
+	This is 
+ */
 class BounceSolver
 {
 protected:
-	p2d::FindContactPoints * m_find;
-	p2d::Body * m_first;
-	p2d::Body * m_second;
+	p2d::FindContactPoints * m_find;  //!< Current algorithm for finding a contact poinnts
+	p2d::Body * m_first;   //!< First body to test against
+	p2d::Body * m_second;  //!< Second body to test against
+	
+	p2d::Vector m_av1;   //!< An approximated speeed for first body
+	p2d::Vector m_av2;   //!< An approximated speed for second body
 	
 	/*! Performs bouncing off a solver
 		\param[in] pairs a pairs
 	 */ 
 	void performBouncing(const p2d::SetOfPointsPair & pairs);
+	/*! Approximately solves time of impact and finds contact points
+	 */
+	void solveTOIFCP(const p2d::SetOfPointsPair & pairs);	
 public:
 	/*! Constructs new solver
 	 */
