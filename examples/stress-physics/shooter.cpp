37082e4fe0683b4f8011bc6b4bd486edb7ae0ab2
HiddenSeeker <HiddenSeeker@localhost>
2013-09-09 23:00:18 +0400

    Shooter gonna shoot.

--- examples/stress-physics/shooter.cpp
+++ examples/stress-physics/shooter.cpp
@@ -1,8 +1,46 @@
 #include "shooter.h"
+#include <renderer.h>
+#include <input.h>
+
+
 
 DECLARE_SOBJ_INHERITANCE(Shooter, p2d::app::Object)
 
 Shooter::Shooter()
 {
 	this->initFromConstants<Shooter>();
+	m_gun = new Shooter::Gun(this);
 }
+
+
+void Shooter::startShooting()
+{
+	sad::Renderer::ref()->controls()->addPreRenderTask(
+			new TimePeriodicalTask( m_gun )
+		);
+}
+
+
+Shooter::Gun::Gun(Shooter * parent) : m_parent(parent)
+{
+	this->setInterval(500);
+	this->setMinSpeed(p2d::Vector(0, 100));
+	this->setMaxSpeed(p2d::Vector(0, 100));
+	this->setMaxCount(2);
+}
+
+p2d::app::App * Shooter::Gun::app()
+{
+	return m_parent->app();
+}
+
+void Shooter::Gun::perform()
+{
+	this->setMinPosition(m_parent->position() - p2d::Vector(5, 0));
+	this->setMaxPosition(m_parent->position() + p2d::Vector(5, 0));
+	this->setInterval(200.0 +  800.0 * this->prand());
+	this->p2d::app::RandomDirectionObjectEmitter<ColoredBullet>::perform();
+}
+
+
+
--- examples/stress-physics/shooter.h
+++ examples/stress-physics/shooter.h
@@ -5,6 +5,8 @@
  */
 #include <p2d/app/object.h>
 #include <p2d/app/constants.h>
+#include <p2d/app/objectemitter.h>
+#include "coloredbullet.h"
 #pragma once
 
 /*! A simple default object
@@ -12,6 +14,19 @@
 class Shooter: public p2d::app::Object
 {
  SAD_OBJECT
+
+ class Gun: public p2d::app::RandomDirectionObjectEmitter<ColoredBullet>
+ {
+  protected:
+	Shooter * m_parent;
+  public:
+	 Gun(Shooter * parent);
+	 virtual p2d::app::App * app();
+	 virtual void perform();
+ };
+ protected:
+	 Gun * m_gun;
  public:
 	 Shooter();
+	 void startShooting();
 };
