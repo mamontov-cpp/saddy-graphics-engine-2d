038b2e8e1314cf2f053672744cb9db5da933b385
HiddenSeeker <HiddenSeeker@localhost>
2013-09-14 15:50:17 +0400

    And now we need only one preprocessed makefile to build whole library.

--- preprocess.sh
+++ preprocess.sh
@@ -1,5 +1,5 @@
 g++ tools/hbuild.cpp -o bin/hbuild -DLINUX
-bin/hbuild  preprocess/Makefile_library_mingw.hbd -o makefiles/Makefile_Library_Debug.mingw "TYPE" "debug" "CCOPTS" "-g"
-bin/hbuild  preprocess/Makefile_library_mingw.hbd -o makefiles/Makefile_Library_Release.mingw "TYPE" "release" "CCOPTS" "-O2"
-bin/hbuild  preprocess/Makefile_library_linux.hbd -o makefiles/Makefile_Library_Debug.linux "TYPE" "debug" "CCOPTS" "-g"
-bin/hbuild  preprocess/Makefile_library_linux.hbd -o makefiles/Makefile_Library_Release.linux "TYPE" "release" "CCOPTS" "-O2"
+bin/hbuild  preprocess/Makefile.hbd -o makefiles/Makefile_Library_Debug.mingw "PLATFORM"  "WIN32" "TYPE" "debug" 
+bin/hbuild  preprocess/Makefile.hbd -o makefiles/Makefile_Library_Release.mingw "PLATFORM"  "WIN32" "TYPE" "release" 
+bin/hbuild  preprocess/Makefile.hbd -o makefiles/Makefile_Library_Debug.linux "PLATFORM"  "LINUX"  "TYPE" "debug" 
+bin/hbuild  preprocess/Makefile.hbd -o makefiles/Makefile_Library_Release.linux "PLATFORM"  "LINUX"  "TYPE" "release"
