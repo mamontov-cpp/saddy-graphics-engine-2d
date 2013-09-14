465adfdaf649893dac8ae80fd68bfa0a70286c85
overanalyzer1 <overanalyzer1@gmail.com>
2013-09-14 13:30:02 +0400

    Fixed linux build

--- preprocess.sh
+++ preprocess.sh
@@ -1,5 +1,5 @@
 g++ tools/hbuild.cpp -o bin/hbuild -DLINUX
-bin/hbuild  preprocess/Makefile_library_debug_mingw.hbd -o makefiles/Makefile_Library_Debug.mingw
-bin/hbuild  preprocess/Makefile_library_release_mingw.hbd -o makefiles/Makefile_Library_Release.mingw
-bin/hbuild  preprocess/Makefile_library_debug_linux.hbd -o makefiles/Makefile_Library_Debug.linux
-bin/hbuild  preprocess/Makefile_library_release_linux.hbd -o makefiles/Makefile_Library_Release.linux
+bin/hbuild  preprocess/Makefile_library_mingw.hbd -o makefiles/Makefile_Library_Debug.mingw "TYPE" "debug" "CCOPTS" "-g"
+bin/hbuild  preprocess/Makefile_library_mingw.hbd -o makefiles/Makefile_Library_Release.mingw "TYPE" "release" "CCOPTS" "-O2"
+bin/hbuild  preprocess/Makefile_library_linux.hbd -o makefiles/Makefile_Library_Debug.linux "TYPE" "debug" "CCOPTS" "-g"
+bin/hbuild  preprocess/Makefile_library_linux.hbd -o makefiles/Makefile_Library_Release.linux "TYPE" "release" "CCOPTS" "-O2"
