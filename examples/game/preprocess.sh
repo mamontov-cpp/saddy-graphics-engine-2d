5cb538ee155b8ab9c1f73aa87a1f4fd269a1cdb3
HiddenSeeker <HiddenSeeker@localhost>
2013-09-14 16:20:24 +0400

    Simplified makefiles in game example.

--- examples/game/preprocess.sh
+++ examples/game/preprocess.sh
@@ -1,4 +1,4 @@
-../../bin/hbuild preprocess/Makefile_demo_debug_mingw.hbd -o makefiles/Makefile_Demo_Debug.mingw
-../../bin/hbuild  preprocess/Makefile_demo_release_mingw.hbd -o makefiles/Makefile_Demo_Release.mingw
-../../bin/hbuild  preprocess/Makefile_demo_debug_linux.hbd -o makefiles/Makefile_Demo_Debug.linux
-../../bin/hbuild  preprocess/Makefile_demo_release_linux.hbd -o makefiles/Makefile_Demo_Release.linux
+../../bin/hbuild preprocess/Makefile.hbd -o makefiles/Makefile_Demo_Debug.mingw  "PLATFORM"  "WIN32" "TYPE" "debug" 
+../../bin/hbuild  preprocess/Makefile.hbd -o makefiles/Makefile_Demo_Release.mingw "PLATFORM"  "WIN32" "TYPE" "release" 
+../../bin/hbuild  preprocess/Makefile.hbd -o makefiles/Makefile_Demo_Debug.linux "PLATFORM"  "LINUX"  "TYPE" "debug" 
+../../bin/hbuild  preprocess/Makefile.hbd -o makefiles/Makefile_Demo_Release.linux "PLATFORM"  "LINUX"  "TYPE" "release"
