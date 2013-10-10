20bbdfaf599bcdd56771fb3551e92e8b825f2c38
HiddenSeeker <HiddenSeeker@localhost>
2013-10-10 19:53:58 +0400

    Renamed os::timer to sad::os::TimerImpl. Added sad::Timer as frontend version of timer.

--- src/os/timerimpl.cpp
+++ src/os/timerimpl.cpp
@@ -1,10 +1,10 @@
-#include <os/timer.h>
+#include <os/timerimpl.h>
 #include <log/log.h>
 
 #ifdef WIN32
 
  
-void os::put_last_error() {
+void sad::os::put_last_error() {
 	DWORD errCode = GetLastError();
     char *err;
     if (!FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
@@ -24,7 +24,7 @@ void os::put_last_error() {
 
 #endif
 
-os::timer::timer()
+sad::os::TimerImpl::TimerImpl()
 {
 #ifdef WIN32
 
@@ -40,7 +40,7 @@ os::timer::timer()
 
 	if(!QueryPerformanceFrequency(&m_frequency))
 	{
-		os::put_last_error();
+		sad::os::put_last_error();
 	}
 #else
 		clock_getres(SADDY_USED_CLOCK_TYPE, &m_frequency);
@@ -49,26 +49,26 @@ os::timer::timer()
 }
 
 
-void os::timer::start()
+void sad::os::TimerImpl::start()
 {
 #ifdef WIN32
 	BOOL result = QueryPerformanceCounter(&m_start); 
 	if (!result)
 	{
-		os::put_last_error();
+		sad::os::put_last_error();
 	}
 #else
 		clock_gettime(SADDY_USED_CLOCK_TYPE, &m_start);
 #endif
 }
 
-void os::timer::stop()
+void sad::os::TimerImpl::stop()
 {
 #ifdef WIN32
 	BOOL result = QueryPerformanceCounter(&m_end); 
 	if (!result)
 	{
-		os::put_last_error();
+		sad::os::put_last_error();
 	}
 #else
 		clock_gettime(SADDY_USED_CLOCK_TYPE, &m_end);
@@ -77,10 +77,11 @@ void os::timer::stop()
 
 
 
-double os::timer::elapsed() const
+double sad::os::TimerImpl::elapsed() const
 {
 #ifdef WIN32
-	QueryPerformanceFrequency(&(const_cast<os::timer*>(this)->m_frequency));
+	sad::os::TimerImpl * me = const_cast<sad::os::TimerImpl *>(this);
+	QueryPerformanceFrequency(&(me->m_frequency));
 	double millisecondsofhpc = (m_end.QuadPart - m_start.QuadPart) * 1000.0 / m_frequency.QuadPart;
 	return millisecondsofhpc;
 #else
