b001bd9eb6163bdb14a5e32c0003dd3df17f8826
mamontov dp <mamontov.dp@gmail.com>
2013-10-16 15:24:51 +0400

    Added debug logging for ball

--- examples/stress-physics/ball.cpp
+++ examples/stress-physics/ball.cpp
@@ -1,8 +1,31 @@
 #include "ball.h"
+#include <renderer.h>
+#include <3rdparty/format/format.h>
 
 DECLARE_SOBJ_INHERITANCE(Ball, sad::p2d::app::Object)
 
 Ball::Ball()
 {
 	this->initFromConstants<Ball>();
+
+	sad::log::FileTarget * t = new sad::log::FileTarget();
+	t->open("ball.txt");
+
+	m_log = new sad::log::Log();
+	m_log->addTarget(t);
+}
+
+void Ball::render()
+{
+	this->sad::p2d::app::Object::render();
+	sad::p2d::Point p = this->body()->position();
+	sad::p2d::Point v = this->body()->tangentialVelocity();
+	std::string s = str(fmt::Format("{0} {1} {2} {3}") << p.x() << p.y() << v.x() << v.y() << sad::Renderer::ref()->fps());
+	SL_DEBUG(s);
+}
+
+
+Ball::~Ball()
+{
+	
 }
