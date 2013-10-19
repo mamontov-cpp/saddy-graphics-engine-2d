ceabefbcb8c618572cc2f4b1eaa86cc1d6b10a48
mamontov dp <mamontov.dp@gmail.com>
2013-10-19 15:08:42 +0400

    Added #ifdef. if any other bug occured, it will be easy to track it, using demo

--- examples/stress-physics/ball.cpp
+++ examples/stress-physics/ball.cpp
@@ -8,13 +8,17 @@ Ball::Ball()
 {
 	this->initFromConstants<Ball>();
 
+#ifdef PHYSICS_ENGINE_DEBUG
 	sad::log::FileTarget * t = new sad::log::FileTarget();
 	t->open("ball.txt");
 
 	m_log = new sad::log::Log();
 	m_log->addTarget(t);
+#endif
 }
 
+
+#ifdef PHYSICS_ENGINE_DEBUG
 void Ball::render()
 {
 	this->sad::p2d::app::Object::render();
@@ -23,6 +27,7 @@ void Ball::render()
 	std::string s = str(fmt::Format("{0} {1} {2} {3} {4}") << p.x() << p.y() << v.x() << v.y() << sad::Renderer::ref()->fps());
 	m_log->debug(s);
 }
+#endif
 
 
 Ball::~Ball()
