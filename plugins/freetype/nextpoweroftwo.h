5436d772fd33f0e8aab86f471b53bd527fa70687
mamontov dp <mamontov.dp@gmail.com>
2013-10-30 17:07:06 +0400

    Added debug output

--- plugins/freetype/nextpoweroftwo.h
+++ plugins/freetype/nextpoweroftwo.h
@@ -12,7 +12,8 @@ namespace sad
 namespace freetype
 {
 
-/*! Computes nearest power of two to number. Guaranteed, that returned number
+/*! Computes nearest power of two to number. Guaranteed, that returned numberþ
+    Taken from http://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2
 	will be more or equal to v
 	\param[in] v number to be rounded
 	\return v number to be rounded
