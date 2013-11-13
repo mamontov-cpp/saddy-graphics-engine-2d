37b17340b4c07d380f03a1854122d8af3765fbcc
HiddenSeeker <HiddenSeeker@localhost>
2013-11-13 22:09:16 +0400

    Implemented creating context on Windows.

--- src/os/glcontextimpl.cpp
+++ src/os/glcontextimpl.cpp
@@ -1,8 +1,15 @@
 #include "os/glcontextimpl.h"
+
 #include <GL/gl.h>
 #include <GL/glu.h>
 
+#include "3rdparty/glext/glext.h"
+#ifdef WIN32
+#include "3rdparty/glext/wglext.h"
+#endif
+
 sad::os::GLContextImpl::GLContextImpl()
+: m_win(NULL), m_handle(), m_isopengl3compatible(false)
 {
 
 }
@@ -16,12 +23,68 @@ sad::os::GLContextImpl::~GLContextImpl()
 }
 
 
+#ifdef WIN32
+typedef HGLRC (APIENTRYP PFNWGLCREATECONTEXTATTRIBSARBPROC)(HDC,HGLRC, const int *);
+#endif
+
+
 bool sad::os::GLContextImpl::createFor(sad::Window * win)
 {
 	if (valid())
 		return true;
 
 	m_win = win;
+	m_isopengl3compatible = false;
+
+#ifdef WIN32
+	bool result = false;
+	m_handle.Context = wglCreateContext(win->handles()->DC); 
+	if(m_handle.Context) 
+	{
+		wglMakeCurrent(win->handles()->DC, m_handle.Context);
+		result = true;
+		// Try to parse version from context
+		int major = 0, minor = 0;
+		const char *verstr = (const char *) glGetString(GL_VERSION);
+		if ((verstr == NULL) || (sscanf(verstr,"%d.%d", &major, &minor) != 2))
+		{
+			major = minor = 0;
+		}
+
+		if (major * 10 + minor >= 30)
+		{
+			PFNWGLCREATECONTEXTATTRIBSARBPROC wglCreateContextAttribsARB = NULL;
+			wglCreateContextAttribsARB = (PFNWGLCREATECONTEXTATTRIBSARBPROC)wglGetProcAddress("wglCreateContextAttribsARB");
+			if (wglCreateContextAttribsARB != NULL)
+			{
+				int attributes[] =
+				{
+					WGL_CONTEXT_MAJOR_VERSION_ARB, 
+					major,
+					WGL_CONTEXT_MINOR_VERSION_ARB, 
+					minor,
+					WGL_CONTEXT_FLAGS_ARB,         
+					WGL_CONTEXT_FORWARD_COMPATIBLE_BIT_ARB,
+					0
+				};
+				HGLRC newContext = wglCreateContextAttribsARB(
+					win->handles()->DC, 
+					0, 
+					attributes
+				);
+				if (newContext != NULL)
+				{
+					m_isopengl3compatible = true;
+					wglDeleteContext(m_handle.Context);
+					m_handle.Context = newContext;
+					wglMakeCurrent(win->handles()->DC, m_handle.Context);
+				}
+			}
+		}
+	} 
+	
+	return result;
+#endif
 
 	// Stub
 	return false;
@@ -41,6 +104,8 @@ void sad::os::GLContextImpl::destroy()
 	glXMakeCurrent(m_win->handles()->Dpy, 0, 0);    
     glXDestroyContext(m_win->handles()->Dpy, m_handle.Context);
 #endif
+
+	m_isopengl3compatible = false;
 	
 	m_handle.cleanup();
 }
@@ -105,4 +170,10 @@ sad::os::GLContextHandle * sad::os::GLContextImpl::handle() const
 sad::Window * sad::os::GLContextImpl::window() const
 {
 	return m_win;
+}
+
+
+bool sad::os::GLContextImpl::isOpenGL3compatible() const
+{
+	return m_isopengl3compatible;
 }
