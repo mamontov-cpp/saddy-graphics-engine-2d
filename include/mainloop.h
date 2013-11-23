8e83d06e0705eb6dd0faa2f1a858978f27dab530
HiddenSeeker <HiddenSeeker@localhost>
2013-11-23 23:01:45 +0400

    Somehow forgor header of main loop

---
+++ include/mainloop.h
@@ -0,0 +1,68 @@
+/*! \file mainloop.h
+	\author HiddenSeeker
+
+	Describes a main event loop. 
+	Note, that you should not directly use this class,
+	use sad::Renderer::run() instead.
+ */
+#pragma once
+
+namespace sad
+{
+class Renderer;
+
+/*! Describes a main event loop.
+	Note, that you should not directly use this class,
+	use sad::Renderer::run() instead.
+ */
+class MainLoop
+{
+public:
+	/*! Constructs new main loop 
+	 */
+	MainLoop();
+	/*! Sets new renderer attached to a loop
+		\param[in] r renderer
+	 */
+	virtual void setRenderer(sad::Renderer * r);
+	/*! Returns a renderer attached to a loop
+		\return attached renderer
+	 */
+	virtual sad::Renderer * renderer() const;
+	/*! Runs a main loop
+	 */
+	void run();
+protected:
+	/*! Tries to elevate priority of current process, when performing main loop
+	 */
+	void tryElevatePriority();
+	/*! Yields execution, giving other processes time to run
+	 */
+	void yield();
+	/*! Tries to set emergency handler, which should handle case, when user
+		closes console window on Windows
+	 */
+	void trySetEmergencyShudownHandler();
+	/*! Registers current renderer in a Windows platform in table of current 
+		renderers. That is done because of implementation of event system on
+		windows platform.
+	 */
+	void registerRenderer();
+	/*! Unregisters current renderer in a Windows platform in table of current 
+		renderers. That is done because of implementation of event system on
+		windows platform.
+	 */
+	void unregisterRenderer();
+	/*! A real unchecked main loop function.
+	 */
+	void perform();
+	/*! Determines, whether main loop is running
+	 */
+	bool m_running;
+	/*! A renderer, linked to loop
+	 */
+	sad::Renderer * m_renderer;
+};
+
+}
+
