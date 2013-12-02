545f8c7fae8a417e3b2475bd21cc46b41550788d
mamontov dp <mamontov.dp@gmail.com>
2013-12-02 13:01:45 +0400

    Started implementing handlerconditions.h

--- include/input/handlerconditions.h
+++ include/input/handlerconditions.h
@@ -5,6 +5,9 @@
  */
 #pragma once
 #include "events.h"
+#include "../sadvector.h"
+#include "../sadpair.h"
+#include "../maybe.h"
 
 namespace sad
 {
@@ -29,7 +32,65 @@ public:
 	virtual ~AbstractHanderCondition();
 };
 
+/*! A handler conditions as a list of conditions
+ */
+typedef sad::Vector<sad::input::AbstractHanderCondition *> 
+	HandlerConditionsList;
+
+
+/*! A class for both type and conditions.
+	Here we introduce a small implementation hack, which allows to reduce count of constructor of handlers.
+
+	Note, that you can create memory leak, by creating it outside of handler specification part (a call to
+	sad:input::Controls::add) and not freeing a handler conditions list.
+ */
+class HandlerTypeAndConditions: public sad::Pair<sad::input::EventType, sad::input::HandlerConditionsList>
+{
+public:
+	/*! A conversion hack for putting event type to handler insertion
+		\param[in] t type of event
+	 */
+	inline HandlerTypeAndConditions(sad::input::EventType t) 
+	: sad::Pair<sad::input::EventType, sad::input::HandlerConditionsList>(
+		t, 
+		sad::input::HandlerConditionsList()
+	)
+	{
+	}
+	/*! A parametric cnstructor for options
+		\param[in] t type of event
+		\param[in] m a list of handler conditions
+	 */
+	inline HandlerTypeAndConditions(sad::input::EventType t, const  sad::input::HandlerConditionsList & m ) 
+	: sad::Pair<sad::input::EventType, sad::input::HandlerConditionsList>(
+		t, 
+		m
+	)
+	{
+	}
+	/*! Adds new condition into list, preserving constant parts
+		\param[in] constants
+	 */
+	inline const HandlerTypeAndConditions & operator<<(sad::input::AbstractHanderCondition * c) const
+	{
+		HandlerTypeAndConditions & me = const_cast<sad::input::HandlerTypeAndConditions &>(*this);
+		me._2() << c;
+		return *this;
+	}
+
+};
+
 
 }
 
+}
+
+/*! Converts event type to type and conditions, allowing to write something like 
+	*t | Condition(a) | Condition(b)
+	\param[in] t type
+	\return type and conditions
+ */
+inline sad::input::HandlerTypeAndConditions operator*(sad::input::EventType t)
+{
+	return sad::input::HandlerTypeAndConditions(t);
 }
