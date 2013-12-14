3bae62ea7fc38bbbfcd205635e04e405eefab278
mamontov dp <mamontov.dp@gmail.com>
2013-12-14 14:08:13 +0400

    Implemented sad::pipeline::Process

--- include/pipeline/pipelineprocess.h
+++ include/pipeline/pipelineprocess.h
@@ -28,6 +28,68 @@ public:
 	virtual bool shouldBeDestroyedAfterProcessing();
 };
 
+
+class Process: public sad::pipeline::AbstractProcess
+{
+public:
+	/*! Creates process for specified callable
+		\param[in] f first callable object
+	 */
+	template<
+		typename _Callable
+	>
+	Process(_Callable f)
+	{
+		m_delegate = new sad::pipeline::Function<_Callable>(f);
+	}
+	/*! Creates process for specified method call
+		\param[in] o object, which method call os performed on
+		\param[in] f called method
+	 */
+	template<
+		typename _Object,
+		typename _Method
+	>
+	Process(_Object * o, _Method f)
+	{
+		m_delegate = new sad::pipeline::MethodCall<_Object, _Method>(o, f);
+	}
+	/*! Creates process for specified chained method call
+		\param[in] o object, which method call os performed on
+		\param[in] f first called method
+		\param[in] g second called method
+	 */
+	template<
+		typename _Object,
+		typename _FirstMethod,
+		typename _SecondMethod
+	>
+	Process(_Object * o, _FirstMethod f, _SecondMethod g)
+	{
+		m_delegate = new sad::pipeline::MethodCall<_Object, _FirstMethod, _SecondMethod>(o, f, g);
+	}
+	/*! Destroys a delegate in process
+	 */
+	virtual ~Process();
+protected:
+	/*! Invokes a delegate inside of process
+	 */ 
+	virtual void _process();
+	/*!  A linked delegate for process
+	 */
+	sad::pipeline::Delegate * m_delegate;
+private:
+	/*! Disable copying for class
+		\param[in] o other process
+	 */
+	Process(const Process & o);
+	/*! Disable copying for class
+		\param[in] o other process
+		\return self-reference
+	 */
+	Process & operator=(const Process & o);
+};
+
 }
 
 }
