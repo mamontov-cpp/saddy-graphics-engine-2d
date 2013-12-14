9725affb1ea91d2d5d5420ad0309d5db9f534449
mamontov dp <mamontov.dp@gmail.com>
2013-12-14 14:08:13 +0400

    Implemented sad::pipeline::Process

--- src/pipeline/pipelineprocess.cpp
+++ src/pipeline/pipelineprocess.cpp
@@ -5,3 +5,12 @@ bool sad::pipeline::AbstractProcess::shouldBeDestroyedAfterProcessing()
 	return false;
 }
 
+sad::pipeline::Process::~Process()
+{
+	delete m_delegate;
+}
+
+void sad::pipeline::Process::_process()
+{
+	m_delegate->call();
+}
