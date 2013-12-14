e455b28e88aca511b8a2f18f590c9b02d18b9fbd
mamontov dp <mamontov.dp@gmail.com>
2013-12-14 14:41:28 +0400

    Started implementing sad::pipeline::Pipeline

---
+++ include/pipeline/pipeline.h
@@ -0,0 +1,28 @@
+/*! \file   pipeline.h
+	\author HiddenSeeker
+
+	Declares a main pipeline item for declaring what should be done in main loop of renderer
+ */
+#pragma once
+#include "pipelinestep.h"
+#include "../temporarilyimmutablecontainer.h"
+#include "../sadpair.h"
+
+namespace sad
+{
+
+namespace pipeline
+{
+
+typedef sad::Triplet< sad::Maybe<>, sad::Maybe<sad::String>, sad::pipeline::Step *>
+
+class Pipeline
+{
+
+
+};
+
+}
+
+}
+
