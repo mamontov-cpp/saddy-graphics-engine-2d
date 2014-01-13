2deb7e11fec9c0d50cdfbe726fd3a233d0a695ed
HiddenSeeker <HiddenSeeker@localhost>
2014-01-13 20:46:22 +0400

    Added missing source files.

---
+++ src/os/generatemipmaps30.cpp
@@ -0,0 +1,26 @@
+#include "os/generatemipmaps30.h"
+#include "log/log.h"
+#include "renderer.h"
+
+static PFNGLGENERATEMIPMAPEXTPROC _generateMipMaps  = NULL;
+
+bool sad::os::generateMipMaps30(sad::Renderer * r, GLenum target)
+{
+	PFNGLGENERATEMIPMAPEXTPROC __generateMipMaps = _generateMipMaps;
+	if (_generateMipMaps == NULL)
+	{
+		__generateMipMaps = (PFNGLGENERATEMIPMAPEXTPROC)getProcAdress("glGenerateMipmap");
+		_generateMipMaps = __generateMipMaps;
+	}
+	bool result = false;
+	if (__generateMipMaps)
+	{
+		__generateMipMaps(target);
+		result = true;
+	}
+	else
+	{
+		SL_COND_LOCAL_INTERNAL("Failed to obtain glGenerateMipmap", r);
+	}
+	return result;
+}
