3523eefede5244e4300f456574c6d5d8771171c2
HiddenSeeker <HiddenSeeker@localhost>
2014-01-13 20:46:22 +0400

    Added missing source files.

---
+++ include/os/glheaders.h
@@ -0,0 +1,39 @@
+/*! \file glheaders.h
+	\author HiddenSeeker
+
+	A summarizing header, which contains all of needed OpenGL headers
+ */
+#pragma once
+
+#ifdef WIN32
+	#ifndef NOMINMAX
+    #define NOMINMAX 
+    #endif
+	#include <windows.h>
+	#include <gl/gl.h>
+	#include <gl/glu.h>
+#endif
+
+#ifdef X11
+	#include <GL/gl.h>
+	#include <GL/glu.h>
+	#include <GL/glx.h>
+#endif
+
+#include <3rdparty/glext/glext.h>
+
+// Placing get proc adress here to make sure it could be accessible
+
+#ifdef WIN32
+	#define getProcAdress wglGetProcAddress
+#endif
+
+#ifdef X11
+
+#ifdef GLX_ARB_get_proc_adress
+	#define getProcAdress glXGetProcAddressARB
+#else
+	#define getProcAdress glXGetProcAddress
+#endif
+
+#endif
