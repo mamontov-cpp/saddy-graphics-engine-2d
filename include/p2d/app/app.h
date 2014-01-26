f1b47faf47e7ead15a7a6709d9abc5417e4e0000
HiddenSeeker <HiddenSeeker@localhost>
2014-01-26 11:30:06 +0400

    Added layer definition in sad::p2d::app::App

--- include/p2d/app/app.h
+++ include/p2d/app/app.h
@@ -29,6 +29,7 @@ class App
 protected:
 	p2d::World   * m_world;          //!< A physical engine world
 	p2d::WorldStepTask * m_steptask; //!< A main step task, used to work with world
+	unsigned int         m_layer;    //!< A layer of scene, which should layer work with
 protected:
 	/*! Returns current in-game scene. A helper function for renderer's scene
 		\return scene from renderer
@@ -38,21 +39,22 @@ protected:
 	 */
 	virtual void createWorld();
 	/*! Inits application
+		\param[in] layer a layer of scene, where world will be attached
 		\param[in] r renderer (NULL for global renderer)
 	 */
-	virtual void initApp(sad::Renderer * r = NULL);
+	virtual void initApp(unsigned int layer = 0, sad::Renderer * r = NULL);
 public:
 	/*! Creates a new application
 	 */
 	App();
-	/*! Quits a game 
+	/*! Quits an application 
 	 */
 	virtual void quit();
 	/*! Removes a game object from game
 		\param[in] o object
 	 */
 	virtual void removeObject(p2d::app::Object * o);
-	/*! Runs a game
+	/*! Runs an application
 	 */
 	virtual void run();
 	/*! Adds a game object
