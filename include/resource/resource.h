f491ffe07486a7667eed402cd0fb1e2bcda548e8
HiddenSeeker <HiddenSeeker@localhost>
2014-02-04 21:45:56 +0400

    Implemented resource class

--- include/resource/resource.h
+++ include/resource/resource.h
@@ -3,58 +3,97 @@
 	
 	Contains definition of class Resource.
 
+	A resource is a basic object, which can be loaded from file and stored in resource databse
+	and have dependency on it.
  */
 #pragma once
+#include "../object.h"
+#include "../sadstring.h"
 
+namespace sad
+{
+class Renderer;
+}
 
 namespace resource
 {
+class PhysicalFile;
+class Error;
+class Folder;
+class AbstractLink;
 
 /*! \class Resource
 
+	A resource is a basic object, which can be loaded from file and stored in resource databse
+	and have dependency on it.
  */
 class Resource: public sad::Object  
 {	
-public:	
+SAD_OBJECT
+public:
+	/*! Inits new resource, which does not belong to any folder
+	 */
+	Resource();
 	/*! This class can be inherited 
 	 */
 	virtual ~Resource();
-	/*! 
-		\return
+	/*! A resources in factory should be created via resourceType() hint. So every resource type
+		should implement it
+		\return name for resource type. Must be unique.
 	 */
-	sad::String resourceType();
-	/*! 
-		\param[in] file
+	virtual sad::String resourceType() = 0;
+	/*! Loads a resource from specified file, using specified renderer for resolving some 
+		properties.
+		\param[in] file a file, via which a resource should be loaded
+		\param[in] r  a renderer, which resource should be linked to (NULL if global renderer)
+		\param[in] store_links hints, whether we should store a links
 	 */
-	void load(resource::PhysicalFile file);
-	/*! 
-		\param[in] a
+	virtual bool load(
+		const resource::PhysicalFile & file,
+		sad::Renderer * r = NULL,
+		bool store_links = true
+	) = 0;
+	/*! Copies all links from current instance to other resource
+		\param[in] a new resource
 	 */
 	void replaceWith(resource::Resource* a);
-	/*! 
-		\return
-	 */
-	sad::Vector<resource::Error*> reload();
-	/*! 
-		\param[in] folder
+	/*! Sets parent folder for a resource
+		\param[in] folder a parent folder for resource
 	 */
 	void setParentFolder(resource::Folder* folder);
-	/*! 
-		\return
+	/*! Returns parent folder for a resource
+		\return a parent folder for a resource
 	 */
 	resource::Folder* parentFolder();
-	/*! 
-		\return
+	/*! Returns name for a resource. Names must be unique in this folder
+		\return name for a resource
 	 */
-	sad::String name();
+	const sad::String & name();
+	/*! Sets name for resource
+		\param[in] name a name to be set
+	 */
+	void setName(const sad::String & name);
+	/*! Adds link to resource. Note, that link is not notified about it.
+		\param[in] link a link to resource
+	 */
+	void addLink(resource::AbstractLink* link);
+	/*! Removes link from resource. Note, that link is not notified about it.
+		\param[in] link a link to resource
+	 */
+	void removeLink(resource::AbstractLink* link);
 protected: 
-	/*! 
+	/*! A parent folder of resoruce
 	 */
-	resource::Folder m_folder;
-private: 
-	/*! 
+	resource::Folder* m_folder;
+	/*! Whether we should store links to resources
+	 */
+	bool m_store_links;
+	/*! Returns name for resource
+	 */
+	sad::String m_name;
+	/*! A links to a resource
 	 */
 	sad::Vector<resource::AbstractLink*> m_links;
-}
+};
 
 }
--- src/resource/resource.cpp
+++ src/resource/resource.cpp
@@ -1,42 +1,71 @@
 #include "resource/resource.h"
 
+#include <algorithm>
+
+DECLARE_SOBJ(resource::Resource);
+
+
+resource::Resource::Resource() : m_folder(NULL), m_store_links(true)
+{
+	
+}
+
 resource::Resource::~Resource()
 {
-
-}
-
-sad::String resource::Resource::resourceType()
-{
-		return sad::String();	
-}
-
-void resource::Resource::load(resource::PhysicalFile file)
-{
-			
+	for(size_t i = 0; i < m_links.size(); i++)
+	{
+		// TODO: Notify links, that resource is gone
+        /* */
+		/*! TODO: Uncomment this */
+		// delete m_links[i];
+	}
 }
 
 void resource::Resource::replaceWith(resource::Resource* a)
 {
-			
+	a->m_links << this->m_links;
+	// Notify links, that we are gone
+
+	// Clear self links
+	m_links.clear();
 }
 
-sad::Vector<resource::Error*> resource::Resource::reload()
-{
-		return sad::Vector<resource::Error*>();	
-}
 
 void resource::Resource::setParentFolder(resource::Folder* folder)
 {
-			
+	m_folder = folder;
 }
 
 resource::Folder* resource::Resource::parentFolder()
 {
-		return resource::Folder*();	
+	return m_folder;
 }
 
-sad::String resource::Resource::name()
+const sad::String& resource::Resource::name()
 {
-		return sad::String();	
+	return m_name;	
 }
 
+void resource::Resource::setName(const sad::String & name)
+{
+	m_name = name;
+}
+
+void resource::Resource::addLink(resource::AbstractLink* link)
+{	
+	if (m_store_links && link)
+	{
+		if (std::find(m_links.begin(), m_links.end(), link) == m_links.end())
+		{
+			m_links << link;
+		}
+	}
+}
+
+void resource::Resource::removeLink(resource::AbstractLink* link)
+{
+	if (m_store_links && link)
+	{
+		m_links.removeFirst(link);
+	}		
+}
