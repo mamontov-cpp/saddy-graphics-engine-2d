4c99dfdf47f27f1c0691453bab64d3da0a33a20e
HiddenSeeker <HiddenSeeker@localhost>
2014-02-04 22:50:04 +0400

    Implemented resource::AbstractLink

--- src/resource/abstractlink.cpp
+++ src/resource/abstractlink.cpp
@@ -1,42 +1,83 @@
 #include "resource/abstractlink.h"
+#include "resource/resource.h"
 
-resource::AbstractLink::AbstractLink(sad::String resourceType)
+resource::AbstractLink::AbstractLink(const sad::String & resource_type)
+: m_changed(false), m_resource(NULL),  m_path(""), m_tree(NULL)
 {
-
+	
 }
 
 resource::AbstractLink::~AbstractLink()
 {
-
+	if (m_resource)
+	{
+		m_resource->removeLink(this);
+	}
 }
 
 void resource::AbstractLink::attach(resource::Resource* r)
 {
-			
+	m_resource->removeLink(this);
+	m_resource = r;
+	if (m_resource)
+	{
+		m_resource->addLink(this);			
+	}
+	m_changed = true;
 }
 
 void resource::AbstractLink::detach()
 {
-			
+	m_changed = true;
+	m_resource = NULL;
 }
 
-bool resource::AbstractLink::valid(resource::Tree* tree)
+resource::Resource* resource::AbstractLink::resource() const
 {
-		return bool();	
-}
-
-resource::Resource* resource::AbstractLink::resource()
-{
-		return resource::Resource*();	
+	if (m_resource == NULL && m_tree != NULL)
+	{
+		// TODO: Actually implement
+	}
+	return m_resource;
 }
 
 void resource::AbstractLink::setPath(sad::String path)
 {
-			
+	m_path = path;
+	if (m_resource)
+	{
+		m_resource->removeLink(this);
+	}
+	m_changed = true;
+	m_resource = NULL;
 }
 
-sad::String resource::AbstractLink::path()
+const sad::String&  resource::AbstractLink::path() const
 {
-		return sad::String();	
+	return m_path;	
 }
 
+void resource::AbstractLink::finishedChanging()
+{
+	m_changed = true;
+}
+
+bool resource::AbstractLink::changed() const
+{
+	return m_changed;
+}
+
+void  resource::AbstractLink::setTree(resource::Tree * tree)
+{
+	m_tree = tree;
+	if (m_resource)
+	{
+		m_resource->removeLink(this);
+	}
+	m_resource = NULL;
+}
+
+resource::Tree* resource::AbstractLink::tree() const
+{
+	return m_tree;	
+}
