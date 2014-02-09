ebc0132a3b6876776f105e3d923798dee6c4c2ec
HiddenSeeker <HiddenSeeker@localhost>
2014-02-09 22:10:20 +0400

    Implemeted sad::resource::Error inheritance from sad::Object and resourcealreadyexists type of error

--- src/resource/error.cpp
+++ src/resource/error.cpp
@@ -1,5 +1,11 @@
 #include "resource/error.h"
 
+DECLARE_SOBJ(sad::resource::Error);
+DECLARE_SOBJ_INHERITANCE(sad::resource::FileLoadError, sad::resource::Error);
+DECLARE_SOBJ_INHERITANCE(sad::resource::ResourceLoadError, sad::resource::Error);
+DECLARE_SOBJ_INHERITANCE(sad::resource::MissingResource, sad::resource::Error);
+DECLARE_SOBJ_INHERITANCE(sad::resource::ResourceAlreadyExists, sad::resource::Error);
+
 sad::resource::Error::~Error()
 {
 
@@ -20,3 +26,7 @@ sad::resource::MissingResource::~MissingResource()
 
 }
 
+sad::resource::ResourceAlreadyExists::~ResourceAlreadyExists()
+{
+	
+}
