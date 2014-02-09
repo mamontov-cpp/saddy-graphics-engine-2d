5cb8405b9e3adee8180b2a9c86a2309821fecd10
HiddenSeeker <HiddenSeeker@localhost>
2014-02-09 22:20:41 +0400

    Implemented creation of rectangle from two points and free for errors.

---
+++ include/util/free.h
@@ -0,0 +1,25 @@
+/*! \file free.h
+	\author HiddenSeeker
+
+	Contains definition for free, which can free a resources, stored in vectors
+ */
+#pragma once
+#include "../sadvector.h"
+
+namespace util
+{
+
+
+/*! Frees resources, stored in vector
+	\param[in] v a vector, whose resources must be freed
+ */ 
+template<typename T>
+void free(const sad::Vector<T*> & v)
+{
+	for(int i = 0; i < v.size(); i++)
+	{
+		delete v[i];
+	}
+}
+
+}
