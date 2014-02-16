fe25517e90c92141a86795585c985b5411e8b6dc
overanalyzer1 <overanalyzer1@gmail.com>
2014-02-16 22:17:49 +0400

    Fixed getting lbrary in mips

--- src/os/generatemipmaps30.cpp
+++ src/os/generatemipmaps30.cpp
@@ -9,7 +9,11 @@ bool sad::os::generateMipMaps30(sad::Renderer * r, GLenum target)
 	PFNGLGENERATEMIPMAPEXTPROC __generateMipMaps = _generateMipMaps;
 	if (_generateMipMaps == NULL)
 	{
+#ifdef WIN32
 		__generateMipMaps = (PFNGLGENERATEMIPMAPEXTPROC)getProcAdress("glGenerateMipmap");
+#else		
+		__generateMipMaps = (PFNGLGENERATEMIPMAPEXTPROC)getProcAdress((GLubyte*)"glGenerateMipmap");
+#endif
 		_generateMipMaps = __generateMipMaps;
 	}
 	bool result = false;
