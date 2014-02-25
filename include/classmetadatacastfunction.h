8c1eacb23b7fee5e72f1d89a6c8d3df1f0199464
overanalyzer1 <overanalyzer1@gmail.com>
2014-02-25 17:24:05 +0400

    Started implementing metadata class function

---
+++ include/classmetadatacastfunction.h
@@ -0,0 +1,63 @@
+/*! \file classmetadatacastfunction.h
+	\author HiddenSeeker
+
+	A function, that could perform user-defined cast from sad::Object from one real-type to sad::Object to other real-type.
+	Could be used to define getters or something
+ */
+#pragma once
+
+namespace sad
+{
+class Object;
+
+/*! A class, that could perform user-defined cast from sad::Object of one type
+ */
+class AbstractClassMetaDataCastFunction
+{
+public:
+	/*! Casts one object to another
+		\param[in] o a metadata function for class
+		\return other object, which was casted
+	 */
+	virtual sad::Object * cast(sad::Object * o) = 0;
+	/*! This class can be inherited to implement your own casts
+	 */
+	virtual ~AbstractClassMetaDataCastFunction();
+};
+
+/*! A cast function, which represents a method call on _SourceType, that returns a sad::Object, or inherited from it
+ */
+template<
+	typename _SourceType,
+	typename _Method
+>
+class ClassMetaDataCastMethod: public AbstractClassMetaDataCastFunction
+{
+public:
+	/*! Creates new method for casted
+		\parma[in] f a casted function
+	 */
+	ClassMetaDataCastMethod(_Method f) : m_f(f)
+	{
+
+	}
+	/*! Casts one object to another
+		\param[in] o a metadata function for class
+		\return other object, which was casted
+	 */
+	virtual sad::Object * cast(sad::Object * o)
+	{
+		return (static_cast<_SourceType*>(o)->*m_f)();
+	}
+	/*! This class can be inherited
+	 */
+	~ClassMetaDataCastMethod()
+	{
+	}
+protected:
+	/*! A called method for returning result
+	 */
+	_Method m_f;
+};
+
+}
