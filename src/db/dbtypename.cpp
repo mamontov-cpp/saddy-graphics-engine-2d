638ba884da4d6384e7be689793ae838991db1a49
overanalyzer1 <overanalyzer1@gmail.com>
2014-03-04 11:06:05 +0400

    Implemented sad::db::Save specs

--- src/db/dbtypename.cpp
+++ src/db/dbtypename.cpp
@@ -1,8 +1,14 @@
 #include "db/dbtypename.h"
+#include "db/save.h"
+#include "db/load.h"
 #include "sadstring.h"
 #include "sadpoint.h"
 #include "sadrect.h"
+#include "sadcolor.h"
+#include "sadsize.h"
 
+DECLARE_COMMON_TYPE(char)
+DECLARE_COMMON_TYPE(unsigned char)
 DECLARE_COMMON_TYPE(bool)
 DECLARE_COMMON_TYPE(int)
 DECLARE_COMMON_TYPE(unsigned int)
@@ -14,5 +20,8 @@ DECLARE_COMMON_TYPE(float)
 DECLARE_COMMON_TYPE(double)
 DECLARE_COMMON_TYPE(std::string)
 DECLARE_COMMON_TYPE(sad::String)
-DECLARE_COMMON_TYPE(sad::Rect2D)
 DECLARE_COMMON_TYPE(sad::Point2D)
+DECLARE_COMMON_TYPE(sad::Rect2D)
+DECLARE_COMMON_TYPE(sad::Color)
+DECLARE_COMMON_TYPE(sad::AColor)
+DECLARE_COMMON_TYPE(sad::Size2D)
