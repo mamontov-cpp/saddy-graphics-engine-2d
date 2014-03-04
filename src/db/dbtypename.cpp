8d3072c1f02b9fd15fdddf2f9508788614f721ea
overanalyzer1 <overanalyzer1@gmail.com>
2014-03-04 10:20:34 +0400

    Added typestrings for common types.

---
+++ src/db/dbtypename.cpp
@@ -0,0 +1,18 @@
+#include "db/dbtypename.h"
+#include "sadstring.h"
+#include "sadpoint.h"
+#include "sadrect.h"
+
+DECLARE_COMMON_TYPE(bool)
+DECLARE_COMMON_TYPE(int)
+DECLARE_COMMON_TYPE(unsigned int)
+DECLARE_COMMON_TYPE(long)
+DECLARE_COMMON_TYPE(unsigned long)
+DECLARE_COMMON_TYPE(long long)
+DECLARE_COMMON_TYPE(unsigned long long)
+DECLARE_COMMON_TYPE(float)
+DECLARE_COMMON_TYPE(double)
+DECLARE_COMMON_TYPE(std::string)
+DECLARE_COMMON_TYPE(sad::String)
+DECLARE_COMMON_TYPE(sad::Rect2D)
+DECLARE_COMMON_TYPE(sad::Point2D)
