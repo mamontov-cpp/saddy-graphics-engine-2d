51104aa9259b6462ec7a163703539a4fa7bdfef1
overanalyzer1 <overanalyzer1@gmail.com>
2014-03-25 15:35:03 +0400

    Added basic property.

---
+++ include/db/property.h
@@ -0,0 +1,32 @@
+/*! \file property.h
+	\author HiddenSeeker
+
+	Describes a basic property, which objects are consists of
+ */
+#pragma once
+#include "dbvariant.h"
+
+namespace sad
+{
+
+namespace db
+{
+
+/*! A basic property, which objects are consists of
+ */
+class Property
+{
+public:
+	/*! Constructs new property
+	 */
+	Property();
+
+	~P
+
+
+};
+
+}
+
+
+}
