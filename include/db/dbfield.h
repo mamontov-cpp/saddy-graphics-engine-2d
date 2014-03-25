51423a28c9a2a67f3741230e8655221070c14af3
overanalyzer1 <overanalyzer1@gmail.com>
2014-03-25 17:49:10 +0400

    Implemented method pair

---
+++ include/db/dbfield.h
@@ -0,0 +1,88 @@
+/*! \file   dbfield.h
+	\author HiddenSeeker
+	
+	Describes a field of class, as property
+ */
+#pragma once
+#include "dbproperty.h"
+#include <cassert>
+
+namespace sad
+{
+
+namespace db
+{
+
+/*! Describes a mapped field for a class
+ */
+template<
+	typename _Object,
+	typename _FieldTypeName
+>
+class Field: public sad::db::Property
+{
+public:
+	/*! Creates new field for a class
+		\param[in] o a field data
+	 */
+	Field(_FieldTypeName (_Object::*f)) : m_f(f)
+	{
+		
+	}
+	/*! A field data
+	 */
+	virtual ~Field()
+	{
+
+	}
+	/*! Sets a value for a property
+		\param[in] v a variant for property
+		\return whether value is set successfully
+	 */
+	virtual bool setValue(const sad::db::Variant & v)
+	{
+		assert( m_o );
+		sad::Maybe<_FieldTypeName> value = v.get<_FieldTypeName>();
+		bool result = false;
+		if (value.exists())
+		{
+			(static_cast<_Object*>(m_o)->*m_f)  = value.value();
+			result = true;
+		}
+		return result;
+	}
+	/*! Gets a value for a property
+		\return a value for a property
+	 */
+	virtual const sad::db::Variant & getValue() const
+	{
+		assert( m_o );
+		m_tmp.set( (static_cast<_Object*>(m_o)->*m_f) );
+		return m_tmp;
+	}
+
+protected:
+	/*! A temporary variant for returning property from field
+	 */
+	sad::Variant m_tmp;
+	/*! A field of class
+	 */
+	_FieldTypeName (_Object::*m_f);
+};
+
+/*! Returns a new field by valye
+	\param[in] f object
+	\return field
+ */
+template<
+	typename _Object,
+	typename _FieldTypeName
+>
+Field<_Object, _FieldTypeName> * define_field(_FieldTypeName (_Object::*f))
+{
+	return new Field<_Object, _FieldTypeName>(f);
+}
+
+}
+
+}
---
+++ include/db/dbmethodpair.h
@@ -0,0 +1,112 @@
+/*! \file   dbmethodpair.h
+	\author HiddenSeeker
+	
+	Describes a pair of methods, which could be used to work as property
+ */
+#pragma once
+#include "dbproperty.h"
+#include "../util/getterproxy.h"
+#include "../util/setterproxy.h"
+
+#include <cassert>
+
+namespace sad
+{
+
+namespace db
+{
+
+/*! Describes a pair of methods, which could be used to get and set data
+ */
+template<
+	typename _Object,
+	typename _FieldTypeName
+>
+class MethodPair
+{
+public:
+	/*! Setups a pair of methods
+		\param[in] g getter part
+		\param[in] s setter part
+	 */
+	inline MethodPair(
+		sad::util::getter::Proxy<_Object, _FieldTypeName> * g, 
+		sad::util::setter::Proxy<_Object, _FieldTypeName> * s
+	) : m_getter(g), m_setter(s)
+	{
+
+	}
+	/*! Sets a pair of methods
+		\param[in] g getter part
+		\param[in] s setter part
+	 */
+	template<
+		typename _Getter,
+		typename _Setter
+	>
+	inline MethodPair(
+		_Getter g, 
+		_Setter s
+	) 
+	{
+		m_getter = sad::util::define_getter<_Object, _FieldTypeName>(g);
+		m_setter = sad::util::define_setter<_Object, _FieldTypeName>(s);
+	}
+
+	/*! Setups a value for a property
+		\param[in] v a variant for property
+		\return whether value is set successfully
+	 */
+	virtual bool setValue(const sad::db::Variant & v)
+	{
+		assert( m_o );
+		sad::Maybe<_FieldTypeName> value = v.get<_FieldTypeName>();
+		bool result = false;
+		if (value.exists())
+		{
+			(static_cast<_Object*>(m_o)->*m_f)  = value.value();
+			result = true;
+		}
+		return result;
+	}
+	/*! Sets a value for a property
+		\param[in] v a variant for property
+		\return whether value is set successfully
+	 */
+	virtual bool setValue(const sad::db::Variant & v)
+	{
+		assert( m_o );
+		sad::Maybe<_FieldTypeName> value = v.get<_FieldTypeName>();
+		bool result = false;
+		if (value.exists())
+		{
+			m_setter->set(static_cast<_Object*>(m_o), value.value());
+			result = true;
+		}
+		return result;
+	}
+	/*! Gets a value for a property
+		\return a value for a property
+	 */
+	virtual const sad::db::Variant & getValue() const
+	{
+		assert( m_o );
+		_FieldTypeName v = m_getter->get(static_cast<_Object*>(m_o));
+		m_tmp.set( v );
+		return m_tmp;
+	}
+protected:
+	/*! A temporary variant for returning property from field
+	 */
+	sad::Variant m_tmp;
+	/*! A getter for method pair
+	 */
+	sad::util::getter::Proxy<_Object, _FieldTypeName> * m_getter;
+	/*! A setter for method pair
+	 */
+	sad::util::setter::Proxy<_Object, _FieldTypeName> * m_setter;
+};
+
+}
+
+}
