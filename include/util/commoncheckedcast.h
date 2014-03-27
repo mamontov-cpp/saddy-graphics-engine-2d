2e8e08b7c3489c392fd9c4973ed19b2b760d3d5e
HiddenSeeker <HiddenSeeker@localhost>
2014-03-27 14:47:46 +0400

    Added commoncheckedcast.h, which was forgotten

---
+++ include/util/commoncheckedcast.h
@@ -0,0 +1,63 @@
+/*! \file commoncheckedcast.h
+	\author HiddenSeeker
+
+	Defines a common checked cast for solving compile-time
+	problems with checked_cast if we trying to cast to POD.
+ */
+#pragma once
+#include "../maybe.h"
+#include "../object.h"
+#include "../sadstring.h"
+#include "../classmetadatacontainer.h"
+#include "../db/dbtypename.h"
+
+namespace sad
+{
+
+namespace util
+{
+
+/*! Defines a common checked cast for solving compile-time
+	problems with checked_cast if we trying to cast to POD.
+ */
+template<typename T, bool isSadObject>
+class CommonCheckedCast
+{
+public:
+	/*! Does nothing, since we don't have a conversion in common cast
+		\param[out] result converted value
+		\param[in] o object
+		\param[in] _typename a typename
+	 */
+	static void perform(Maybe<T> & v, void * o, const sad::String & _typename)
+	{
+		
+	}
+};
+
+/*! Defines a common checked cast case for solving problems with casting to a type.
+ */
+template<typename T>
+class CommonCheckedCast<T, true>
+{
+public:
+	/*! Does nothing, since we don't have a conversion in common cast
+		\param[out] result converted value
+		\param[in] o object
+		\param[in] _typename a typename
+	 */
+	static void perform(Maybe<T> & result, void * object, const sad::String & _typename)
+	{
+		bool created = false;
+		if (sad::ClassMetaDataContainer::ref()->get(_typename, created)->canBeCastedTo(sad::db::TypeName<T>::Name) == true)
+		{
+			sad::Object * o = (sad::Object*)object;
+			result.setValue(*sad::checked_cast<T, sad::Object>(o));
+		}
+	}
+};
+
+
+}
+
+}
