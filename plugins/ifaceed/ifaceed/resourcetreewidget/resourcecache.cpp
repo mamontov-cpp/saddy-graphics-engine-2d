18e3345f83806e0ba7645787354aaae3c6970864
HiddenSeeker <HiddenSeeker@localhost>
2014-04-06 14:13:11 +0400

    Started implementing resource cache.

---
+++ plugins/ifaceed/ifaceed/resourcetreewidget/resourcecache.cpp
@@ -0,0 +1,45 @@
+#include "resourcetreewidget/resourcecache.h"
+#include "resourcetreewidget/resourcetreewidget.h"
+
+#include <cassert>
+
+resourcetreewidget::ResourceCache::ResourceCache() : m_parent(NULL)
+{
+	
+}
+
+resourcetreewidget::ResourceCache::~ResourceCache()
+{
+	
+}
+
+void resourcetreewidget::ResourceCache::setParent(ResourceTreeWidget * parent)
+{
+	m_parent = parent;
+	m_resource_cache.clear();
+}
+
+ResourceTreeWidget* resourcetreewidget::ResourceCache::parent() const
+{
+	return m_parent;
+}
+
+const QImage& resourcetreewidget::ResourceCache::imageForResource(const QString & resourcename)
+{
+	// A name for resource
+	if (m_resource_cache.contains(resourcename))
+	{
+		return m_resource_cache[resourcename];
+	}
+	// Try fetch result
+	QImage result;
+	this->createDefaultImage(result);
+	m_resource_cache.insert(resourcename, result);
+	return result;
+}
+
+
+void resourcetreewidget::ResourceCache::createDefaultImage(QImage & im)
+{
+	
+}
