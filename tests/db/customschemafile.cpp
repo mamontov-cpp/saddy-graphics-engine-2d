f1d5c768930f65060bad73285f25ece0ff18ff8d
HiddenSeeker <HiddenSeeker@localhost>
2014-08-01 23:13:36 +0400

    Fixed bug, when reloading does not reset links to resource.

--- tests/db/customschemafile.cpp
+++ tests/db/customschemafile.cpp
@@ -34,7 +34,10 @@ struct SadDbCustomSchemaFileTest : tpunit::TestFixture
 		TEST(SadDbCustomSchemaFileTest::test_load_duplicate_name),
 		TEST(SadDbCustomSchemaFileTest::test_load_resource_name_exists),
 		TEST(SadDbCustomSchemaFileTest::test_load_missing_resource),
-		TEST(SadDbCustomSchemaFileTest::test_load_valid)
+		TEST(SadDbCustomSchemaFileTest::test_load_valid),
+		TEST(SadDbCustomSchemaFileTest::test_reload_not_exists),
+		TEST(SadDbCustomSchemaFileTest::test_reload_cannot_delete_referenced_resource),
+		TEST(SadDbCustomSchemaFileTest::test_reload_valid)
    ) {}
 
    void test_load_not_exists()
@@ -403,6 +406,138 @@ struct SadDbCustomSchemaFileTest : tpunit::TestFixture
 
 	   link1.setPath("myschema");
 	   ASSERT_TRUE(link1.get() != NULL);
+   }
+
+
+   void test_reload_not_exists()
+   {
+	    sad::Renderer r;
+	   sad::resource::Tree tree;
+	   tree.setStoreLinks(true);
+	   tree.setRenderer(&r);
+
+	   sad::Vector<sad::resource::Error *> errors = tree.loadFromString(
+		   "["
+				"{"
+				    "\"type\"   : \"sad::resource::TextureAtlasFile\","
+					"\"filename\": \"tests/icons.json\""
+				"},"
+				"{"
+					"\"type\"   : \"sad::db::custom::SchemaFile\","
+					"\"filename\": \"tests/db/valid.json\""
+				"}"
+			"]"
+		);
+	   int count = (int)(errors.size());
+	   sad::util::free(errors);
+	   ASSERT_TRUE(count == 0);
+
+	   sad::resource::Link<sad::db::custom::Schema> link1;
+	   link1.setPath("myschema");
+	   link1.setTree(&tree);
+	   sad::resource::Resource* old = link1.get();
+	   ASSERT_TRUE(old != NULL);
+
+	   rename("tests/db/valid.json", "tests/db/valid.json.tmp");
+	   errors = old->file()->reload(); 
+	   count = (int)(errors.size());
+	   sad::util::free(errors);
+	   rename("tests/db/valid.json.tmp", "tests/db/valid.json");
+	   ASSERT_TRUE(count != 0);
+	   ASSERT_TRUE(link1.get() == old);
+   }
+
+
+   void test_reload_cannot_delete_referenced_resource()
+   {
+	   sad::Renderer r;
+	   sad::resource::Tree tree;
+	   tree.setStoreLinks(true);
+	   tree.setRenderer(&r);
+
+	   sad::Vector<sad::resource::Error *> errors = tree.loadFromString(
+		   "["
+				"{"
+				    "\"type\"   : \"sad::resource::TextureAtlasFile\","
+					"\"filename\": \"tests/icons.json\""
+				"},"
+				"{"
+					"\"type\"   : \"sad::db::custom::SchemaFile\","
+					"\"filename\": \"tests/db/valid.json\""
+				"}"
+			"]"
+		);
+	   int count = (int)(errors.size());
+	   sad::util::free(errors);
+	   ASSERT_TRUE(count == 0);
+
+	   sad::resource::Link<sad::db::custom::Schema> link1;
+	   link1.setPath("myschema");
+	   link1.setTree(&tree);
+	   sad::resource::Resource* old = link1.get();
+	   ASSERT_TRUE(old != NULL);
+
+	   rename("tests/db/valid.json", "tests/db/valid.json.tmp");
+	   rename("tests/db/cannotdeletereference.json", "tests/db/valid.json");
+	   errors = old->file()->reload(); 
+	   count = (int)(errors.size());
+	   sad::util::free(errors);
+	   rename("tests/db/valid.json", "tests/db/cannotdeletereference.json");
+	   rename("tests/db/valid.json.tmp", "tests/db/valid.json");
+	   ASSERT_TRUE(count != 0);
+	   ASSERT_TRUE(link1.get() == old);
+   }
+
+
+   int test;
+
+   void inc()
+   {
+	   test++;
+   }
+
+   void test_reload_valid()
+   {
+	   sad::Renderer r;
+	   sad::resource::Tree tree;
+	   tree.setStoreLinks(true);
+	   tree.setRenderer(&r);
+
+	   sad::Vector<sad::resource::Error *> errors = tree.loadFromString(
+		   "["
+				"{"
+				    "\"type\"   : \"sad::resource::TextureAtlasFile\","
+					"\"filename\": \"tests/icons.json\""
+				"},"
+				"{"
+					"\"type\"   : \"sad::db::custom::SchemaFile\","
+					"\"filename\": \"tests/db/valid.json\""
+				"}"
+			"]"
+		);
+	   int count = (int)(errors.size());
+	   sad::util::free(errors);
+	   ASSERT_TRUE(count == 0);
+
+	   sad::resource::Link<sad::db::custom::Schema> link1;
+	   link1.setPath("myschema");
+	   link1.setTree(&tree);
+	   sad::resource::Resource* old = link1.get();
+	   ASSERT_TRUE(old != NULL);
+
+	   link1.add(this, &SadDbCustomSchemaFileTest::inc);
+	   test = 0;
+
+	   rename("tests/db/valid.json", "tests/db/valid.json.tmp");
+	   rename("tests/db/valid2.json", "tests/db/valid.json");
+	   errors = old->file()->reload(); 
+	   count = (int)(errors.size());
+	   sad::util::free(errors);
+	   rename("tests/db/valid.json", "tests/db/valid2.json");
+	   rename("tests/db/valid.json.tmp", "tests/db/valid.json");
+	   ASSERT_TRUE(count == 0);
+	   ASSERT_TRUE(link1.get() != old);
+	   ASSERT_TRUE(test != 0);
    }
 
 } _sad_db_custom_schema_file_test;
