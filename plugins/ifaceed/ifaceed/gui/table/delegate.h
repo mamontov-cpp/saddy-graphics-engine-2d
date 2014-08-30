39c0589b3f8de8050d1a2c02e2261335e339eeac
HiddenSeeker <HiddenSeeker@localhost>
2014-08-30 14:14:15 +0400

    Added 64-bit  spinboxes to editor, taken from other lib.

--- plugins/ifaceed/ifaceed/gui/table/delegate.h
+++ plugins/ifaceed/ifaceed/gui/table/delegate.h
@@ -12,6 +12,9 @@
 #include <db/dbvariant.h>
 #include <db/save.h>
 #include <db/load.h>
+#include <db/dbdatabase.h>
+
+#include <renderer.h>
 
 
 namespace core
@@ -65,14 +68,15 @@ public:
 	const QString& propertyName() const;
 	/*! Adds a delegate to table
 	 */
-	virtual void add() = 0;
-	/*! Removes a delegate from table
-	 */
-	virtual void remove() = 0;
+	virtual void add();
 	/*! Sets a value for a delegate. Used by commands to reset
 		\param[in] v a value
 	 */
 	virtual void set(const sad::db::Variant& v) = 0;
+public slots:
+	/*! Removes a delegate from table
+	 */
+	virtual void remove();
 protected:
 	/*! A name of property (for database or custom object)
 	 */
@@ -89,6 +93,63 @@ protected:
 	/*! A local widget for delegate for setting value
 	 */
 	QWidget* m_my_widget;
+	/*! A row, where element should be inserted
+	 */
+	int m_row;
+	/*! At this point delegate must create own editor, set it to table and connect delegate slots
+		to it.
+	 */
+	virtual void makeEditor() = 0;
+	/*! Disconnects slots  for own editor here
+	 */ 
+	virtual void disconnect() = 0;
+	/*! Fetches current value of property from database and other stuff
+		\return current value
+	 */
+	template<
+		typename T
+	>
+	T currentValue() const
+	{
+		if (this->isLinkedToDatabase())
+		{
+			return sad::Renderer::ref()
+			->database("")
+			->getProperty<T>(this->propertyName().toStdString()).value();
+		}
+		else
+		{
+			// TODO: Implement for custom object
+		}
+		return T();
+	}
+	/*! Sets current value for property
+		\param[in] o object
+	 */
+	template<
+		typename T
+	>
+	void setCurrentValue(const T & o)
+	{
+		if (this->isLinkedToDatabase())
+		{
+			sad::Renderer::ref()
+			->database("")
+			->setProperty<signed char>(this->propertyName().toStdString(), o);
+		}
+		else
+		{
+			// TODO: Implement for custom object
+		}
+	}
+	/*! Finds property in database table widget
+		\return row index
+	 */
+	int findPropertyInDatabase();
+	/*! Inserts linked widget to table. MUST be called inside ::createWidget to make sure, that
+		widget is inserted to table
+	 */
+	void insertToTable();
 };
 
 }
