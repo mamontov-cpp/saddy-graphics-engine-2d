8b21bd4f26c3f62ad1a5d441117ac8f608e52e7e
HiddenSeeker <HiddenSeeker@localhost>
2014-09-18 23:13:51 +0400

    Started reimplementing rotation process

---
+++ plugins/ifaceed/ifaceed/gui/rotationprocess.cpp
@@ -0,0 +1,12 @@
+#include "rotationprocess.h"
+
+gui::RotationProcess::RotationProcess()
+: m_node(NULL), m_history(NULL)
+{
+	
+}
+
+gui::RotationProcess::~RotationProcess()
+{
+	
+}
---
+++ plugins/ifaceed/ifaceed/history/scenenodes/scenenodeschangeangle.h
@@ -0,0 +1,39 @@
+/*! \file scenesnodeschangeangle.h
+    \author HiddenSeeker
+
+    Describe a command, which change angle of scene node
+ */
+#pragma once
+#include "scenenodeschangeproperty.h"
+
+namespace history
+{
+
+namespace scenenodes
+{
+/*! A command, which change angle of scene node
+ */
+class ChangeAngle: public history::scenenodes::ChangeProperty<float>
+{
+public:
+     /*! Constructs new command for node
+        \param[in] d a node
+        \param[in] oldvalue old text
+        \param[in] newvalue new text
+      */
+    ChangeAngle(sad::SceneNode* d, float oldvalue, float newvalue);
+    /*! Erases link to a node
+      */
+    virtual ~ChangeAngle();
+protected:
+    /*!
+     * Updates current text in field of object name
+     * \param[in] e editor
+     * \param[in] value a value
+     */
+    virtual void updateUI(core::Editor* e, const float& value);
+};
+
+}
+
+}
