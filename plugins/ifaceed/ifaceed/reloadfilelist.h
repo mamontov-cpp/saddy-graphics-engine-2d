1d62736c09f05ffc0a3abb78165b4456b7c0a41a
HiddenSeeker <HiddenSeeker@localhost>
2014-10-06 22:15:32 +0400

    First try to implement reloading resources.

---
+++ plugins/ifaceed/ifaceed/reloadfilelist.h
@@ -0,0 +1,35 @@
+/*! \file reloadfilelist.h
+	\author HiddenSeeker
+
+	Describes a dialog for picking file for reloading
+ */
+#pragma once
+
+#include <QDialog>
+#include <QString>
+
+#include "ui_reloadfilelist.h"
+
+#include <resource/physicalfile.h>
+
+/*! A dialog for picking color with alpha
+ */
+class ReloadFileList: public QDialog
+{
+Q_OBJECT
+public:
+	/*! Creates new file list for reloading
+		\param[in] parent a parent widget for selecting
+	 */
+    ReloadFileList(QWidget* parent = NULL);
+	/*! Returns a list of selected file
+	 */
+	sad::resource::PhysicalFile* selectedFile();
+	/*! Could be inherited
+	 */
+	virtual ~ReloadFileList();
+protected:
+	/*! UI for reloading file list
+	 */
+	Ui::ReloadFileList  m_ui;
+};
