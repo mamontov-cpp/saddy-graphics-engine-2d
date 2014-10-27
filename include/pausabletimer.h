ac93a1e5de981c3c0c7ecc12adbb840d7056576d
overanalyzer1 <overanalyzer1@gmail.com>
2014-10-27 16:32:13 +0300

    Implemented and tested pausable timer

---
+++ include/pausabletimer.h
@@ -0,0 +1,55 @@
+/*! \file pausabletimer.h
+	\author HiddenSeeker
+
+	Defines a timer, which could be paused
+ */
+#pragma once
+#include "timer.h"
+
+namespace sad
+{
+
+/*! A timer, which could be paused and resumed. 
+	The time, between pause() and resume() calls will not be counted in elapsed
+ */
+class PausableTimer
+{
+public:
+	/*! Constructs default pausable timer
+	 */
+	PausableTimer();
+	/*! Implemented mainly for safety reasons, doesn't do anything
+	 */
+	~PausableTimer();
+	/*! Starts new iteration of timer, erasing local elapsed time
+	 */
+	void start();
+	/*! Stops timer, ending iteration for timer
+	 */
+	void stop();
+	/*! Pauses timer, saving local elapsed time
+	 */
+	void pause();
+	/*! Resumes timer, working to next iteration
+	 */
+	void resume();
+	/*! Returns elapsed time, between calls of sad::Timer::start() and
+		sad::Timer::stop() using high-perfomance counter, if possible. For
+		some chipsets could fall to wallclock time, since correct information
+		won't be available
+		\return time in milliseconds
+	 */
+	double elapsed() const;
+protected:
+	/*! Whether timer is running
+	 */
+	bool m_running;
+	/*! A local elapsed time, used for pause
+	 */
+	double m_elapsed;
+	/*! A local timer for measuring time
+	 */
+	sad::Timer m_timer;
+};
+
+}
---
+++ src/pausabletimer.cpp
@@ -0,0 +1,56 @@
+#include "pausabletimer.h"
+
+sad::PausableTimer::PausableTimer() 
+: m_elapsed(0), m_running(false)
+{
+
+}
+
+sad::PausableTimer::~PausableTimer() 
+{
+
+}
+
+void sad::PausableTimer::start()
+{
+	m_elapsed = 0;
+	m_timer.start();
+	m_running = true;
+}
+
+void sad::PausableTimer::stop()
+{
+	m_timer.stop();
+	m_running = false;
+}
+
+
+void sad::PausableTimer::pause()
+{
+	m_timer.stop();
+	m_elapsed += m_timer.elapsed();
+	m_running = false;
+}
+
+void sad::PausableTimer::resume()
+{
+	m_timer.start();
+	m_running = true;
+}
+
+double sad::PausableTimer::elapsed() const
+{
+	double elapsed = 0;
+	sad::PausableTimer* me = const_cast<sad::PausableTimer*>(this);
+	if (m_running)
+	{
+		me->m_timer.stop();
+		me->m_elapsed += m_timer.elapsed();
+		me->m_timer.start();
+	}
+	else
+	{
+		elapsed = m_timer.elapsed();
+	}
+	return m_elapsed + elapsed;
+}
