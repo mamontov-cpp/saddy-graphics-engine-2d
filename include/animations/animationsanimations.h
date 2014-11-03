193f07dacf9d11cd59bd21f2248529b0e22567ba
HiddenSeeker <HiddenSeeker@localhost>
2014-11-03 12:02:55 +0400

    Implemented animations pipeline step.

---
+++ include/animations/animationsanimations.h
@@ -0,0 +1,55 @@
+/*! \file animationanimations.h
+	\author HiddenSeeker
+
+	Defines an animations as pipeline step
+ */
+#pragma once
+
+#include "../temporarilyimmutablecontainer.h"
+#include "../sadvector.h"
+
+#include "../pipeline/pipelineprocess.h"
+
+#include "animationsprocess.h"
+
+
+namespace sad
+{
+	
+namespace animations
+{
+
+/*! An animations, as a pipeline rendering step
+ */
+class Animations: public sad::pipeline::AbstractProcess, 
+public sad::TemporarilyImmutableContainer<sad::animations::Process>
+{
+public:
+	/*! Creates new empty animations list
+	 */
+	Animations();
+	/*! Removes links to processes
+	 */
+	virtual ~Animations();
+protected:
+	/*! Runs every process
+	 */ 
+	virtual void _process();
+	/*! Adds an animation process to list
+		\param[in] o object
+	 */
+	void addNow(sad::animations::Process* o);
+	/*! Removes an animation process to list
+	 */
+	void removeNow(sad::animations::Process* o);
+	/*! Clears an animation list
+	 */
+	void clearNow();
+	/*! A list of animations processes
+	 */
+	sad::Vector<sad::animations::Process*> m_list;
+};
+
+}
+
+}
