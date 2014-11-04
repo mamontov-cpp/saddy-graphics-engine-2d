c04704a3c953295b8c6e78cd2f9e100df45f8f2d
HiddenSeeker <HiddenSeeker@localhost>
2014-11-04 14:21:19 +0400

    Implemented texture coordinates animations.

--- include/animations/animationstexturecoordinateslist.h
+++ include/animations/animationstexturecoordinateslist.h
@@ -1,13 +1,14 @@
-/*! \file animations/animationsfontlist.h
+/*! \file animations/animationstexturecoordinateslist.h
 	\author HiddenSeeker
 
-	An animations as animation of changing fonts by time
+	An animations as animation of changing texture coordinates lists by time
  */
 #pragma once
 
 #include "../fuzzyequal.h"
 #include "../sadvector.h"
 #include "../sadstring.h"
+#include "../sadhash.h"
 
 #include "animationsanimation.h"
 
@@ -18,31 +19,31 @@ namespace sad
 namespace animations
 {
 
-/*! An animation, which changes font of label
+/*! An animation, which changes texture coordinates of sprite over time
  */
-class FontList: public sad::animations::Animation
+class TextureCoordinatesList: public sad::animations::Animation
 {
 SAD_OBJECT
 public:
 	/*! Creates new empty animation
 	 */
-	FontList();
+	TextureCoordinatesList();
 	/*! Can be inherited
 	 */
-	virtual ~FontList();
+	virtual ~TextureCoordinatesList();
     /*! Tries to load animation from value
         \param[in] v value
         \return whether it was successfull
      */
     virtual bool loadFromValue(const picojson::value& v);
-	/*! Sets list of fonts
+	/*! Sets list of options, which defines texture coordinate sources
 		\param[in] fonts a font list, used in animation
 	 */
-	void setFonts(const sad::Vector<sad::String>& fonts);
+	void setList(const sad::Vector<sad::String>& list);
 	/*! Returns font list
 		\return font list
 	 */
-	const sad::Vector<sad::String> & fonts() const;
+	const sad::Vector<sad::String> & list() const;
 	/*! Sets state of object from animation
         \param[in] o object
         \param[in] time a time of playing of animation
@@ -58,9 +59,23 @@ public:
      */
     virtual void resetState(sad::animations::Instance* i);
 protected:
-	/*! A list of fonts to be set in animation
+	/*! Tries to fetch coordinates from cache
+		\param[in] c coordinates
+		\return NULL if not found, otherwise rectangle
+	 */ 
+	sad::Rect2D* coordinates(const sad::String& c);
+	/*! A texture coordinates cache
 	 */
-	sad::Vector<sad::String> m_fonts;
+	sad::Hash<sad::String, sad::Rect2D> m_cache;
+	/*! A cached root folder for faster finding options with texture coordinates
+	 */
+	sad::resource::Folder* m_cache_root_folder;
+	/*! A folder, for which cache is being created
+	 */
+	sad::resource::Folder* m_cache_folder;
+	/*! A list of texture coordinates to be set
+	 */
+	sad::Vector<sad::String> m_list;
 };
 
 }
