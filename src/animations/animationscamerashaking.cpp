309faf4c0f0c421cfa91b374ffbfc628abd21194
HiddenSeeker <HiddenSeeker@localhost>
2014-11-06 10:25:37 +0300

    Updated MinGW build

--- src/animations/animationscamerashaking.cpp
+++ src/animations/animationscamerashaking.cpp
@@ -1,9 +1,9 @@
 #include "animations/animationscamerashaking.h"
 #include "animations/animationsinstance.h"
 
-#include "../fuzzyequal.h"
-#include "../scene.h"
-#include "../camera.h"
+#include "fuzzyequal.h"
+#include "scene.h"
+#include "camera.h"
 
 #include "db/schema/schema.h"
 #include "db/dbproperty.h"
