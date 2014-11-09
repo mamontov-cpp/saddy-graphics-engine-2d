9c9f2e5ce40bfc7dc94e40f8ccc0bf11c0b9b9ac
HiddenSeeker <HiddenSeeker@localhost>
2014-11-09 17:31:45 +0400

    Implemented animation factory.

---
+++ src/animations/animationfactory.cpp
@@ -0,0 +1,88 @@
+#include "animations/animationfactory.h"
+#include "animations/animationsblinking.h"
+#include "animations/animationscamerarotation.h"
+#include "animations/animationscamerashaking.h"
+#include "animations/animationscolor.h"
+#include "animations/animationsfontlist.h"
+#include "animations/animationsfontsize.h"
+#include "animations/animationsoptionlist.h"
+#include "animations/animationsresize.h"
+#include "animations/animationsrotate.h"
+#include "animations/animationstyping.h"
+#include "animations/animationswaymoving.h"
+#include "animations/animationstexturecoordinateslist.h"
+
+// ======================== PUBLIC METHODS of sad::animations::Factory::AbstractDelegate ========================
+
+sad::animations::Factory::AbstractDelegate::~AbstractDelegate()
+{
+	
+}
+
+// ======================== PUBLIC METHODS of sad::animations::Factory ========================
+
+sad::animations::Factory::Factory()
+{
+	push<sad::animations::Blinking>("sad::animations::Blinking");
+	push<sad::animations::CameraRotation>("sad::animations::CameraRotation");
+	push<sad::animations::CameraShaking>("sad::animations::CameraShaking");
+	push<sad::animations::Color>("sad::animations::Color");
+	push<sad::animations::FontList>("sad::animations::FontList");
+	push<sad::animations::FontSize>("sad::animations::FontSize");
+	push<sad::animations::OptionList>("sad::animations::OptionList");
+	push<sad::animations::Resize>("sad::animations::Resize");
+	push<sad::animations::Rotate>("sad::animations::Rotate");
+	push<sad::animations::Typing>("sad::animations::Typing");
+	push<sad::animations::WayMoving>("sad::animations::WayMoving");
+
+}
+
+sad::animations::Factory::Factory(const sad::animations::Factory& f)
+{
+	copy(f);
+}
+
+sad::animations::Factory& sad::animations::Factory::operator=(const sad::animations::Factory& f)
+{
+	clear();
+	copy(f);
+	return *this;
+}
+
+sad::animations::Factory::~Factory()
+{
+	
+}
+
+sad::animations::Animation* sad::animations::Factory::create(const sad::String& s)
+{
+	if (m_delegates.contains(s))
+	{
+		return m_delegates[s]->create();
+	}
+	return NULL;
+}
+
+void sad::animations::Factory::clear()
+{
+	for(sad::PtrHash<sad::String, sad::animations::Factory::AbstractDelegate>::const_iterator it = m_delegates.const_begin();
+		it != m_delegates.const_end();
+		++it)
+	{
+		delete it.value();
+	}
+	m_delegates.clear();
+}
+
+// ======================== PROTECTED METHODS of sad::animations::Factory  ========================
+
+void sad::animations::Factory::copy(const sad::animations::Factory& f)
+{
+	for(sad::PtrHash<sad::String, sad::animations::Factory::AbstractDelegate>::const_iterator it = f.m_delegates.const_begin();
+		it != f.m_delegates.const_end();
+		++it)
+	{
+		m_delegates.insert(it.key(), it.value()->clone());
+	}
+}
+
