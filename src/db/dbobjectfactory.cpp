24989c870cb4960f74b1b069895b23e343aa892f
HiddenSeeker <HiddenSeeker@localhost>
2014-11-12 10:49:35 +0300

    Added animations to list of created stuff in database factory

--- src/db/dbobjectfactory.cpp
+++ src/db/dbobjectfactory.cpp
@@ -16,7 +16,20 @@
 #include "animations/animationsinstance.h"
 #include "animations/animationswayinstance.h"
 #include "animations/animationsgroup.h"
-
+#include "animations/animationsblinking.h"
+#include "animations/animationscamerarotation.h"
+#include "animations/animationscamerashaking.h"
+#include "animations/animationscolor.h"
+#include "animations/animationsfontlist.h"
+#include "animations/animationsfontsize.h"
+#include "animations/animationsoptionlist.h"
+#include "animations/animationsresize.h"
+#include "animations/animationsrotate.h"
+#include "animations/animationsparallel.h"
+#include "animations/animationssequential.h"
+#include "animations/animationstyping.h"
+#include "animations/animationswaymoving.h"
+#include "animations/animationstexturecoordinateslist.h"
 
 sad::db::ObjectFactory::AbstractDelegate::~AbstractDelegate()
 {
@@ -25,17 +38,38 @@ sad::db::ObjectFactory::AbstractDelegate::~AbstractDelegate()
 
 sad::db::ObjectFactory::ObjectFactory()
 {
-	add<sad::Label>("sad::Label", sad::Label::basicSchema(), false);
+    // A scene objects
+    add<sad::Label>("sad::Label", sad::Label::basicSchema(), false);
 	add<sad::Scene>("sad::Scene", sad::Scene::basicSchema(), false);
 	add<sad::Sprite2D>("sad::Sprite2D", sad::Sprite2D::basicSchema(), false);
 	add<sad::Sprite3D>("sad::Sprite3D", sad::Sprite3D::basicSchema(), false);
-	// Custom object has no schema at all
+
+    // Custom object has no schema at all
 	add<sad::db::custom::Object>("sad::db::custom::Object", NULL, false);
-	add<sad::p2d::app::Way>("sad::p2d::app::Way", sad::p2d::app::Way::basicSchema(), false);
-	add<sad::dialogue::Dialogue>("sad::dialogue::Dialogue", sad::dialogue::Dialogue::basicSchema(), false);
+
+    add<sad::p2d::app::Way>("sad::p2d::app::Way", sad::p2d::app::Way::basicSchema(), false);
+
+    add<sad::dialogue::Dialogue>("sad::dialogue::Dialogue", sad::dialogue::Dialogue::basicSchema(), false);
+
+    // Animation instance primitives
     add<sad::animations::Instance>("sad::animations::Instance", sad::animations::Instance::basicSchema(), false);
     add<sad::animations::WayInstance>("sad::animations::WayInstance", sad::animations::WayInstance::basicSchema(), false);
 	add<sad::animations::Group>("sad::animations::Group", sad::animations::Group::basicSchema(), false);
+
+    // A types of animations
+    add<sad::animations::Blinking>("sad::animations::Blinking", sad::animations::Blinking::basicSchema(), false);
+    add<sad::animations::CameraRotation>("sad::animations::CameraRotation", sad::animations::CameraRotation::basicSchema(), false);
+    add<sad::animations::CameraShaking>("sad::animations::CameraShaking", sad::animations::CameraShaking::basicSchema(), false);
+    add<sad::animations::Color>("sad::animations::Color", sad::animations::Color::basicSchema(), false);
+    add<sad::animations::FontList>("sad::animations::FontList", sad::animations::FontList::basicSchema(), false);
+    add<sad::animations::FontSize>("sad::animations::FontSize", sad::animations::FontSize::basicSchema(), false);
+    add<sad::animations::OptionList>("sad::animations::OptionList", sad::animations::OptionList::basicSchema(), false);
+    add<sad::animations::Parallel>("sad::animations::Parallel", sad::animations::Parallel::basicSchema(), false);
+    add<sad::animations::Resize>("sad::animations::Resize", sad::animations::Resize::basicSchema(), false);
+    add<sad::animations::Rotate>("sad::animations::Rotate", sad::animations::Rotate::basicSchema(), false);
+    add<sad::animations::Sequential>("sad::animations::Sequential", sad::animations::Sequential::basicSchema(), false);
+    add<sad::animations::Typing>("sad::animations::Typing", sad::animations::Typing::basicSchema(), false);
+    add<sad::animations::WayMoving>("sad::animations::WayMoving", sad::animations::WayMoving::basicSchema(), false);
 }
 
 void sad::db::ObjectFactory::add(
