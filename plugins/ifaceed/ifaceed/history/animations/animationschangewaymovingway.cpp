ddf5d50139343ea01120f68d6109aa02cfc9583d
HiddenSeeker <HiddenSeeker@localhost>
2015-01-04 14:24:24 +0400

    Implemented changing way in way moving animation

---
+++ plugins/ifaceed/ifaceed/history/animations/animationschangewaymovingway.cpp
@@ -0,0 +1,48 @@
+#include "animationschangewaymovingway.h"
+
+#include "../../core/editor.h"
+
+#include "../../mainpanel.h"
+
+#include "../../closuremethodcall.h"
+#include "../../blockedclosuremethodcall.h"
+
+Q_DECLARE_METATYPE(sad::p2d::app::Way*)
+
+history::animations::ChangeWayMovingWay::ChangeWayMovingWay(
+    sad::animations::Animation* d,
+    unsigned long long oldvalue,
+    unsigned long long newvalue
+
+)
+: history::animations::ChangeProperty<unsigned long long>(
+      d,
+      "way",
+      oldvalue,
+      newvalue
+)
+{
+}
+
+history::animations::ChangeWayMovingWay::~ChangeWayMovingWay()
+{
+
+}
+
+void history::animations::ChangeWayMovingWay::performUpdateUI(core::Editor* e, unsigned long long value)
+{
+	MainPanel* panel = e->panel();
+	QComboBox* list = panel->UI()->cmbWayAnimationWay;
+	invoke_blocked(list, &QComboBox::setCurrentIndex,  panel->findInComboBoxByMajorId<sad::p2d::app::Way*>(list,value));
+}
+
+void history::animations::ChangeWayMovingWay::updateUI(core::Editor* e, const unsigned long long& value)
+{
+    e->emitClosure( bind(
+            this,
+            &history::animations::ChangeWayMovingWay::performUpdateUI,
+            e,
+			value
+        )
+    );
+}
---
+++ plugins/ifaceed/ifaceed/history/animations/animationschangewaymovingway.h
@@ -0,0 +1,45 @@
+/*! \file animationschangewaymovingway.h
+    \author HiddenSeeker
+
+    Describes a command, which changes way for WayMoving animation
+ */
+#pragma once
+#include "animationschangeproperty.h"
+
+namespace history
+{
+
+namespace animations
+{
+/*! A command,which changes ay for WayMoving animation
+ */
+class ChangeWayMovingWay: public history::animations::ChangeProperty<unsigned long long>
+{
+public:
+     /*! Constructs new command for animation
+        \param[in] d an animation
+        \param[in] oldvalue old value
+        \param[in] newvalue new value
+      */
+    ChangeWayMovingWay(sad::animations::Animation* d, unsigned long long oldvalue, unsigned long long newvalue);
+    /*! Erases link to an animation
+      */
+    virtual ~ChangeWayMovingWay();
+protected:
+	/*!
+     * Updates current text in field with object name
+     * \param[in] e editor
+     * \param[in] value a value
+     */
+	void performUpdateUI(core::Editor* e, unsigned long long value);
+    /*!
+     * Updates current text in field with object name
+     * \param[in] e editor
+     * \param[in] value a value
+     */
+    virtual void updateUI(core::Editor* e, const unsigned long long& value);
+};
+
+}
+
+}
