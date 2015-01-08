d8af532133c4acb2992d233f79bae05ac1469229
HiddenSeeker <HiddenSeeker@localhost>
2015-01-08 20:35:54 +0400

    Implemented consistency check when adding scene node

--- plugins/ifaceed/ifaceed/history/scenenodes/scenenodesnew.cpp
+++ plugins/ifaceed/ifaceed/history/scenenodes/scenenodesnew.cpp
@@ -4,6 +4,10 @@
 
 #include "../../mainpanel.h"
 
+#include "../../closuremethodcall.h"
+
+Q_DECLARE_METATYPE(sad::db::Object*)
+
 history::scenenodes::New::New(sad::SceneNode* d) : m_node(d)
 {
 	m_node->addRef();
@@ -20,6 +24,19 @@ history::scenenodes::New::~New()
 	if (m_node->scene() == ob->panel()->currentScene())
 	{
 		ob->panel()->addSceneNodeToSceneNodeList(m_node);
+	}
+
+	QVariant v;
+	v.setValue(static_cast<sad::db::Object*>(m_node));
+	if(ob)
+	{
+		void (QComboBox::*f)(const QString&, const QVariant&) = &QComboBox::addItem;
+		ob->emitClosure( bind(
+			ob->panel()->UI()->cmbAnimationInstanceObject,
+			f,
+			ob->panel()->fullNameForNode(m_node),
+			v
+		));
 	}
 }
 
@@ -38,4 +55,19 @@ history::scenenodes::New::~New()
 			ob->machine()->enterState("idle");
 		}
 	}
+	ob->emitClosure( bind(
+			this,
+			&history::scenenodes::New::eraseFromAnimationObjectsCombo,
+			ob
+	));
 }
+
+void history::scenenodes::New::eraseFromAnimationObjectsCombo(core::Editor* e)
+{
+	int pos  = e->panel()->findInComboBox<sad::db::Object*>(e->panel()->UI()->cmbAnimationInstanceObject, m_node);
+	if (pos > -1)
+	{
+		e->panel()->UI()->cmbAnimationInstanceObject->removeItem(pos);
+	}
+}
+
