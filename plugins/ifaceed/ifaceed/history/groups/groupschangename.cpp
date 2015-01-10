8cd3a7685d6f4e96827e41ac24600b834ef46498
HiddenSeeker <HiddenSeeker@localhost>
2015-01-10 15:11:41 +0400

    Implemented editing name and looped for animation group

---
+++ plugins/ifaceed/ifaceed/history/groups/groupschangename.cpp
@@ -0,0 +1,53 @@
+#include "groupschangename.h"
+
+#include "../../core/editor.h"
+
+#include "../../mainpanel.h"
+
+#include "../../blockedclosuremethodcall.h"
+#include "../../closuremethodcall.h"
+
+
+history::groups::ChangeName::ChangeName(
+    sad::animations::Group* d,
+	int position, 
+    const sad::String& oldvalue,
+    const sad::String& newvalue
+
+)
+: history::groups::ChangeProperty<sad::String>(
+      d,
+      "name",
+      oldvalue,
+      newvalue
+), m_position(position)
+{
+}
+
+history::groups::ChangeName::~ChangeName()
+{
+
+}
+
+void history::groups::ChangeName::updateItem(core::Editor* e, const sad::String&)
+{
+	e->panel()->UI()->lstAnimationsGroup->item(m_position)->setText(
+		e->panel()->nameForGroup(m_animation)
+	);
+}
+
+void history::groups::ChangeName::tryUpdateUI(core::Editor* e, const sad::String& value)
+{
+    this->history::groups::ChangeProperty<sad::String>::tryUpdateUI(e, value);
+	e->emitClosure(bind(this, &history::groups::ChangeName::updateItem, e, value));	
+}
+
+void history::groups::ChangeName::updateUI(core::Editor* e, const sad::String& value)
+{
+    e->emitClosure( blocked_bind(
+            e->panel()->UI()->txtAnimationsGroupName,
+            &QLineEdit::setText,
+            QString(value.c_str())
+        )
+    );
+}
---
+++ plugins/ifaceed/ifaceed/history/groups/groupschangename.h
@@ -0,0 +1,52 @@
+/*! \file groupschangename.h
+    \author HiddenSeeker
+
+    Describe a command, which changes name of animation group
+ */
+#pragma once
+#include "groupschangeproperty.h"
+
+namespace history
+{
+
+namespace groups
+{
+/*! A command, which change name of animation group
+ */
+class ChangeName: public history::groups::ChangeProperty<sad::String>
+{
+public:
+     /*! Constructs new command for animation
+        \param[in] d a node
+		\param[in] position a position of instance
+        \param[in] oldvalue old text
+        \param[in] newvalue new text
+      */
+    ChangeName(sad::animations::Group* d, int position, const sad::String& oldvalue, const sad::String& newvalue);
+    /*! Erases link to an animation
+      */
+    virtual ~ChangeName();
+protected:
+	/*! Updates item in main list
+		\param[in] e editor
+	 */
+	void updateItem(core::Editor* e, const sad::String& value);
+    /*! Tries to update UI in case if node is selected
+        \param[in] e editor
+        \param[in] value a value
+     */
+    virtual void tryUpdateUI(core::Editor* e, const sad::String& value);
+    /*!
+     * Updates current text in field with object name
+     * \param[in] e editor
+     * \param[in] value a value
+     */
+    virtual void updateUI(core::Editor* e, const sad::String& value);
+	/*! A position
+	 */
+	int m_position;
+};
+
+}
+
+}
