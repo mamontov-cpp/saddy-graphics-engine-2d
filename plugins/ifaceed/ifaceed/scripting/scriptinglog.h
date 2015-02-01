4d5355b37c49203c5c0ad77b9b63bcd9a7c812e8
HiddenSeeker <HiddenSeeker@localhost>
2015-02-01 15:04:03 +0300

    Made console.log more object aware

--- plugins/ifaceed/ifaceed/scripting/scriptinglog.h
+++ plugins/ifaceed/ifaceed/scripting/scriptinglog.h
@@ -9,6 +9,13 @@
 namespace scripting
 {
 
+/*! Logs object to a string
+	\param[in] v value
+	\param[in] engine an engine to be object
+	\return string
+ */
+QString  scripting_log_object(const QScriptValue& v, QScriptEngine *engine);
+
 /*! Performs logging action, when scripting scripting
 	\param[in] context a context, which contains arguments
 	\param[in] engine an engine
