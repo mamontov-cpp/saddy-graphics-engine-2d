6bbce1b3c4853da6b8e1ec907769337dbcc58f0f
HiddenSeeker <HiddenSeeker@localhost>
2015-02-02 08:45:23 +0300

    Implemented adding Sprite2D to a scene

--- plugins/ifaceed/ifaceed/scripting/scenenodes/scenenodesbindings.cpp
+++ plugins/ifaceed/ifaceed/scripting/scenenodes/scenenodesbindings.cpp
@@ -11,60 +11,13 @@
 
 #include <renderer.h>
 #include <label.h>
+#include <sprite2d.h>
 
 #include <resource/tree.h>
 #include <resource/resource.h>
 
 #include <db/dbdatabase.h>
 #include <db/dbtable.h>
-
-
-unsigned long long scripting::scenenodes::_addLabel(
-	scripting::Scripting* scripting, 
-	sad::Scene* scene,
-	sad::String resource, 
-	unsigned int fontsize,
-	sad::String text, 
-	sad::String name,
-	sad::Point2D topleftpoint,
-	sad::AColor clr
-)
-{
-	QScriptEngine* engine = scripting->engine();
-	sad::resource::Tree* tree = sad::Renderer::ref()->tree("");
-	sad::resource::Resource* maybefont = tree->root()->resource(resource);
-	if (!maybefont)
-	{
-		engine->currentContext()->throwError(QScriptContext::TypeError, QString("_addLabel: ") + resource.c_str() + " does not name resource");
-		return 0;
-	}
-
-	if (maybefont->metaData()->canBeCastedTo("sad::Font") == false 
-		&& maybefont->metaData()->canBeCastedTo("sad::freetype::Font") == false)
-	{
-		engine->currentContext()->throwError(QScriptContext::TypeError, QString("_addLabel: ") + resource.c_str() + " does not name resource");
-		return 0;
-	}
-
-	sad::Renderer::ref()->lockRendering();
-
-	sad::Label* label = new sad::Label(resource, topleftpoint, text, "");
-	label->setObjectName(name);
-	label->setColor(clr);
-	label->setSize(fontsize);
-
-	scene->add(label);
-	sad::Renderer::ref()->database("")->table("scenenodes")->add(label);
-
-	sad::Renderer::ref()->unlockRendering();
-
-	history::scenenodes::New* cmd = new history::scenenodes::New(label);
-	core::Editor* c = scripting->panel()->editor();
-	cmd->commit(c);
-	c->currentBatchCommand()->add(cmd);
-	
-	return label->MajorId;
-}
 
 
 QScriptValue scripting::scenenodes::list(
@@ -86,3 +39,103 @@ QVector<unsigned long long> scripting::scenenodes::listScene(scripting::Scriptin
 	return result;
 }
 
+
+unsigned long long scripting::scenenodes::_addLabel(
+    scripting::Scripting* scripting,
+    sad::Scene* scene,
+    sad::String resource,
+    unsigned int fontsize,
+    sad::String text,
+    sad::String name,
+    sad::Point2D topleftpoint,
+    sad::AColor clr
+)
+{
+    QScriptEngine* engine = scripting->engine();
+    sad::resource::Tree* tree = sad::Renderer::ref()->tree("");
+    sad::resource::Resource* maybefont = tree->root()->resource(resource);
+    if (!maybefont)
+    {
+        engine->currentContext()->throwError(QScriptContext::TypeError, QString("_addLabel: ") + resource.c_str() + " does not name font resource");
+        return 0;
+    }
+
+    if (maybefont->metaData()->canBeCastedTo("sad::Font") == false
+        && maybefont->metaData()->canBeCastedTo("sad::freetype::Font") == false)
+    {
+        engine->currentContext()->throwError(QScriptContext::TypeError, QString("_addLabel: ") + resource.c_str() + " does not name font resource");
+        return 0;
+    }
+
+    sad::Renderer::ref()->lockRendering();
+
+    sad::Label* label = new sad::Label(resource, topleftpoint, text, "");
+    label->setObjectName(name);
+    label->setColor(clr);
+    label->setSize(fontsize);
+
+    scene->add(label);
+    sad::Renderer::ref()->database("")->table("scenenodes")->add(label);
+
+    sad::Renderer::ref()->unlockRendering();
+
+    history::scenenodes::New* cmd = new history::scenenodes::New(label);
+    core::Editor* c = scripting->panel()->editor();
+    cmd->commit(c);
+    c->currentBatchCommand()->add(cmd);
+
+    return label->MajorId;
+}
+
+
+unsigned long long scripting::scenenodes::_addSprite2D(
+    scripting::Scripting* scripting,
+    sad::Scene* scene,
+    sad::String resource,
+    sad::String name,
+    sad::Rect2D rect,
+    sad::AColor clr
+)
+{
+    QScriptEngine* engine = scripting->engine();
+    sad::resource::Tree* tree = sad::Renderer::ref()->tree("");
+    sad::resource::Resource* maybefont = tree->root()->resource(resource);
+    if (!maybefont)
+    {
+        engine->currentContext()->throwError(QScriptContext::TypeError, QString("_addLabel: ") + resource.c_str() + " does not name sprite options resource (sad::Sprite2D::Options type)");
+        return 0;
+    }
+
+    if (maybefont->metaData()->canBeCastedTo("sad::Sprite2D::Options") == false)
+    {
+        engine->currentContext()->throwError(QScriptContext::TypeError, QString("_addLabel: ") + resource.c_str() + " does not name sprite options resource (sad::Sprite2D::Options type)");
+        return 0;
+    }
+
+    if (sad::isAABB(rect) == false)
+    {
+        engine->currentContext()->throwError(QScriptContext::TypeError, QString("_addLabel: ") + resource.c_str() + " rectangle must be axis aligned");
+        return 0;
+    }
+
+    sad::Renderer::ref()->lockRendering();
+
+    sad::Sprite2D* sprite = new sad::Sprite2D();
+    sprite->setTreeName(sad::Renderer::ref(), "");
+    sprite->set(resource);
+    sprite->setArea(rect);
+    sprite->setObjectName(name);
+    sprite->setColor(clr);
+
+    scene->add(sprite);
+    sad::Renderer::ref()->database("")->table("scenenodes")->add(sprite);
+
+    sad::Renderer::ref()->unlockRendering();
+
+    history::scenenodes::New* cmd = new history::scenenodes::New(sprite);
+    core::Editor* c = scripting->panel()->editor();
+    cmd->commit(c);
+    c->currentBatchCommand()->add(cmd);
+
+    return sprite->MajorId;
+}
