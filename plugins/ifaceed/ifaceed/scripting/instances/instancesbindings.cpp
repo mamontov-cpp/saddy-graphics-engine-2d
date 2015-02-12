3d819458fe29b1bb2cc7a66402b061a20a605c2b
HiddenSeeker <HiddenSeeker@localhost>
2015-02-12 16:42:49 +0300

    Implemented scripting adding way instance to list

--- plugins/ifaceed/ifaceed/scripting/instances/instancesbindings.cpp
+++ plugins/ifaceed/ifaceed/scripting/instances/instancesbindings.cpp
@@ -15,6 +15,8 @@
 #include "../../history/instances/instancesnew.h"
 #include "../../history/instances/instancesremove.h"
 
+#include <animations/animationswayinstance.h>
+
 QScriptValue scripting::instances::list(
     QScriptContext* ctx,
     QScriptEngine* engine
@@ -129,6 +131,108 @@ unsigned long long scripting::instances::_addInstance(
     {
         delete instance;
         scripting->engine()->currentContext()->throwError(QScriptContext::SyntaxError, QString("_addInstance(): \"") + object.toString() + "\" does not name a valid object");
+        return result;
+    }
+
+    instance->setProperty("starttime", starttime);
+    instance->setObjectName(name);
+
+    sad::Renderer::ref()->database("")->table("animationinstances")->add(instance);
+
+    result = instance->MajorId;
+    core::Editor* e = scripting->panel()->editor();
+    history::instances::New* c  = new history::instances::New(instance);
+    c->commit(e);
+    e->currentBatchCommand()->add(c);
+
+   return result;
+}
+
+unsigned long long scripting::instances::_addWayInstance(
+    scripting::Scripting* scripting,
+    sad::String name,
+    QScriptValue way,
+    QScriptValue object,
+    double starttime
+)
+{
+    unsigned long long result = 0;
+
+    sad::animations::WayInstance* instance = new sad::animations::WayInstance();
+
+    sad::Maybe<sad::p2d::app::Way*> maybeway = scripting::ToValue<sad::p2d::app::Way*>::perform(way);
+    unsigned long long wayid = 0ull;
+    bool validway = false;
+    if (maybeway.exists())
+    {
+        validway = true;
+        wayid = maybeway.value()->MajorId;
+    }
+    else
+    {
+        sad::Maybe<unsigned long long> maybewayid = scripting::ToValue<unsigned long long>::perform(way);
+        sad::Maybe<sad::String> maybewayname = scripting::ToValue<sad::String>::perform(way);
+        if (maybewayid.exists())
+        {
+            validway = maybewayid.value() == 0;
+        }
+        else
+        {
+            if (maybewayname.exists())
+            {
+                 validway = maybewayname.value().length() == 0;
+            }
+        }
+    }
+
+    if (validway)
+    {
+        instance->setProperty("way", wayid);
+    }
+    else
+    {
+        delete instance;
+        scripting->engine()->currentContext()->throwError(QScriptContext::SyntaxError, QString("_addWayInstance(): \"") + way.toString() + "\" does not name a valid way and is not 0 or empty string");
+        return result;
+    }
+
+    sad::Maybe<unsigned long long> maybeobjectid = scripting::ToValue<unsigned long long>::perform(object);
+    sad::Maybe<sad::String> maybeobjectname = scripting::ToValue<sad::String>::perform(object);
+    bool validobject = false;
+    unsigned long long objectid = 0ull;
+    if (maybeobjectid.exists() || maybeobjectname.exists())
+    {
+        bool donotscan = false;
+        if (maybeobjectid.exists())
+        {
+           donotscan =  maybeobjectid.value() == 0;
+        }
+        else
+        {
+           donotscan =  maybeobjectname.value().length() == 0;
+        }
+        if (!donotscan)
+        {
+            sad::db::Object* a = scripting::query_object(object);
+            if (a)
+            {
+                 validobject = true;
+                 objectid = a->MajorId;
+            }
+        }
+        else
+        {
+            validobject = true;
+        }
+    }
+    if (validobject)
+    {
+        instance->setProperty("object", objectid);
+    }
+    else
+    {
+        delete instance;
+        scripting->engine()->currentContext()->throwError(QScriptContext::SyntaxError, QString("_addWayInstance(): \"") + object.toString() + "\" does not name a valid object");
         return result;
     }
 
