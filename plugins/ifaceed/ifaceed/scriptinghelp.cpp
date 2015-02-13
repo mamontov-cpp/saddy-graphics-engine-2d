815bbb94d214b704e46e4859a28aa6d9eea614bf
overanalyzer1 <overanalyzer1@gmail.com>
2015-02-13 18:54:46 +0300

    Added and made working slots of help window

--- plugins/ifaceed/ifaceed/scriptinghelp.cpp
+++ plugins/ifaceed/ifaceed/scriptinghelp.cpp
@@ -3,14 +3,25 @@
 ScriptingHelp::ScriptingHelp(QWidget* parent) : QDialog(parent)
 {
 	m_ui.setupUi(this);
-}
 
+	connect(m_ui.txtRequest, SIGNAL(returnPressed()), this,SLOT(applySearch()));
+	connect(m_ui.btnCancelSearch, SIGNAL(clicked()), this,SLOT(cancelSearch()));
+}
 
 ScriptingHelp::~ScriptingHelp()
 {
 	
 }
 
+void ScriptingHelp::applySearch()
+{
+	
+}
+
+void ScriptingHelp::cancelSearch()
+{
+	
+}
 
 void ScriptingHelp::setText(const QString& text)
 {
