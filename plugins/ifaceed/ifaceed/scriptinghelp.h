06358d45025c01412712f7964977a62e84d7cada
overanalyzer1 <overanalyzer1@gmail.com>
2015-02-13 18:45:36 +0300

    Started implementing search in help window

---
+++ plugins/ifaceed/ifaceed/scriptinghelp.h
@@ -0,0 +1,33 @@
+/*! \file scriptinghelp.h
+	\author HiddenSeeker
+
+	Describes a dialog for showing scripting help
+ */
+#pragma once
+
+#include <QDialog>
+#include <QString>
+
+#include "ui_scriptinghelp.h"
+
+/*! A dialog for showing help for scripting editor
+ */
+class ScriptingHelp: public QDialog
+{
+Q_OBJECT
+public:
+	/*! Creates new file list for reloading
+		\param[in] parent a parent widget for selecting
+	 */
+    ScriptingHelp(QWidget* parent = NULL);
+	/*! Could be inherited
+	 */
+	virtual ~ScriptingHelp();
+	/*! Sets help text
+	 */
+	void setText(const QString& text);
+protected:
+	/*! UI for dialog
+	 */
+	Ui::ScriptingHelp  m_ui;
+};
