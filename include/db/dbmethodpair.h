d1110cd13ab95add522298d38660f5e4f7057cf5
mamontov-cpp <overanalyzer1@gmail.com>
2015-09-09 10:48:12 +0300

    First implementation of default field value for property.

--- include/db/dbmethodpair.h
+++ include/db/dbmethodpair.h
@@ -1,7 +1,7 @@
 /*! \file   dbmethodpair.h
-	
-	
-	Describes a pair of methods, which could be used to work as property
+    
+    
+    Describes a pair of methods, which could be used to work as property
  */
 #pragma once
 #include "dbproperty.h"
@@ -19,119 +19,126 @@ namespace db
 /*! Describes a pair of methods, which could be used to get and set data
  */
 template<
-	typename _Object,
-	typename _FieldTypeName
+    typename _Object,
+    typename _FieldTypeName
 >
 class MethodPair: public sad::db::Property
 {
 public:
-	/*! Setups a pair of methods
-		\param[in] g getter part
-		\param[in] s setter part
-	 */
-	inline MethodPair(
-		sad::util::getter::Proxy<_Object, _FieldTypeName> * g, 
-		sad::util::setter::Proxy<_Object, _FieldTypeName> * s
-	) : m_getter(g), m_setter(s)
-	{
-		m_base_type = sad::db::TypeName<_FieldTypeName>::baseName();
-		m_type_is_kind_of_sad_object = sad::db::TypeName<_FieldTypeName>::isSadObject();
-		m_pointer_stars_count = sad::db::TypeName<_FieldTypeName>::POINTER_STARS_COUNT;
-	}
-	/*! Sets a pair of methods
-		\param[in] g getter part
-		\param[in] s setter part
-	 */
-	template<
-		typename _Getter,
-		typename _Setter
-	>
-	inline MethodPair(
-		_Getter g, 
-		_Setter s
-	) 
-	{
-		m_getter = sad::util::define_getter<_Object, _FieldTypeName>(g);
-		m_setter = sad::util::define_setter<_Object, _FieldTypeName>(s);
+    /*! Setups a pair of methods
+        \param[in] g getter part
+        \param[in] s setter part
+     */
+    inline MethodPair(
+        sad::util::getter::Proxy<_Object, _FieldTypeName> * g, 
+        sad::util::setter::Proxy<_Object, _FieldTypeName> * s
+    ) : m_getter(g), m_setter(s)
+    {
+        m_base_type = sad::db::TypeName<_FieldTypeName>::baseName();
+        m_type_is_kind_of_sad_object = sad::db::TypeName<_FieldTypeName>::isSadObject();
+        m_pointer_stars_count = sad::db::TypeName<_FieldTypeName>::POINTER_STARS_COUNT;
+    }
+    /*! Sets a pair of methods
+        \param[in] g getter part
+        \param[in] s setter part
+     */
+    template<
+        typename _Getter,
+        typename _Setter
+    >
+    inline MethodPair(
+        _Getter g, 
+        _Setter s
+    ) 
+    {
+        m_getter = sad::util::define_getter<_Object, _FieldTypeName>(g);
+        m_setter = sad::util::define_setter<_Object, _FieldTypeName>(s);
 
-		m_base_type = sad::db::TypeName<_FieldTypeName>::baseName();
-		m_type_is_kind_of_sad_object = sad::db::TypeName<_FieldTypeName>::isSadObject();
-		m_pointer_stars_count = sad::db::TypeName<_FieldTypeName>::POINTER_STARS_COUNT;
-	}
+        m_base_type = sad::db::TypeName<_FieldTypeName>::baseName();
+        m_type_is_kind_of_sad_object = sad::db::TypeName<_FieldTypeName>::isSadObject();
+        m_pointer_stars_count = sad::db::TypeName<_FieldTypeName>::POINTER_STARS_COUNT;
+    }
     /*! Frees all proxies
      */
     virtual ~MethodPair()
-	{
-	    delete m_getter;
+    {
+        delete m_getter;
         delete m_setter;
-	}
+    }
 
-	/*! Clones a property
-		\return a property clone
-	 */
-	virtual sad::db::Property* clone() const
-	{
-		return new sad::db::MethodPair<_Object,_FieldTypeName>(m_getter->clone(), m_setter->clone());
-	}
-	/*! Sets a value for a property
-		\param[in] o an object
-		\param[in] v a value for property
-		\return whether value is set successfully
-	 */
-	virtual bool set(sad::db::Object * o, const sad::db::Variant & v)
-	{
-		assert( o );
-		sad::Maybe<_FieldTypeName> value = v.get<_FieldTypeName>();
-		bool result = false;
-		if (value.exists() && o)
-		{
-			m_setter->set(reinterpret_cast<_Object*>(o), value.value());
-			result = true;
-		}
-		return result;
-	}
-	/*! Gets a value for a property
-		\param[in] o an object
-		\param[in] v a value for a property
-	 */
-	virtual void get(sad::db::Object const* o, sad::db::Variant & v) const
-	{
-		assert( o );
-		if (o)
-		{
-			v.set(m_getter->get(reinterpret_cast<_Object const*>(o)));
-		}
-	}
+    /*! Clones a property
+        \return a property clone
+     */
+    virtual sad::db::Property* clone() const
+    {
+        return new sad::db::MethodPair<_Object,_FieldTypeName>(m_getter->clone(), m_setter->clone());
+    }
+    /*! Sets a value for a property
+        \param[in] o an object
+        \param[in] v a value for property
+        \return whether value is set successfully
+     */
+    virtual bool set(sad::db::Object * o, const sad::db::Variant & v)
+    {
+        assert( o );
+        sad::Maybe<_FieldTypeName> value = v.get<_FieldTypeName>();
+        bool result = false;
+        if (value.exists() && o)
+        {
+            m_setter->set(reinterpret_cast<_Object*>(o), value.value());
+            result = true;
+        }
+        return result;
+    }
+    /*! Gets a value for a property
+        \param[in] o an object
+        \param[in] v a value for a property
+     */
+    virtual void get(sad::db::Object const* o, sad::db::Variant & v) const
+    {
+        assert( o );
+        if (o)
+        {
+            v.set(m_getter->get(reinterpret_cast<_Object const*>(o)));
+        }
+    }
 
-	/*! Checks, whether value has property type in key field
-		\param[in] key a key of field to check
-		\param[in] v value
-		\return whether field has following type
-	 */
-	virtual bool check(const sad::String& key, const picojson::value& v)
-	{
-		bool result = false;
-		if (v.is<picojson::object>())
-		{
-			picojson::value data = v.get(key);
-			if (data.is<picojson::null>() == false) 
-			{
-				result = picojson::ValueToType<_FieldTypeName>().get(data).exists();
-			}
-		}
-		return result;
-	}
-
+    /*! Checks, whether value has property type in key field
+        \param[in] key a key of field to check
+        \param[in] v value
+        \return whether field has following type
+     */
+    virtual bool check(const sad::String& key, const picojson::value& v)
+    {
+        bool result = false;
+        if (v.is<picojson::object>())
+        {
+            picojson::value data = v.get(key);
+            if (data.is<picojson::null>() == false) 
+            {
+                result = picojson::ValueToType<_FieldTypeName>().get(data).exists();
+            }
+        }
+        return result;
+    }
+    /*! Returns true, if property value could be set from specified value
+        \param[in] v value
+        \return whether it could be set from value
+     */ 
+    virtual bool couldBeSetFrom(const sad::db::Variant& v)
+    {
+        return v.get<_FieldTypeName>().exists();
+    }
 protected:
-	/*! A temporary variant for returning property from field
-	 */
-	sad::db::Variant m_tmp;
-	/*! A getter for method pair
-	 */
-	sad::util::getter::Proxy<_Object, _FieldTypeName> * m_getter;
-	/*! A setter for method pair
-	 */
-	sad::util::setter::Proxy<_Object, _FieldTypeName> * m_setter;
+    /*! A temporary variant for returning property from field
+     */
+    sad::db::Variant m_tmp;
+    /*! A getter for method pair
+     */
+    sad::util::getter::Proxy<_Object, _FieldTypeName> * m_getter;
+    /*! A setter for method pair
+     */
+    sad::util::setter::Proxy<_Object, _FieldTypeName> * m_setter;
 };
 
 }
--- include/db/dbproperty.h
+++ include/db/dbproperty.h
@@ -1,7 +1,7 @@
 /*! \file dbproperty.h
-	
+    
 
-	Describes a basic property, which objects are consists of
+    Describes a basic property, which objects are consists of
  */
 #pragma once
 #include "../sadstring.h"
@@ -21,63 +21,87 @@ class Variant;
 class Property
 {
 public:
-	/*! Constructs new property
-	 */
-	Property();
-	/*! Can be inherited
-	 */
-	virtual ~Property();
-	/*! Returns name of type of property
-		\return type of property
-	 */
-	virtual const sad::String & baseType() const;
-	/*! Returns whether type of property is kind of sad::Object
-		\return whether type of property is kind of sadd::Object
-	 */
-	virtual bool typeIsKindOfSadObject() const;
-	/*! Returns count for pointer stars count
-		\return pointer stars count data
-	 */
-	virtual int pointerStarsCount() const;
-	/*! Clones a property
-		\return a property clone
-	 */
-	virtual sad::db::Property* clone() const = 0;
-	/*! Sets a value for a property
-		\param[in] o an object
-		\param[in] v a variant for property
-		\return whether value is set successfully
-	 */
-	virtual bool set(sad::db::Object * o, const sad::db::Variant & v) = 0;
-	/*! Gets a value for a property
-		\param[in] o an object
-		\param[in] v a value for a property
-	 */
-	virtual void get(sad::db::Object const* o, sad::db::Variant & v) const = 0;
-	/*! Checks, whether value has property type in key field
-		\param[in] key a key of field to check
-		\param[in] v value
-		\return whether field has following type
-	 */
-	virtual bool check(const sad::String& key, const picojson::value& v);
-	/*! Whether properties has totally equal type
-		\param[in] o other type
-		\return whether types are equal
-	 */
-	bool hasEqualTypeAs(sad::db::Property * o) const;
-	/*! A type, under which property could be serialized
-	 */
-	sad::String serializableType() const;
+    /*! Constructs new property
+     */
+    Property();
+    /*! Can be inherited
+     */
+    virtual ~Property();
+    /*! Returns name of type of property
+        \return type of property
+     */
+    virtual const sad::String & baseType() const;
+    /*! Returns whether type of property is kind of sad::Object
+        \return whether type of property is kind of sadd::Object
+     */
+    virtual bool typeIsKindOfSadObject() const;
+    /*! Returns count for pointer stars count
+        \return pointer stars count data
+     */
+    virtual int pointerStarsCount() const;
+    /*! Clones a property
+        \return a property clone
+     */
+    virtual sad::db::Property* clone() const = 0;
+    /*! Sets a value for a property
+        \param[in] o an object
+        \param[in] v a variant for property
+        \return whether value is set successfully
+     */
+    virtual bool set(sad::db::Object * o, const sad::db::Variant & v) = 0;
+    /*! Gets a value for a property
+        \param[in] o an object
+        \param[in] v a value for a property
+     */
+    virtual void get(sad::db::Object const* o, sad::db::Variant & v) const = 0;
+    /*! Checks, whether value has property type in key field
+        \param[in] key a key of field to check
+        \param[in] v value
+        \return whether field has following type
+     */
+    virtual bool check(const sad::String& key, const picojson::value& v);
+    /*! Whether properties has totally equal type
+        \param[in] o other type
+        \return whether types are equal
+     */
+    bool hasEqualTypeAs(sad::db::Property * o) const;
+    /*! A type, under which property could be serialized
+     */
+    sad::String serializableType() const;
+    /*! Makes a property non-required with default value
+        \param[in] default_value
+        \return true if successfull, false in case property could not be set from
+                     specified type or default_value is NULL.
+     */
+    bool makeNonRequiredWithDefaultValue(
+        sad::db::Variant* default_value   
+    );
+    /*! Returns true, if property value could be set from specified value
+        \param[in] v value
+        \return whether it could be set from value
+     */ 
+    virtual bool couldBeSetFrom(const sad::db::Variant& v) = 0;
+    /*! Returns true, if property has a default value
+        \return true, if default value is presented
+     */
+    bool hasDefaultValue() const;
+    /*! A default value for property
+        \return default value for property
+     */
+    sad::db::Variant* defaultValue() const;
 protected:
-	/*! Amount of pointers for type
-	 */
-	int m_pointer_stars_count;
-	/*! True if type of property is kind of sad object
-	 */
-	bool m_type_is_kind_of_sad_object;
-	/*! A base name of type of property (a name of type without a pointers)
-	 */
-	sad::String m_base_type;
+    /*! Amount of pointers for type
+     */
+    int m_pointer_stars_count;
+    /*! True if type of property is kind of sad object
+     */
+    bool m_type_is_kind_of_sad_object;
+    /*! A base name of type of property (a name of type without a pointers)
+     */
+    sad::String m_base_type;
+    /*! A default value for property, which should be set in case, if it's absent in JSON
+     */
+    sad::db::Variant* m_default_value;
 };
 
 }
