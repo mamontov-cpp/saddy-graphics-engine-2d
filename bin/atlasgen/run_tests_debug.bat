32d291f2171532a69080b891b3a4abd0d07b17cc
mamontov-cpp <overanalyzer1@gmail.com>
2016-01-22 13:24:56 +0300

    Added tests for merging several configuration.

--- bin/atlasgen/run_tests_debug.bat
+++ bin/atlasgen/run_tests_debug.bat
@@ -1,3 +1,4 @@
 "../atlasgen-debug.exe" non_unique/non_unique.json -non-unique-textures
 "../atlasgen-debug.exe" bug-48/bug-48.json 
 "../atlasgen-debug.exe" bug-47/bug-47.json 
+"../atlasgen-debug.exe" merge1/non_unique_1.json merge1/non_unique_2.json -non-unique-textures
