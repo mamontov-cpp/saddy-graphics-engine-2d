4088d1cd3fc9716bbfa525dbd4c25771c0b5423d
mamontov-cpp <overanalyzer1@gmail.com>
2016-03-23 13:47:59 +0300

    Implemented functions, related to working with cell editor gui::actions::GridActions.

--- plugins/ifaceed/ifaceed/gui/actions/gridactions.cpp
+++ plugins/ifaceed/ifaceed/gui/actions/gridactions.cpp
@@ -1,14 +1,53 @@
 #include "gridactions.h"
+#include "../../core/editor.h"
+#include "../../core/shared.h"
 
 
 gui::actions::GridActions::GridActions(QObject* parent)
-: QObject(parent)
+: QObject(parent), m_provider(NULL)
 {
 
+}
+
+void gui::actions::GridActions::setEditor(core::Editor* e)
+{
+	m_provider = new gui::ChildrenProvider();
+	m_provider->setProxy(e->panelProxy());
 }
 
 gui::actions::GridActions::~GridActions()
 {
-
+	delete m_provider;
 }
 
+sad::layouts::Grid* gui::actions::GridActions::selectedGrid() const
+{
+	sad::layouts::Grid* result = NULL;
+	if (m_editor)
+	{
+		result = m_editor->shared()->selectedGrid();
+	}
+	return result;
+}
+
+void gui::actions::GridActions::insertCellEditor(size_t row,  size_t col, gui::layouts::LayoutCellEdit* cell)
+{
+	if (m_cell_editors.contains(row) == false)
+	{
+		m_cell_editors.insert(row, QHash<size_t, gui::layouts::LayoutCellEdit*>());
+	}
+	m_cell_editors[row].insert(col, cell);
+}
+
+gui::layouts::LayoutCellEdit* gui::actions::GridActions::cellEditor(size_t row,  size_t col)
+{
+	gui::layouts::LayoutCellEdit* edit = NULL;
+	if (m_cell_editors.contains(row))
+	{
+		if (m_cell_editors[row].contains(col))
+		{
+			edit = m_cell_editors[row][col];
+		}
+	}
+	return edit;
+}
--- plugins/ifaceed/ifaceed/gui/actions/gridactions.h
+++ plugins/ifaceed/ifaceed/gui/actions/gridactions.h
@@ -8,6 +8,8 @@
 
 #include <input/controls.h>
 #include "abstractactions.h"
+#include "../childrenprovider.h"
+#include "../layouts/layoutcelledit.h"
 
 class MainPanel;
 
@@ -27,9 +29,36 @@ public:
         \param[in] parent a parent object
      */
     GridActions(QObject* parent = NULL);
+    /*! Sets editor
+        \param[in] e editor
+     */
+    virtual void setEditor(core::Editor* e);
     /*! This class could be inherited
      */
     virtual ~GridActions();
+	/*! Returns selected grid, according to editor
+		\param[in] grid a current grid
+	 */
+	sad::layouts::Grid* selectedGrid() const;
+	/*! Inserts new cell editor
+		\param[in] row a row
+		\param[in] col a column
+		\param[in] cell a cell editor
+	 */
+	void insertCellEditor(size_t row,  size_t col, gui::layouts::LayoutCellEdit* cell);
+	/*! Returns a cell editor
+		\param[in] row a row
+		\param[in] col a column
+		\return a cell editor
+	 */
+	gui::layouts::LayoutCellEdit* cellEditor(size_t row,  size_t col);
+private:
+	/*! A provider for grid action
+	 */
+	gui::ChildrenProvider* m_provider;
+	/*! A hash table from actions to local editors for provider
+	 */
+	QHash<size_t, QHash<size_t, gui::layouts::LayoutCellEdit*> > m_cell_editors; 
 };
 
 }
