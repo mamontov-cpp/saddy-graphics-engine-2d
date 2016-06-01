a9ab4d0c61359070cb6ce4b6d7eec05e23eeb122
mamontov-cpp <overanalyzer1@gmail.com>
2016-06-01 12:48:15 +0300

    [IFaceEd] [GH-15] Updated VS project, added definition for scripting::ScriptableGridCell.

--- plugins/ifaceed/ifaceed/scripting/layouts/scriptablegridcell.h
+++ plugins/ifaceed/ifaceed/scripting/layouts/scriptablegridcell.h
@@ -3,5 +3,44 @@
     A grid cell scripting bindings should be placed here
  */
 #pragma once
+#include "../classwrapper.h"
 
-// TODO: Actually, implement it
+#include <layouts/grid.h>
+
+namespace scripting
+{
+
+class Scripting;
+
+namespace layouts
+{
+
+/*! A wrapper to point reference
+ */
+class ScriptableGridCell: public scripting::ClassWrapper
+{
+	Q_OBJECT
+public:
+    /*! A destructor for grid
+     */
+    virtual ~ScriptableGridCell();
+
+protected:
+    /*! A major id for database object
+     */
+    unsigned long long m_majorid;
+	/*! A row for cell
+	 */
+	unsigned int m_row;
+	/*! A column for cell
+	 */
+	unsigned int m_column;
+    /*! A main scripting object
+     */
+    scripting::Scripting* m_scripting;
+};
+
+}
+
+}
+
