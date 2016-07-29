e7aed83ea4ca4d0468af68b4e46617fab58cec0f
Mamontov Dmitry <mamontov.dp@gmail.com>
2016-07-29 16:08:23 +0300

    [ATLASGEN] Implemented support for one pixel in FullSearchPacker. Testing needed.

--- tools/atlasgen/fullsearchpacker/fullsearchpacker.cpp
+++ tools/atlasgen/fullsearchpacker/fullsearchpacker.cpp
@@ -26,13 +26,102 @@ void fullsearchpacker::FullSearchPacker::pack(Atlas& atlas, QImage*& image)
     image = new QImage(widthheight, widthheight, QImage::Format_ARGB32);
     image->fill(QColor(255, 255, 255, 0));
 
+    bool add_pixel = (*m_options)["add-pixel"].toBool();
+
     QPainter painter(image);
     for(size_t i = 0; i < atlas.textures().size(); i++)
     {
-        painter.drawImage(
-            atlas.textures()[i]->TextureRectangle.topLeft(),
-            atlas.textures()[i]->Image
-        );
+        QPointF topleft = atlas.textures()[i]->TextureRectangle.topLeft();
+        const QImage& img = atlas.textures()[i]->Image;
+        if (add_pixel)
+        {
+            // Top part
+            painter.drawImage(
+                topleft.x(),
+                topleft.y(),
+                img,
+                0,
+                0,
+                1,
+                1
+            );
+            painter.drawImage(
+                topleft.x() + 1,
+                topleft.y(),
+                img,
+                0,
+                0,
+                img.width(),
+                1
+            );
+            painter.drawImage(
+                topleft.x() + img.width() + 1,
+                topleft.y(),
+                img,
+                img.width() - 1,
+                0,
+                1,
+                1
+            );
+            // Middle part
+            painter.drawImage(
+                topleft.x(),
+                topleft.y() + 1,
+                img,
+                0,
+                0,
+                1,
+                img.height()
+            );
+            painter.drawImage(
+                topleft + QPointF(1, 1),
+                img
+            );
+            painter.drawImage(
+                topleft.x() + img.width() + 1,
+                topleft.y() + 1,
+                img,
+                img.width() - 1,
+                0,
+                1,
+                img.height()
+            );
+            // Bottom part
+            painter.drawImage(
+                topleft.x(),
+                topleft.y() + img.height() + 1,
+                img,
+                0,
+                img.height() - 1,
+                1,
+                1
+            );
+            painter.drawImage(
+                topleft.x() + 1,
+                topleft.y() + img.height() + 1,
+                img,
+                0,
+                img.height() - 1,
+                img.width(),
+                1
+            );
+            painter.drawImage(
+                topleft.x() + img.width() + 1,
+                topleft.y() + img.height() + 1,
+                img,
+                img.width()  - 1,
+                img.height() - 1,
+                1,
+                1
+            );
+        }
+        else
+        {
+            painter.drawImage(
+                topleft,
+                img
+            );
+        }
     }
     painter.end();
 }
