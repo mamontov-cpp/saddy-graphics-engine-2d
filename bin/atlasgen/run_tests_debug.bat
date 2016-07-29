76c23854663f2b2acf5f5880c93cbeee68cd486d
Mamontov Dmitry <mamontov.dp@gmail.com>
2016-07-29 17:02:10 +0300

    [ATLASGEN] Added tests for one-pixel offset hack.

--- bin/atlasgen/run_tests_debug.bat
+++ bin/atlasgen/run_tests_debug.bat
@@ -4,3 +4,5 @@
 "../atlasgen-debug.exe" merge1/non_unique_1.json merge1/non_unique_2.json -non-unique-textures
 "../atlasgen-debug.exe" merge2/non_unique_atlas.json merge2/non_unique_atlas2.json merge2/itemicons.json -non-unique-textures
 "../atlasgen-debug.exe" srgba/srgba.json -srgba 
+"../atlasgen-debug.exe" 1pix/1pix.json --add-pixel --full-search
+"../atlasgen-debug.exe" 1pix/1pix-one.json --add-pixel --full-search
