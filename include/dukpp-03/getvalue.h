c03a2d6092c18b53231b33502b49fd53ee739d23
Mamontov Dmitry <mamontov.dp@gmail.com>
2017-01-31 14:13:26 +0300

    [SADDY-DUKPP-03] a get value for variants.

--- include/dukpp-03/getvalue.h
+++ include/dukpp-03/getvalue.h
@@ -63,4 +63,53 @@ public:
 
 };
 
+
+/*! An instantiation for variant
+ */
+template<>
+class GetValue<sad::db::Variant,  sad::dukpp03::BasicContext>
+{
+public:
+/*! Performs getting value from stack
+    \param[in] ctx context
+    \param[in] pos index for stack
+    \return a value if it exists, otherwise empty maybe
+ */
+inline static ::dukpp03::Maybe<sad::db::Variant> perform(
+    sad::dukpp03::BasicContext* ctx,
+    duk_idx_t pos
+)
+{
+    ::dukpp03::Maybe<sad::db::Variant> result;
+    if (duk_is_string(ctx->context(), pos))
+    {
+        result.setValue(sad::String(duk_to_string(ctx->context(), pos)));
+    }
+    if (duk_is_boolean(ctx->context(), pos))
+    {
+        result.setValue(static_cast<bool>(duk_to_string(ctx->context(), pos)));
+    }
+    if (duk_is_number(ctx->context(), pos))
+    {
+        result.setValue(duk_to_number(ctx->context(), pos));
+    }
+    if (duk_is_object(ctx->context(), pos))
+    {
+        duk_get_prop_string(ctx->context(), pos, DUKPP03_VARIANT_PROPERTY_SIGNATURE);
+        if (duk_is_pointer(ctx->context(), -1))
+        {
+            void* ptr = duk_to_pointer(ctx->context(), -1);
+            sad::db::Variant * v = reinterpret_cast<sad::db::Variant *>(ptr);
+            if (v)
+            {
+                result.setValue(*v);
+            }
+        }
+        duk_pop(ctx->context());
+    }
+    return result;
+}
+
+};
+
 }
