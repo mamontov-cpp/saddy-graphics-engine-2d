092bed3f2f3e98bbbae32b7411ef85c04409415f
mamontov-cpp <mamontov.dp@gmail.com>
2017-03-21 21:26:47 +0300

    [CORE] Added sad::util::Markup::parseBoolValue.

--- include/util/markup.h
+++ include/util/markup.h
@@ -6,6 +6,13 @@
 #include "../sadvector.h"
 #include "../sadhash.h"
 #include "../sadstring.h"
+#include "../sadcolor.h"
+
+namespace pugi
+{
+class xml_node;
+
+}
 
 namespace sad
 {
@@ -20,26 +27,36 @@ struct Markup
 
 /*! A tag element, that is being parsed
  */
-struct Tag
+struct Command
 {
-    /*! A name of tag
+    /*! Whether text should be strikethrough
      */
-    sad::String Name;
+    bool Strikethrough;
+    /*! Whether text should be underlined
+     */
+    bool Underlined;
+    /*! A size in pixel, that should be used to render font
+     */
+    unsigned int Size;
+    /*! A color, which shouldbe used to draw text
+     */
+    sad::AColor Color;
+    /*! A font, that should be used to render text in command
+     */
+    sad::String Font;
+    /*! A linespacing, that should be used for rendering
+     */
+    double Linespacing;
+    /*! Describes, whether line spacing should be taken from font or just left here
+     */
+    bool LinespacingIsSet;
     /*! A content of string
      */
     sad::String Content;
 
-    /*! A list of attributes for tag
+    /*! Constructs new comand
      */
-    sad::Hash<sad::String, sad::String> Attributes;
-
-    /*! A parent tag offset
-     */
-    size_t ParentOffset;
-
-    /*! Constructs default tag
-     */
-    inline Tag() : ParentOffset(0)
+    inline Command() : Strikethrough(false), Underlined(false), Linespacing(0), LinespacingIsSet(false)
     {
 
     }
@@ -47,13 +64,35 @@ struct Tag
 
 /*! A document is a list of tags
  */
-typedef sad::Vector<Tag> Document;
+typedef sad::Vector<sad::util::Markup::Command> Document;
 
 /*! Parses document for markup. Makes a non-hierarchical list of tags, with respect to nesting.
     \param[in] s string a string
+    \param[in] basic a basic command for parent
     \return document
  */
-sad::util::Markup::Document parseDocument(const sad::String& s);
+sad::util::Markup::Document parseDocument(
+    const sad::String& s,
+    const sad::util::Markup::Command& basic
+);
+
+
+/*! Parses tag for markup. Makes a non-hierarchical list of tags, with respect to nesting.
+    \param[in] source a source tag
+    \param[in] parent a parent command for base
+    \return document
+ */
+sad::util::Markup::Document parseTag(
+    const pugi::xml_node& source,
+    const sad::util::Markup::Command& parent
+);
+
+/*! Parses bool value
+    \param[in] value a value
+    \param[in] parent a parent value
+ */
+bool parseBoolValue(const char* value, bool parent);
+
 };
 
 }
--- src/util/markup.cpp
+++ src/util/markup.cpp
@@ -1,129 +1,54 @@
 #include "util/markup.h"
+#include "3rdparty/pugixml/pugixml.hpp"
 
-/*! A state, when working with local text
- */
-enum MarkupParserState
-{
-    INSIDE_TEXT = 0,
-    TAG_NAME_SEARCH = 1,
-    INSIDE_TAG_NAME = 2,
-    SEARCH_TAG_ATTRIBUTE_NAME = 3,
-    INSIDE_TAG_ATTRIBUTE_NAME = 4,
-    SEARCH_TAG_ATTRIBUTE_EQUAL = 5,
-    INSIDE_TAG_ATTRIBUTE_EQUAL = 6,
-    INSIDE_TAG_ATTRIBUTE_VALUE = 7
-};
+#include <cstring>
 
-// This looks ugly. Can't we just use descent parser?
-sad::util::Markup::Document sad::util::Markup::parseDocument(const sad::String& s)
+sad::util::Markup::Document sad::util::Markup::parseDocument(
+    const sad::String& s,
+    const sad::util::Markup::Command& basic
+)
 {
     sad::util::Markup::Document result;
-    result << sad::util::Markup::Tag();
-    int parent_offset = 0;
-    int tag_start = 0;
-    sad::String buffer_name;
-    sad::String buffer_attr_name;
-    MarkupParserState state = INSIDE_TEXT;
-    for (size_t i = 0; i < s.size(); i++)
+    pugi::xml_document doc;
+    doc.load_string(s.c_str(), pugi::parse_fragment);
+    for (pugi::xml_node child = doc.first_child(); child; child = child.next_sibling())
     {
-        bool handled = false;
-        switch (state)
+        if (child.type() == pugi::node_pcdata)
         {
-        case INSIDE_TEXT: {
-                if (s[i] != '<')
-                {
-                    result[parent_offset].Content << s[i];
-                }
-                else
-                {
-                    tag_start = i;
-                    state = TAG_NAME_SEARCH;
-                }
-                break;
-            }
-        case TAG_NAME_SEARCH: {
-                if (s[i] != '>')
-                {
-                    if (s[i] != ' ' && s[i] != '\t')
-                    {
-                        buffer_name.clear();
-                        buffer_name << s[i];
-                        state = INSIDE_TAG_NAME;
-                    }
-                }
-                else
-                {
-                    result[parent_offset].Content.substr(tag_start, i - tag_start + 1);
-                    state = INSIDE_TEXT;
-                }
-                break;
-            }
-        case INSIDE_TAG_NAME: {
-                if (s[i] == '>')
-                {
-                    result << sad::util::Markup::Tag();
-                    sad::util::Markup::Tag& t = result[result.size() - 1];
-                    t.Name = buffer_name;
-                    buffer_name.clear();
-                    t.ParentOffset = parent_offset;
-                    parent_offset = result.size() - 1;
-                    state = INSIDE_TEXT;
-                }
-                else
-                {
-                    if (s[i] == ' ' || s[i] == '\t')
-                    {
-                        result << sad::util::Markup::Tag();
-                        sad::util::Markup::Tag& t = result[result.size() - 1];
-                        t.Name = buffer_name;
-                        buffer_name.clear();
-                        t.ParentOffset = parent_offset;
-                        state = SEARCH_TAG_ATTRIBUTE_NAME;
-                    }
-                    else
-                    {
-                        buffer_name << s[i];
-                    }
-                }
-                break;
-            }
-        case SEARCH_TAG_ATTRIBUTE_NAME: {
-                if (s[i] != ' ' && s[i] != '\t')
-                {
-                    if (s[i] == '>')
-                    {
-                        state = INSIDE_TEXT;
-                    }
-                    else
-                    {
-                        buffer_attr_name.clear();
-                        buffer_attr_name << s[i];
-                        state = INSIDE_TAG_ATTRIBUTE_NAME;
-                    }
-                }
-                break;
-            }
-        case INSIDE_TAG_ATTRIBUTE_NAME: {
-                if (s[i] == '>') 
-                {
-                    state = INSIDE_TEXT;
-                }
-                else 
-                {
-                    if (s[i] == ' ' || s[i] == '\t')
-                    {
-                        state = SEARCH_TAG_ATTRIBUTE_EQUAL;
-                    }
-                    else
-                    {
-                        buffer_attr_name << s[i];
-                    }
-                }
-                break;
-            }
-        };
+            sad::util::Markup::Command cmd = basic;
+            cmd.Content = child.text().get();
+        }
+        if (child.type() == pugi::node_element)
+        {
+            result << sad::util::Markup::parseTag(child, basic);
+        }
     }
-    // End of string
-
     return result;
 }
+
+sad::util::Markup::Document sad::util::Markup::parseTag(
+    const pugi::xml_node& source,
+    const sad::util::Markup::Command& parent
+)
+{
+    return sad::util::Markup::Document();
+}
+
+
+bool sad::util::Markup::parseBoolValue(const char* value, bool parent)
+{
+    if (!value)
+    {
+        return parent;
+    }
+    if (!strcmp(value, "true"))
+    {
+        return true;
+    }
+    if (!strcmp(value, "false"))
+    {
+        return false;
+    }
+    return parent;
+}
+
