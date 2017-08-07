736ac3e85e509cc8e689a55d5b43e45101188668
Mamontov Dmitry <mamontov.dp@gmail.com>
2017-08-07 13:42:43 +0300

    [CORE] Repaired build order in build-all-msvc.

--- build-all-msvc.bat
+++ build-all-msvc.bat
@@ -22,10 +22,10 @@ if [%VALID%] EQU [0]  (
 	exit /B 1
 )
 @echo on
-devenv plugins/freetype/saddy-ft.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
 devenv libsaddy.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
 devenv tools/atlasgen/atlasgen.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
 devenv plugins/dukpp-03/dukpp-03.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
+devenv plugins/freetype/saddy-ft.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
 devenv tools/exporter/exporter.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
 devenv plugins/irrklang/irrklang.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
 devenv plugins/dukpp-03-irrklang/dukpp-03-irrklang.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
