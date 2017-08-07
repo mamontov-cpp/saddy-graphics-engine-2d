0abd4bba3ee0a40c4c6193c098784395e4abb4db
Mamontov Dmitry <mamontov.dp@gmail.com>
2017-08-07 11:50:39 +0300

    [CORE] Attempt to debug Qt variables not being set.

--- build-all-msvc.bat
+++ build-all-msvc.bat
@@ -22,8 +22,8 @@ if [%VALID%] EQU [0]  (
 	exit /B 1
 )
 @echo on
-devenv libsaddy.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
 devenv tools/atlasgen/atlasgen.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
+devenv libsaddy.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
 devenv plugins/dukpp-03/dukpp-03.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
 devenv tools/exporter/exporter.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
 devenv plugins/freetype/saddy-ft.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
