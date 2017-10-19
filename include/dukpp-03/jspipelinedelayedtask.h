981d4fa6bd4c7839d05b105520f1f101d12abbda
Nikita Biryukov <biryukov.nikita96@mail.ru>
2017-10-19 19:43:13 +0300

    Added class JSPipelineDelayedTask

--- include/dukpp-03/jspipelinedelayedtask.h
+++ include/dukpp-03/jspipelinedelayedtask.h
@@ -1,1 +1,42 @@
+/*! \file jspipelinedelayedtask.h
+
+A simple pipeline delayed task for performingcompiled function calls
+*/
 #pragma once
+
+#include "../pipeline/pipelinedelayedtask.h"
+#include "../timer.h"
+#include "context.h"
+
+namespace sad
+{
+
+namespace dukpp03
+{
+
+	class JSPipelineDelayedTask : public sad::pipeline::DelayedTask
+	{
+	public:
+		/*! Makes new handler
+		\param[in] interval an interval for launch
+		\param[in] ctx context
+		\param[in] f function
+		*/
+		JSPipelineDelayedTask(double interval, sad::dukpp03::Context* ctx, sad::dukpp03::CompiledFunction f);
+		/*! Destroys a task
+		*/
+		virtual ~JSPipelineDelayedTask();
+
+	protected:
+		/*! A context
+		*/
+		sad::dukpp03::Context* m_ctx;
+		/*! A function
+		*/
+		sad::dukpp03::CompiledFunction m_function;
+
+	};
+
+}
+
+}
