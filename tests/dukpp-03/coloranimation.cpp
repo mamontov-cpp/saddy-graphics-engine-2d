58a33013ed7bac70f7239bf5973ce356615a8695
Shalievskaya <Shalievskaya@gmail.com>
2017-12-03 19:09:44 +0300

    Libraries connected to file coloranimation.cpp

---
+++ tests/dukpp-03/coloranimation.cpp
@@ -0,0 +1,10 @@
+#pragma warning(push)
+#pragma warning(disable: 4273)
+#pragma warning(disable: 4351)
+// ReSharper disable once CppUnusedIncludeDirective
+#include <cstdio>
+#include "dukpp-03/context.h"
+#include "sprite2d.h"
+#define _INC_STDIO
+#include "3rdparty/tpunit++/tpunit++.hpp"
+#pragma warning(pop)
