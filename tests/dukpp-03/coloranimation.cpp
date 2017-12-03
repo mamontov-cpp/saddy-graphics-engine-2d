bba7033f72a688b82f7c49ddb8b706a8da003dc3
Shalievskaya <Shalievskaya@gmail.com>
2017-12-03 19:14:35 +0300

    Added test for maxColor

--- tests/dukpp-03/coloranimation.cpp
+++ tests/dukpp-03/coloranimation.cpp
@@ -15,7 +15,8 @@ struct ColorAnimationTest : tpunit::TestFixture
 {
 public:
     ColorAnimationTest() : tpunit::TestFixture(
-        TEST(ColorAnimationTest::testMinColor)
+        TEST(ColorAnimationTest::testMinColor),
+        TEST(ColorAnimationTest::testMaxColor)
     ) {}
     
     /*! Test for getting and setting minColor property
@@ -26,6 +27,25 @@ public:
         std::string error;
         sad::dukpp03::Context ctx;
         bool eval_result = ctx.eval("var b = new sad.animations.Color(); b.setMinColor(new sad.AColor(12,12,12)); b.minColor()", false, &error);
+        if (!eval_result)
+        {
+            printf("%s\n", error.c_str());
+        }
+        ASSERT_TRUE(eval_result);
+        ASSERT_TRUE(error.size() == 0);
+        ::dukpp03::Maybe<sad::AColor> result = ::dukpp03::GetValue<sad::AColor, sad::dukpp03::BasicContext>::perform(&ctx, -1);
+        ASSERT_TRUE(result.exists());
+        ASSERT_TRUE(result.value() == sad::AColor(12, 12, 12));
+    }
+    
+    /*! Test for getting and setting maxColor property
+    */
+    // ReSharper disable once CppMemberFunctionMayBeStatic
+    // ReSharper disable once CppMemberFunctionMayBeConst
+    void testMaxColor() {
+        std::string error;
+        sad::dukpp03::Context ctx;
+        bool eval_result = ctx.eval("var b = new sad.animations.Color(); b.setMaxColor(new sad.AColor(12,12,12)); b.maxColor()", false, &error);
         if (!eval_result)
         {
             printf("%s\n", error.c_str());
