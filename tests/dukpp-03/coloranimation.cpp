0228e4ff4bd56fab50ea10b0c2e84d3e2f5e970c
Shalievskaya <Shalievskaya@gmail.com>
2017-12-03 19:11:27 +0300

    Added test for minColor

--- tests/dukpp-03/coloranimation.cpp
+++ tests/dukpp-03/coloranimation.cpp
@@ -8,3 +8,32 @@
 #define _INC_STDIO
 #include "3rdparty/tpunit++/tpunit++.hpp"
 #pragma warning(pop)
+
+/*! Tests for Color class
+*/
+struct ColorAnimationTest : tpunit::TestFixture
+{
+public:
+    ColorAnimationTest() : tpunit::TestFixture(
+        TEST(ColorAnimationTest::testMinColor)
+    ) {}
+    
+    /*! Test for getting and setting minColor property
+    */
+    // ReSharper disable once CppMemberFunctionMayBeStatic
+    // ReSharper disable once CppMemberFunctionMayBeConst
+    void testMinColor() {
+        std::string error;
+        sad::dukpp03::Context ctx;
+        bool eval_result = ctx.eval("var b = new sad.animations.Color(); b.setMinColor(new sad.AColor(12,12,12)); b.minColor()", false, &error);
+        if (!eval_result)
+        {
+            printf("%s\n", error.c_str());
+        }
+        ASSERT_TRUE(eval_result);
+        ASSERT_TRUE(error.size() == 0);
+        ::dukpp03::Maybe<sad::AColor> result = ::dukpp03::GetValue<sad::AColor, sad::dukpp03::BasicContext>::perform(&ctx, -1);
+        ASSERT_TRUE(result.exists());
+        ASSERT_TRUE(result.value() == sad::AColor(12, 12, 12));
+    }
+} _coloranimation_test;
