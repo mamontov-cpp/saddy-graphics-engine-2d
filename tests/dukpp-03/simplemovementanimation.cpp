a929db9210f48c353ab340de9b680c744971dc2b
Dmitriy Ostrovskiy <d.s.ostr@gmail.com>
2017-12-06 13:42:24 +0300

    Added test testSetTime

--- tests/dukpp-03/simplemovementanimation.cpp
+++ tests/dukpp-03/simplemovementanimation.cpp
@@ -18,7 +18,8 @@ public:
 	SimpleMovementAnimationTest() : tpunit::TestFixture(
 		TEST(SimpleMovementAnimationTest::testSetStartingPoint),
 		TEST(SimpleMovementAnimationTest::testSetEndingPoint),
-		TEST(SimpleMovementAnimationTest::testSetLooped)
+		TEST(SimpleMovementAnimationTest::testSetLooped),
+		TEST(SimpleMovementAnimationTest::testSetTime)
 	) {}
 
 	/*! Test for getting and setting startingPoint property
@@ -76,6 +77,25 @@ public:
 		::dukpp03::Maybe<bool> result = ::dukpp03::GetValue<bool, sad::dukpp03::BasicContext>::perform(&ctx, -1);
 		ASSERT_TRUE(result.exists());
 		ASSERT_TRUE(result.value() == false);
+	}
+
+	/*! Test for getting and setting time property
+	*/
+	// ReSharper disable once CppMemberFunctionMayBeStatic
+	// ReSharper disable once CppMemberFunctionMayBeConst
+	void testSetTime() {
+		std::string error;
+		sad::dukpp03::Context ctx;
+		bool eval_result = ctx.eval("var b = new sad.animations.SimpleMovement(); b.setTime(5.0); b.time()", false, &error);
+		if (!eval_result)
+		{
+			printf("%s\n", error.c_str());
+		}
+		ASSERT_TRUE(eval_result);
+		ASSERT_TRUE(error.size() == 0);
+		::dukpp03::Maybe<double> result = ::dukpp03::GetValue<double, sad::dukpp03::BasicContext>::perform(&ctx, -1);
+		ASSERT_TRUE(result.exists());
+		ASSERT_TRUE(result.value() == 5.0);
 	}
 
 } _simplemovementanimation_test;
