a0d39e5bf34df1a6a17fc52aab3dfe86d7d30ded
Dmitriy Ostrovskiy <d.s.ostr@gmail.com>
2017-12-06 13:43:51 +0300

    Added test testSetObjectName

--- tests/dukpp-03/simplemovementanimation.cpp
+++ tests/dukpp-03/simplemovementanimation.cpp
@@ -19,7 +19,8 @@ public:
 		TEST(SimpleMovementAnimationTest::testSetStartingPoint),
 		TEST(SimpleMovementAnimationTest::testSetEndingPoint),
 		TEST(SimpleMovementAnimationTest::testSetLooped),
-		TEST(SimpleMovementAnimationTest::testSetTime)
+		TEST(SimpleMovementAnimationTest::testSetTime),
+		TEST(SimpleMovementAnimationTest::testSetObjectName)
 	) {}
 
 	/*! Test for getting and setting startingPoint property
@@ -96,6 +97,26 @@ public:
 		::dukpp03::Maybe<double> result = ::dukpp03::GetValue<double, sad::dukpp03::BasicContext>::perform(&ctx, -1);
 		ASSERT_TRUE(result.exists());
 		ASSERT_TRUE(result.value() == 5.0);
+	}
+
+	/*! Test for getting and setting objectName property
+	*/
+	// ReSharper disable once CppMemberFunctionMayBeStatic
+	// ReSharper disable once CppMemberFunctionMayBeConst
+	void testSetObjectName()
+	{
+		std::string error;
+		sad::dukpp03::Context ctx;
+		bool eval_result = ctx.eval("var b = new sad.animations.SimpleMovement(); b.setObjectName(\"Name\"); b.objectName()", false, &error);
+		if (!eval_result)
+		{
+			printf("%s\n", error.c_str());
+		}
+		ASSERT_TRUE(eval_result);
+		ASSERT_TRUE(error.size() == 0);
+		::dukpp03::Maybe<sad::String> result = ::dukpp03::GetValue<sad::String, sad::dukpp03::BasicContext>::perform(&ctx, -1);
+		ASSERT_TRUE(result.exists());
+		ASSERT_TRUE(result.value() == "Name");
 	}
 
 } _simplemovementanimation_test;
