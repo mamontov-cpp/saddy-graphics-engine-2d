dffa5c46ed3ddb2fa1f5529c56e5f65090c45129
Dmitriy Ostrovskiy <d.s.ostr@gmail.com>
2017-12-06 13:40:02 +0300

    Added test testSetEndingPoint

--- tests/dukpp-03/simplemovementanimation.cpp
+++ tests/dukpp-03/simplemovementanimation.cpp
@@ -16,7 +16,8 @@ struct SimpleMovementAnimationTest : tpunit::TestFixture
 {
 public:
 	SimpleMovementAnimationTest() : tpunit::TestFixture(
-		TEST(SimpleMovementAnimationTest::testSetStartingPoint)
+		TEST(SimpleMovementAnimationTest::testSetStartingPoint),
+		TEST(SimpleMovementAnimationTest::testSetEndingPoint)
 	) {}
 
 	/*! Test for getting and setting startingPoint property
@@ -36,6 +37,25 @@ public:
 		::dukpp03::Maybe<sad::Point2D> result = ::dukpp03::GetValue<sad::Point2D, sad::dukpp03::BasicContext>::perform(&ctx, -1);
 		ASSERT_TRUE(result.exists());
 		ASSERT_TRUE(sad::equal(result.value(), sad::Point2D(1, 5)));
+	}
+
+	/*! Test for getting and setting endingPoint property
+	*/
+	// ReSharper disable once CppMemberFunctionMayBeStatic
+	// ReSharper disable once CppMemberFunctionMayBeConst
+	void testSetEndingPoint() {
+		std::string error;
+		sad::dukpp03::Context ctx;
+		bool eval_result = ctx.eval("var b = new sad.animations.SimpleMovement(); b.setEndingPoint(new sad.Point2D(2,8)); b.endingPoint()", false, &error);
+		if (!eval_result)
+		{
+			printf("%s\n", error.c_str());
+		}
+		ASSERT_TRUE(eval_result);
+		ASSERT_TRUE(error.size() == 0);
+		::dukpp03::Maybe<sad::Point2D> result = ::dukpp03::GetValue<sad::Point2D, sad::dukpp03::BasicContext>::perform(&ctx, -1);
+		ASSERT_TRUE(result.exists());
+		ASSERT_TRUE(sad::equal(result.value(), sad::Point2D(2, 8)));
 	}
 
 } _simplemovementanimation_test;
