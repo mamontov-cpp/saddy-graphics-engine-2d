ebf02abee129258e974ab4d45523e2fa5a37be90
Dmitriy Ostrovskiy <d.s.ostr@gmail.com>
2017-12-06 13:37:13 +0300

    Added test testSetStartingPoint

--- tests/dukpp-03/simplemovementanimation.cpp
+++ tests/dukpp-03/simplemovementanimation.cpp
@@ -10,20 +10,19 @@
 #include "3rdparty/tpunit++/tpunit++.hpp"
 #pragma warning(pop)
 
+/*! Tests for SimpleMovement class
+*/
 struct SimpleMovementAnimationTest : tpunit::TestFixture
 {
 public:
 	SimpleMovementAnimationTest() : tpunit::TestFixture(
-		TEST(SimpleMovementAnimationTest::testSetStartingPoint),
-		TEST(SimpleMovementAnimationTest::testSetEndingPoint),
-		TEST(SimpleMovementAnimationTest::testSetLooped),
-		TEST(SimpleMovementAnimationTest::testSetTime)/*,
-		TEST(SimpleMovementAnimationTest::testSetObjectName),
-		TEST(SimpleMovementAnimationTest::testObjectName),
-		TEST(SimpleMovementAnimationTest::testMajorId),
-		TEST(SimpleMovementAnimationTest::testMinorId)*/
+		TEST(SimpleMovementAnimationTest::testSetStartingPoint)
 	) {}
 
+	/*! Test for getting and setting startingPoint property
+	*/
+	// ReSharper disable once CppMemberFunctionMayBeStatic
+	// ReSharper disable once CppMemberFunctionMayBeConst
 	void testSetStartingPoint() {
 		std::string error;
 		sad::dukpp03::Context ctx;
@@ -36,67 +35,7 @@ public:
 		ASSERT_TRUE(error.size() == 0);
 		::dukpp03::Maybe<sad::Point2D> result = ::dukpp03::GetValue<sad::Point2D, sad::dukpp03::BasicContext>::perform(&ctx, -1);
 		ASSERT_TRUE(result.exists());
-		ASSERT_TRUE(sad::equal(result.value(), sad::Point2D(1,5)));
+		ASSERT_TRUE(sad::equal(result.value(), sad::Point2D(1, 5)));
 	}
-
-	void testSetEndingPoint() {
-		std::string error;
-		sad::dukpp03::Context ctx;
-		bool eval_result = ctx.eval("var b = new sad.animations.SimpleMovement(); b.setEndingPoint(new sad.Point2D(2,8)); b.endingPoint()", false, &error);
-		if (!eval_result)
-		{
-			printf("%s\n", error.c_str());
-		}
-		ASSERT_TRUE(eval_result);
-		ASSERT_TRUE(error.size() == 0);
-		::dukpp03::Maybe<sad::Point2D> result = ::dukpp03::GetValue<sad::Point2D, sad::dukpp03::BasicContext>::perform(&ctx, -1);
-		ASSERT_TRUE(result.exists());
-		ASSERT_TRUE(sad::equal(result.value(), sad::Point2D(2,8)));
-	}
-
-	void testSetLooped() {
-		std::string error;
-		sad::dukpp03::Context ctx;
-		bool eval_result = ctx.eval("var b = new sad.animations.SimpleMovement(); b.setLooped(false); b.looped()", false, &error);
-		if (!eval_result)
-		{
-			printf("%s\n", error.c_str());
-		}
-		ASSERT_TRUE(eval_result);
-		ASSERT_TRUE(error.size() == 0);
-		::dukpp03::Maybe<bool> result = ::dukpp03::GetValue<bool, sad::dukpp03::BasicContext>::perform(&ctx, -1);
-		ASSERT_TRUE(result.exists());
-		ASSERT_TRUE(result.value()==false);
-	}
-
-	void testSetTime() {
-		std::string error;
-		sad::dukpp03::Context ctx;
-		bool eval_result = ctx.eval("var b = new sad.animations.SimpleMovement(); b.setTime(5.0); b.time()", false, &error);
-		if (!eval_result)
-		{
-			printf("%s\n", error.c_str());
-		}
-		ASSERT_TRUE(eval_result);
-		ASSERT_TRUE(error.size() == 0);
-		::dukpp03::Maybe<double> result = ::dukpp03::GetValue<double, sad::dukpp03::BasicContext>::perform(&ctx, -1);
-		ASSERT_TRUE(result.exists());
-		ASSERT_TRUE(result.value() == 5.0);
-	}
-
-	/*void testSetObjectName() {
-		std::string error;
-		sad::dukpp03::Context ctx;
-		bool eval_result = ctx.eval("var b = new sad.animations.SimpleMovement(); b.setTime(5.0); b.time()", false, &error);
-		if (!eval_result)
-		{
-			printf("%s\n", error.c_str());
-		}
-		ASSERT_TRUE(eval_result);
-		ASSERT_TRUE(error.size() == 0);
-		::dukpp03::Maybe<double> result = ::dukpp03::GetValue<double, sad::dukpp03::BasicContext>::perform(&ctx, -1);
-		ASSERT_TRUE(result.exists());
-		ASSERT_TRUE(result.value() == 5.0);
-	}*/
 
 } _simplemovementanimation_test;
