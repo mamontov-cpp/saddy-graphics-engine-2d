51ab45a2a3bda84495aba4bc30c9f768883eb2b5
Shalievskaya <sh_nastya96@mail.ru>
2017-12-17 21:32:21 +0300

    Added test for minorId

--- tests/dukpp-03/animationanimation.cpp
+++ tests/dukpp-03/animationanimation.cpp
@@ -16,7 +16,8 @@ struct AnimationAnimationTest : tpunit::TestFixture
 public:
     AnimationAnimationTest() : tpunit::TestFixture(
         TEST(AnimationAnimationTest::testObjectName),
-        TEST(AnimationAnimationTest::testMajorId)
+        TEST(AnimationAnimationTest::testMajorId),
+        TEST(AnimationAnimationTest::testMinorId)
     ) {}
     
     /*! Test for getting and setting objectName property
@@ -57,5 +58,25 @@ public:
         ::dukpp03::Maybe<long> result = ::dukpp03::GetValue<long, sad::dukpp03::BasicContext>::perform(&ctx, -1);
         ASSERT_TRUE(result.exists());
         ASSERT_TRUE(result.value() == 14);
+    }
+    
+    /*! Test for getting and setting minorId property
+    */
+    // ReSharper disable once CppMemberFunctionMayBeStatic
+    // ReSharper disable once CppMemberFunctionMayBeConst
+    void testMinorId() {
+        std::string error;
+        sad::dukpp03::Context ctx;
+        ctx.registerGlobal("animationObject", static_cast <sad::animations::Animation*> (new sad::animations::Color()));
+        bool eval_result = ctx.eval("animationObject.MinorId=11; animationObject.MinorId", false, &error);
+        if (!eval_result)
+        {
+            printf("%s\n", error.c_str());
+        }
+        ASSERT_TRUE(eval_result);
+        ASSERT_TRUE(error.size() == 0);
+        ::dukpp03::Maybe<long> result = ::dukpp03::GetValue<long, sad::dukpp03::BasicContext>::perform(&ctx, -1);
+        ASSERT_TRUE(result.exists());
+        ASSERT_TRUE(result.value() == 11);
     }
 } _animationanimation_test;
