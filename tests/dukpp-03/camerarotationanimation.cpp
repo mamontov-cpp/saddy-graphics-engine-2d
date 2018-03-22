57496d16755d162a4ac3274b0ae43393d46c3527
iwannachaos <frederickbrown@yandex.ru>
2018-03-23 01:11:18 +0300

    Добавлен тест ObjectName

--- tests/dukpp-03/camerarotationanimation.cpp
+++ tests/dukpp-03/camerarotationanimation.cpp
@@ -120,4 +120,23 @@ public:
         ASSERT_TRUE(result.exists());
         ASSERT_TRUE(result.value() == 4.0);
     }
+
+     /*! Test for getting and setting objectName property
+    */
+    // ReSharper disable once CppMemberFunctionMayBeStatic
+    // ReSharper disable once CppMemberFunctionMayBeConst
+    void testObjectName() {
+        std::string error;
+        sad::dukpp03::Context ctx;
+        bool eval_result = ctx.eval("var b = new sad.animations.CameraRotation(); b.setObjectName(\"name\"); b.objectName()", false, &error);
+        if (!eval_result)
+        {
+            printf("%s\n", error.c_str());
+        }
+        ASSERT_TRUE(eval_result);
+        ASSERT_TRUE(error.size() == 0);
+        ::dukpp03::Maybe<sad::String> result = ::dukpp03::GetValue<sad::String, sad::dukpp03::BasicContext>::perform(&ctx, -1);
+        ASSERT_TRUE(result.exists());
+        ASSERT_TRUE(result.value() == "name");
+    }
     
