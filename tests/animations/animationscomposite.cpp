23299dc645e8cedc2552e9203b02a096ec8389cf
Иван <kirkeinos@gmail.com>
2018-03-23 16:01:03 +0300

    Added headers for test.

---
+++ tests/animations/animationscomposite.cpp
@@ -0,0 +1,15 @@
+#pragma warning(push)
+#pragma warning(disable: 4273)
+#pragma warning(disable: 4351)
+#include <cstdio>
+#define _INC_STDIO
+#include "3rdparty/tpunit++/tpunit++.hpp"
+#include <label.h>
+#include <animations/animationscomposite.h>
+#include <db/dbtable.h>
+#include <db/dbdatabase.h>
+#pragma warning(pop)
+
+/*! Tests for composite animation
+*/
+
