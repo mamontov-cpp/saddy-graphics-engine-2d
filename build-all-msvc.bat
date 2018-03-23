b9495b16c6c7e73a0380e1b28d0f6fc8212fbe47
Mamontov Dmitry <mamontov.dp@gmail.com>
2018-03-23 10:40:58 +0300

    [APPVEYOR] Another attempt to debug MSVC.

--- build-all-msvc.bat
+++ build-all-msvc.bat
@@ -22,42 +22,41 @@ if [%VALID%] EQU [0]  (
 	exit /B 1
 )
 @echo on
-devenv libsaddy.vcxproj /Build "%1|%2" /out lastsolutionbuild.log /Log errors.log
-type errors.log
-goto :error
-devenv tools/atlasgen/atlasgen.vcxproj /Build "%1|%2" /out lastsolutionbuild.log  || goto :error
-devenv plugins/dukpp-03/dukpp-03.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
-devenv plugins/freetype/saddy-ft.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
-devenv tools/exporter/exporter.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
-devenv plugins/irrklang/irrklang.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
-devenv plugins/dukpp-03-irrklang/dukpp-03-irrklang.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
-devenv plugins/imgui/imgui.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
-devenv tools/ifaceed/ifaceed/ifaceed.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
-devenv examples/game/game.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
-devenv examples/multithreading/multithreading.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
-devenv examples/stress-physics/stress-physics.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
-devenv examples/imgui/imgui.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
-devenv tests/animations/alltests.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
-devenv tests/db/alltests.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
-devenv tests/dukpp-03/alltests.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
-devenv tests/freetype/alltests.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
-devenv tests/hfsm/alltests.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
-devenv tests/input/alltests.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
-devenv tests/irrklang/alltests.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
-devenv tests/dukpp-03-irrklang/alltests.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
-devenv tests/p2d/alltests.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
-devenv tests/pipeline/alltests.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
-devenv tests/resource/alltests.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
-devenv tests/sad/alltests.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
-devenv tests/layouts/alltests.vcxproj /Build "%1|%2" /out lastsolutionbuild.log || goto :error
-devenv tools/isqt580/isqt580.vcxproj  /Build "%1|%2" /out lastsolutionbuild.log || goto :error
+devenv libsaddy.vcxproj /Build "%1|%2" /out lastsolutionbuild.log /Log errors.log   || goto :error
+devenv tools/atlasgen/atlasgen.vcxproj /Build "%1|%2" /out lastsolutionbuild.log /Log errors.log   || goto :error
+devenv plugins/dukpp-03/dukpp-03.vcxproj /Build "%1|%2" /out lastsolutionbuild.log /Log errors.log || goto :error
+devenv plugins/freetype/saddy-ft.vcxproj /Build "%1|%2" /out lastsolutionbuild.log /Log errors.log || goto :error
+devenv tools/exporter/exporter.vcxproj /Build "%1|%2" /out lastsolutionbuild.log /Log errors.log || goto :error
+devenv plugins/irrklang/irrklang.vcxproj /Build "%1|%2" /out lastsolutionbuild.log /Log errors.log || goto :error
+devenv plugins/dukpp-03-irrklang/dukpp-03-irrklang.vcxproj /Build "%1|%2" /out lastsolutionbuild.log /Log errors.log || goto :error
+devenv plugins/imgui/imgui.vcxproj /Build "%1|%2" /out lastsolutionbuild.log /Log errors.log || goto :error
+devenv tools/ifaceed/ifaceed/ifaceed.vcxproj /Build "%1|%2" /out lastsolutionbuild.log /Log errors.log || goto :error
+devenv examples/game/game.vcxproj /Build "%1|%2" /out lastsolutionbuild.log /Log errors.log || goto :error
+devenv examples/multithreading/multithreading.vcxproj /Build "%1|%2" /out lastsolutionbuild.log /Log errors.log || goto :error
+devenv examples/stress-physics/stress-physics.vcxproj /Build "%1|%2" /out lastsolutionbuild.log /Log errors.log || goto :error
+devenv examples/imgui/imgui.vcxproj /Build "%1|%2" /out lastsolutionbuild.log /Log errors.log || goto :error
+devenv tests/animations/alltests.vcxproj /Build "%1|%2" /out lastsolutionbuild.log /Log errors.log || goto :error
+devenv tests/db/alltests.vcxproj /Build "%1|%2" /out lastsolutionbuild.log /Log errors.log || goto :error
+devenv tests/dukpp-03/alltests.vcxproj /Build "%1|%2" /out lastsolutionbuild.log /Log errors.log || goto :error
+devenv tests/freetype/alltests.vcxproj /Build "%1|%2" /out lastsolutionbuild.log /Log errors.log || goto :error
+devenv tests/hfsm/alltests.vcxproj /Build "%1|%2" /out lastsolutionbuild.log /Log errors.log || goto :error
+devenv tests/input/alltests.vcxproj /Build "%1|%2" /out lastsolutionbuild.log /Log errors.log || goto :error
+devenv tests/irrklang/alltests.vcxproj /Build "%1|%2" /out lastsolutionbuild.log /Log errors.log || goto :error
+devenv tests/dukpp-03-irrklang/alltests.vcxproj /Build "%1|%2" /out lastsolutionbuild.log /Log errors.log || goto :error
+devenv tests/p2d/alltests.vcxproj /Build "%1|%2" /out lastsolutionbuild.log /Log errors.log || goto :error
+devenv tests/pipeline/alltests.vcxproj /Build "%1|%2" /out lastsolutionbuild.log /Log errors.log || goto :error
+devenv tests/resource/alltests.vcxproj /Build "%1|%2" /out lastsolutionbuild.log /Log errors.log || goto :error
+devenv tests/sad/alltests.vcxproj /Build "%1|%2" /out lastsolutionbuild.log /Log errors.log || goto :error
+devenv tests/layouts/alltests.vcxproj /Build "%1|%2" /out lastsolutionbuild.log /Log errors.log || goto :error
+devenv tools/isqt580/isqt580.vcxproj  /Build "%1|%2" /out lastsolutionbuild.log /Log errors.log || goto :error
 %CHECKQTVERTOOL%
 if errorlevel 1 (
-  devenv plugins/qt-widget/qt-widget.vcxproj  /Build "%1|%2" /out lastsolutionbuild.log
-  devenv examples/example-game-in-qt/example-game-in-qt.vcxproj  /Build "%1|%2" /out lastsolutionbuild.log
+  devenv plugins/qt-widget/qt-widget.vcxproj  /Build "%1|%2" /out lastsolutionbuild.log  /Log errors.log
+  devenv examples/example-game-in-qt/example-game-in-qt.vcxproj  /Build "%1|%2" /out lastsolutionbuild.log  /Log errors.log
 )
 goto :EOF
 
 :error
+type errors.log
 echo Failed with error #%errorlevel%.
 exit /b %errorlevel%
