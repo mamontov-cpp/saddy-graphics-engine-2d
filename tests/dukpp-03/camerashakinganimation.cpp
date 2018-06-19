942597cec0e7f4e9dffbb41637cb5c36107918a7
mamontov-cpp <mamontov.dp@gmail.com>
2018-06-19 20:41:22 +0300

    [TESTS-DUKPP-03] Fixed dukpp-03 tests being broken.

--- tests/dukpp-03/camerashakinganimation.cpp
+++ tests/dukpp-03/camerashakinganimation.cpp
@@ -15,17 +15,15 @@ struct CameraShakingTest : tpunit::TestFixture
 {
 public:
     CameraShakingTest() : tpunit::TestFixture(
-        TEST(CameraShakingTest::testMinFrequency),
-        TEST(CameraShakingTest::testMaxFrequency),
-		TEST(CameraShakingTest::testSetMinOffset),
-		TEST(CameraShakingTest::testSetMaxOffset)
+        TEST(CameraShakingTest::testFrequency),
+        TEST(CameraShakingTest::testOffset)
     ) {}
     
-    /*! Test for getting and setting minAngle property
+    /*! Test for getting and setting frequency property
      */
     // ReSharper disable once CppMemberFunctionMayBeStatic
     // ReSharper disable once CppMemberFunctionMayBeConst
-    void testMinFrequency() 
+    void testFrequency() 
     {
         std::string error;
         sad::dukpp03::Context ctx;
@@ -40,32 +38,15 @@ public:
         ASSERT_TRUE(result.exists());
         ASSERT_TRUE(result.value() == 1);
     }
-
-    /*! Test for getting and setting maxAngle property
-     */
+    /*! Test for getting and setting frequency property
+    */
     // ReSharper disable once CppMemberFunctionMayBeStatic
     // ReSharper disable once CppMemberFunctionMayBeConst
-    void testMaxFrequency() 
+    void testOffset()
     {
         std::string error;
         sad::dukpp03::Context ctx;
-        bool eval_result = ctx.eval("var b = new sad.animations.CameraShaking(); b.setFrequency(255); b.frequency()", false, &error);
-        if (!eval_result)
-        {
-            printf("%s\n", error.c_str());
-        }
-        ASSERT_TRUE(eval_result);
-        ASSERT_TRUE(error.size() == 0);
-        ::dukpp03::Maybe<int> result = ::dukpp03::GetValue<int, sad::dukpp03::BasicContext>::perform(&ctx, -1);
-        ASSERT_TRUE(result.exists());
-        ASSERT_TRUE(result.value() == 255);
-    }
-
-	void testSetMinOffset() 
-    {
-        std::string error;
-        sad::dukpp03::Context ctx;
-        bool eval_result = ctx.eval("var b = new sad.animations.CameraShaking(); b.setOffset(new sad.Point2D(1,1)); b.offset().getX()", false, &error);
+        bool eval_result = ctx.eval("var b = new sad.animations.CameraShaking(); b.setOffset(new sad.Point2D(1,1)); b.offset().x", false, &error);
         if (!eval_result)
         {
             printf("%s\n", error.c_str());
@@ -76,24 +57,7 @@ public:
         ASSERT_TRUE(result.exists());
         ASSERT_TRUE(result.value() == 1);
     }
-   
-   void testSetMaxOffset() 
-    {
-        std::string error;
-        sad::dukpp03::Context ctx;
-        bool eval_result = ctx.eval("var b = new sad.animations.CameraShaking(); b.setOffset(new sad.Point2D(255,255)); b.offset().getX()", false, &error);
-        if (!eval_result)
-        {
-            printf("%s\n", error.c_str());
-        }
-        ASSERT_TRUE(eval_result);
-        ASSERT_TRUE(error.size() == 0);
-        ::dukpp03::Maybe<double> result = ::dukpp03::GetValue<double, sad::dukpp03::BasicContext>::perform(&ctx, -1);
-        ASSERT_TRUE(result.exists());
-        ASSERT_TRUE(result.value() == 255);
-    }
- 
- 
+
    
  
-} _CameraShakinganimation_test;
+} _camera_shaking_test;
