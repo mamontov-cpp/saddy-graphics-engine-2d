31354fc16c22bb1e17e95a1ad80529f92a4a23ea
animandy <brother_andrey@mail.ru>
2018-06-19 15:31:07 +0300

    добавлены тесты на методы setOffset() и offset()

--- tests/dukpp-03/camerashakinganimation.cpp
+++ tests/dukpp-03/camerashakinganimation.cpp
@@ -17,7 +17,8 @@ public:
     CameraShakingTest() : tpunit::TestFixture(
         TEST(CameraShakingTest::testMinFrequency),
         TEST(CameraShakingTest::testMaxFrequency),
-         
+		TEST(CameraShakingTest::testSetMinOffset),
+		TEST(CameraShakingTest::testSetMaxOffset),
     ) {}
     
     /*! Test for getting and setting minAngle property
@@ -60,7 +61,37 @@ public:
         ASSERT_TRUE(result.value() == 255);
     }
 
+	void testSetMinOffset() 
+    {
+        std::string error;
+        sad::dukpp03::Context ctx;
+        bool eval_result = ctx.eval("var b = new sad.animations.CameraShaking(); b.setOffset(new sad.Point2D(1,1)); b.offset().getX()", false, &error);
+        if (!eval_result)
+        {
+            printf("%s\n", error.c_str());
+        }
+        ASSERT_TRUE(eval_result);
+        ASSERT_TRUE(error.size() == 0);
+        ::dukpp03::Maybe<double> result = ::dukpp03::GetValue<int, sad::dukpp03::BasicContext>::perform(&ctx, -1);
+        ASSERT_TRUE(result.exists());
+        ASSERT_TRUE(result.value() == 1);
+    }
    
+   void testSetMaxOffset() 
+    {
+        std::string error;
+        sad::dukpp03::Context ctx;
+        bool eval_result = ctx.eval("var b = new sad.animations.CameraShaking(); b.setOffset(new sad.Point2D(255,255)); b.offset().getX()", false, &error);
+        if (!eval_result)
+        {
+            printf("%s\n", error.c_str());
+        }
+        ASSERT_TRUE(eval_result);
+        ASSERT_TRUE(error.size() == 0);
+        ::dukpp03::Maybe<double> result = ::dukpp03::GetValue<int, sad::dukpp03::BasicContext>::perform(&ctx, -1);
+        ASSERT_TRUE(result.exists());
+        ASSERT_TRUE(result.value() == 255);
+    }
  
  
    
