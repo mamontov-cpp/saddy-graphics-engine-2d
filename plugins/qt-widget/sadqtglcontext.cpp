840eb08917f61a20affa4e74791e92998b5b8bfe
mamontov-cpp <mamontov.dp@gmail.com>
2020-01-15 23:27:41 +0400

    [CORE, QT] Fixed cursor position and displaying fonts in example.

--- plugins/qt-widget/sadqtglcontext.cpp
+++ plugins/qt-widget/sadqtglcontext.cpp
@@ -11,6 +11,9 @@
 #include <GL/gl.h>
 #include <GL/glu.h>
 
+#include "../../3rdparty/glm/glm/glm.hpp"
+#include "../../3rdparty/glm/glm/ext.hpp"
+
 
 sad::qt::GLContext::GLContext() : m_window(NULL), m_valid(false)
 {
@@ -48,27 +51,13 @@ sad::Point3D sad::qt::GLContext::mapToViewport(const sad::Point2D & p, bool ztes
     {
         return p;
     }
+
+    bool isOpenGL3 = this->isOpenGL3compatible();
     sad::qt::Renderer* r = static_cast<sad::qt::Renderer*>(m_window->renderer());
     GLint*     viewport = r->viewport();
-    GLdouble  modelview[16];
-    GLdouble  projection[16];
-    GLdouble  oldprojection[16];
 
     GLfloat winx = 0, winy = 0, winz = 0;
     GLdouble result[3];
-
-    glGetDoublev(GL_MODELVIEW_MATRIX, modelview);
-    glGetDoublev(GL_PROJECTION_MATRIX, projection);
-
-    bool hasCamera = false;
-    sad::Camera* cam = r->getDefaultCamera();
-    if (cam)
-    {
-        hasCamera = true;
-        cam->apply();
-        memcpy(oldprojection, projection, 16 * sizeof(GLdouble));
-        glGetDoublev(GL_PROJECTION_MATRIX, projection);
-    }
 
     winx = (float)(p.x());
     winy = (float)(p.y());
@@ -76,17 +65,63 @@ sad::Point3D sad::qt::GLContext::mapToViewport(const sad::Point2D & p, bool ztes
         glReadPixels((int)winx, (int)winy, 1, 1, GL_DEPTH_COMPONENT, GL_FLOAT, &winz);
     else
         winz = DEFAULT_DEPTH_VALUE;
-
-
-    gluUnProject(winx, winy, winz, modelview, projection, viewport, result, result + 1, result + 2);
-
-    result[0] -= this->renderer()->globalTranslationOffset().x();
-    result[1] -= this->renderer()->globalTranslationOffset().y();
-
-    if (hasCamera)
+    if (!isOpenGL3)
     {
-        glMatrixMode(GL_PROJECTION);
-        glLoadMatrixd(oldprojection);
+        GLdouble  modelview[16];
+        GLdouble  projection[16];
+        GLdouble  oldprojection[16];
+
+        glGetDoublev(GL_MODELVIEW_MATRIX, modelview);
+        glGetDoublev(GL_PROJECTION_MATRIX, projection);
+
+        bool hasCamera = false;
+        sad::Camera* cam = r->getDefaultCamera();
+        if (cam)
+        {
+            hasCamera = true;
+            cam->apply();
+            memcpy(oldprojection, projection, 16 * sizeof(GLdouble));
+            glGetDoublev(GL_PROJECTION_MATRIX, projection);
+        }
+
+        gluUnProject(winx, winy, winz, modelview, projection, viewport, result, result + 1, result + 2);
+
+        result[0] -= this->renderer()->globalTranslationOffset().x();
+        result[1] -= this->renderer()->globalTranslationOffset().y();
+
+        if (hasCamera)
+        {
+            glMatrixMode(GL_PROJECTION);
+            glLoadMatrixd(oldprojection);
+        }
+    }
+    else
+    {
+        glm::mat4x4 model, projection;
+        bool hasCamera = false;
+        sad::Camera* cam = r->getDefaultCamera();
+        if (cam)
+        {
+            hasCamera = true;
+            cam->forceRecomputeMatrices();
+            model = glm::make_mat4x4(cam->modelViewMatrix());
+            projection = glm::make_mat4x4(cam->projectionMatrix());
+        }
+        else
+        {
+            float id[16] = {
+                1, 0, 0, 0,
+                0, 1, 0, 0,
+                0, 0, 1, 0,
+                0, 0, 0, 1
+            };
+            model = glm::make_mat4x4(id);
+            projection = glm::make_mat4x4(id);
+        }
+        glm::vec3 tmp_vec = glm::unProject(glm::vec3(winx, winy, winz), model, projection, glm::make_vec4(viewport));
+        result[0] = tmp_vec[0];
+        result[1] = tmp_vec[1];
+        result[2] = tmp_vec[2];
     }
 
     return sad::Point3D(result[0], result[1], result[2]);
