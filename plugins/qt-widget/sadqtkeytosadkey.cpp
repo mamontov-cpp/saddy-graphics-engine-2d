deee08383ca2504518bd940f53a3199b13d9ebb1
mamontov-cpp <mamontov.dp@gmail.com>
2021-08-01 14:51:42 +0300

    [PLUGINS-QT-WIDGET] Fixed build.

--- plugins/qt-widget/sadqtkeytosadkey.cpp
+++ plugins/qt-widget/sadqtkeytosadkey.cpp
@@ -11,131 +11,131 @@
 #endif
 
 int table_100000_1000026[][2] = {
-    { Qt::Key_Escape, sad::Esc },          //0
-    { Qt::Key_Tab, sad::Tab },             //1
-    { Qt::Key_Backtab, sad::KeyNone },     //2
-    { Qt::Key_Backspace, sad::Backspace }, //3
-    { Qt::Key_Return, sad::Enter },        //4
-    { Qt::Key_Enter, sad::Enter },         //5
-    { Qt::Key_Insert, sad::Insert },       //6
-    { Qt::Key_Delete, sad::Delete },       //7
-    { Qt::Key_Pause, sad::Pause },         //8
-    { Qt::Key_Print, sad::KeyNone},        //9
-    { Qt::Key_SysReq, sad::PrintScreen },  //10
-    { Qt::Key_Clear, sad::KeyNone },       //11
-    { 0x0100000c, sad::KeyNone},
-    { 0x0100000d, sad::KeyNone },
-    { 0x0100000e, sad::KeyNone },
-    { 0x0100000f, sad::KeyNone },
-    { Qt::Key_Home, sad::Home },           //12
-    { Qt::Key_End, sad::End },             //13
-    { Qt::Key_Left, sad::KeyLeft },        //14
-    { Qt::Key_Up, sad::KeyUp },            //15
-    { Qt::Key_Right, sad::KeyRight },      //16
-    { Qt::Key_Down, sad::KeyDown },        //17
-    { Qt::Key_PageUp, sad::PageUp },       //18
-    { Qt::Key_PageDown, sad::PageDown },   //19
-    { 0x01000018, sad::KeyNone },
-    { 0x01000019, sad::KeyNone },
-    { 0x0100001a, sad::KeyNone },
-    { 0x0100001b, sad::KeyNone },
-    { 0x0100001c, sad::KeyNone },
-    { 0x0100001d, sad::KeyNone },
-    { 0x0100001e, sad::KeyNone },
-    { 0x0100001f, sad::KeyNone },
-    { Qt::Key_Shift, sad::LeftShift },     //20
-    { Qt::Key_Control, sad::LeftControl }, //21
-    { Qt::Key_Meta, sad::WindowsKey },     //22
-    { Qt::Key_Alt, sad::LeftAlt },         //23
-    { Qt::Key_CapsLock, sad::CapsLock },   //24
-    { Qt::Key_NumLock, sad::NumLock },     //25
-    { Qt::Key_ScrollLock, sad::ScrollLock }//26
+    { Qt::Key_Escape, static_cast<int>(sad::KeyboardKey::Esc) },          //0
+    { Qt::Key_Tab, static_cast<int>(sad::KeyboardKey::Tab) },             //1
+    { Qt::Key_Backtab, static_cast<int>(sad::KeyboardKey::KeyNone) },     //2
+    { Qt::Key_Backspace, static_cast<int>(sad::KeyboardKey::Backspace) }, //3
+    { Qt::Key_Return, static_cast<int>(sad::KeyboardKey::Enter) },        //4
+    { Qt::Key_Enter, static_cast<int>(sad::KeyboardKey::Enter) },         //5
+    { Qt::Key_Insert, static_cast<int>(sad::KeyboardKey::Insert) },       //6
+    { Qt::Key_Delete, static_cast<int>(sad::KeyboardKey::Delete) },       //7
+    { Qt::Key_Pause, static_cast<int>(sad::KeyboardKey::Pause) },         //8
+    { Qt::Key_Print, static_cast<int>(sad::KeyboardKey::KeyNone)},        //9
+    { Qt::Key_SysReq, static_cast<int>(sad::KeyboardKey::PrintScreen) },  //10
+    { Qt::Key_Clear, static_cast<int>(sad::KeyboardKey::KeyNone) },       //11
+    { 0x0100000c, static_cast<int>(sad::KeyboardKey::KeyNone)},
+    { 0x0100000d, static_cast<int>(sad::KeyboardKey::KeyNone) },
+    { 0x0100000e, static_cast<int>(sad::KeyboardKey::KeyNone) },
+    { 0x0100000f, static_cast<int>(sad::KeyboardKey::KeyNone) },
+    { Qt::Key_Home, static_cast<int>(sad::KeyboardKey::Home) },           //12
+    { Qt::Key_End, static_cast<int>(sad::KeyboardKey::End) },             //13
+    { Qt::Key_Left, static_cast<int>(sad::KeyboardKey::KeyLeft) },        //14
+    { Qt::Key_Up, static_cast<int>(sad::KeyboardKey::KeyUp) },            //15
+    { Qt::Key_Right, static_cast<int>(sad::KeyboardKey::KeyRight) },      //16
+    { Qt::Key_Down, static_cast<int>(sad::KeyboardKey::KeyDown) },        //17
+    { Qt::Key_PageUp, static_cast<int>(sad::KeyboardKey::PageUp) },       //18
+    { Qt::Key_PageDown, static_cast<int>(sad::KeyboardKey::PageDown) },   //19
+    { 0x01000018, static_cast<int>(sad::KeyboardKey::KeyNone) },
+    { 0x01000019, static_cast<int>(sad::KeyboardKey::KeyNone) },
+    { 0x0100001a, static_cast<int>(sad::KeyboardKey::KeyNone) },
+    { 0x0100001b, static_cast<int>(sad::KeyboardKey::KeyNone) },
+    { 0x0100001c, static_cast<int>(sad::KeyboardKey::KeyNone) },
+    { 0x0100001d, static_cast<int>(sad::KeyboardKey::KeyNone) },
+    { 0x0100001e, static_cast<int>(sad::KeyboardKey::KeyNone) },
+    { 0x0100001f, static_cast<int>(sad::KeyboardKey::KeyNone) },
+    { Qt::Key_Shift, static_cast<int>(sad::KeyboardKey::LeftShift) },     //20
+    { Qt::Key_Control, static_cast<int>(sad::KeyboardKey::LeftControl) }, //21
+    { Qt::Key_Meta, static_cast<int>(sad::KeyboardKey::WindowsKey) },     //22
+    { Qt::Key_Alt, static_cast<int>(sad::KeyboardKey::LeftAlt) },         //23
+    { Qt::Key_CapsLock, static_cast<int>(sad::KeyboardKey::CapsLock) },   //24
+    { Qt::Key_NumLock, static_cast<int>(sad::KeyboardKey::NumLock) },     //25
+    { Qt::Key_ScrollLock, static_cast<int>(sad::KeyboardKey::ScrollLock) }//26
 };
 
 int table_100030_100003b[][2] = {
-    { Qt::Key_F1, sad::F1 },
-    { Qt::Key_F2, sad::F2 },
-    { Qt::Key_F3, sad::F3 },
-    { Qt::Key_F4, sad::F4 },
-    { Qt::Key_F5, sad::F5 },
-    { Qt::Key_F6, sad::F6 },
-    { Qt::Key_F7, sad::F7 },
-    { Qt::Key_F8, sad::F8 },
-    { Qt::Key_F9, sad::F9 },
-    { Qt::Key_F10, sad::F10 },
-    { Qt::Key_F11, sad::F11 },
-    { Qt::Key_F12, sad::F12 }
+    { Qt::Key_F1, static_cast<int>(sad::KeyboardKey::F1) },
+    { Qt::Key_F2, static_cast<int>(sad::KeyboardKey::F2) },
+    { Qt::Key_F3, static_cast<int>(sad::KeyboardKey::F3) },
+    { Qt::Key_F4, static_cast<int>(sad::KeyboardKey::F4) },
+    { Qt::Key_F5, static_cast<int>(sad::KeyboardKey::F5) },
+    { Qt::Key_F6, static_cast<int>(sad::KeyboardKey::F6) },
+    { Qt::Key_F7, static_cast<int>(sad::KeyboardKey::F7) },
+    { Qt::Key_F8, static_cast<int>(sad::KeyboardKey::F8) },
+    { Qt::Key_F9, static_cast<int>(sad::KeyboardKey::F9) },
+    { Qt::Key_F10, static_cast<int>(sad::KeyboardKey::F10) },
+    { Qt::Key_F11, static_cast<int>(sad::KeyboardKey::F11) },
+    { Qt::Key_F12, static_cast<int>(sad::KeyboardKey::F12) }
 };
 
 
 const int table_common_size = 11;
 int table_common[table_common_size][2] = {
-    { Qt::Key_BracketLeft, sad::OpeningSquareBracket },  //0
-    { Qt::Key_BracketRight, sad::ClosingSquareBracket }, //1
-    { Qt::Key_QuoteLeft, sad::Tilde },                   //2
-    { Qt::Key_Minus, sad::Minus },                       //3
-    { Qt::Key_Plus, sad::Equal },                        //4
-    { Qt::Key_Slash, sad::Slash },                       //5
-    { Qt::Key_Backslash, sad::BackSlash },               //6
-    { Qt::Key_Period, sad::Period },                     //7
-    { Qt::Key_Semicolon, sad::Semicolon },               //8
-    { Qt::Key_Apostrophe, sad::Apostrophe },             //9
-    { Qt::Key_Comma, sad::Comma }                        //10
+    { Qt::Key_BracketLeft, static_cast<int>(sad::KeyboardKey::OpeningSquareBracket) },  //0
+    { Qt::Key_BracketRight, static_cast<int>(sad::KeyboardKey::ClosingSquareBracket) }, //1
+    { Qt::Key_QuoteLeft, static_cast<int>(sad::KeyboardKey::Tilde) },                   //2
+    { Qt::Key_Minus, static_cast<int>(sad::KeyboardKey::Minus) },                       //3
+    { Qt::Key_Plus, static_cast<int>(sad::KeyboardKey::Equal) },                        //4
+    { Qt::Key_Slash, static_cast<int>(sad::KeyboardKey::Slash) },                       //5
+    { Qt::Key_Backslash, static_cast<int>(sad::KeyboardKey::BackSlash) },               //6
+    { Qt::Key_Period, static_cast<int>(sad::KeyboardKey::Period) },                     //7
+    { Qt::Key_Semicolon, static_cast<int>(sad::KeyboardKey::Semicolon) },               //8
+    { Qt::Key_Apostrophe, static_cast<int>(sad::KeyboardKey::Apostrophe) },             //9
+    { Qt::Key_Comma, static_cast<int>(sad::KeyboardKey::Comma) }                        //10
 };
 
 sad::KeyboardKey sad::qt::qtKeyToSadKey(int key, quint32 nativeScanCode)
 {
     if (key == 0 || key == Qt::Key_unknown)
     {
-        return sad::KeyNone;
+        return sad::KeyboardKey::KeyNone;
     }
 
     if (key == Qt::Key_AltGr)
     {
-        return sad::RightAlt;
+        return sad::KeyboardKey::RightAlt;
     }
 
     if (key  >= Qt::Key_0 && key <= Qt::Key_9)
     {
-        return static_cast<sad::KeyboardKey>(static_cast<int>(key) - static_cast<int>(Qt::Key_0) + static_cast<int>(sad::Numeric0));
+        return static_cast<sad::KeyboardKey>(static_cast<int>(key) - static_cast<int>(Qt::Key_0) + static_cast<int>(sad::KeyboardKey::Numeric0));
     }
 
     if (key >= Qt::Key_A && key <= Qt::Key_Z)
     {
-        return static_cast<sad::KeyboardKey>(static_cast<int>(key) - static_cast<int>(Qt::Key_A) + static_cast<int>(sad::A));
+        return static_cast<sad::KeyboardKey>(static_cast<int>(key) - static_cast<int>(Qt::Key_A) + static_cast<int>(sad::KeyboardKey::A));
     }
 
 
 #ifdef WIN32
     if (nativeScanCode == VK_LCONTROL)
     {
-        return sad::LeftControl;
+        return sad::KeyboardKey::LeftControl;
     }
 
     if (nativeScanCode == VK_RCONTROL)
     {
-        return sad::RightControl;
+        return sad::KeyboardKey::RightControl;
     }
     
     if (nativeScanCode == VK_LSHIFT)
     {
-        return sad::LeftShift;
+        return sad::KeyboardKey::LeftShift;
     }
 
     if (nativeScanCode == VK_RSHIFT)
     {
-        return sad::RightShift;
+        return sad::KeyboardKey::RightShift;
     }
 
 
     if (nativeScanCode == VK_LMENU)
     {
-        return sad::LeftAlt;
+        return sad::KeyboardKey::LeftAlt;
     }
 
     if (nativeScanCode == VK_RMENU)
     {
-        return sad::RightAlt;
+        return sad::KeyboardKey::RightAlt;
     }
 #endif
 
@@ -190,17 +190,17 @@ sad::KeyboardKey sad::qt::qtKeyToSadKey(int key, quint32 nativeScanCode)
 
     if (key == Qt::Key_Super_L || key == Qt::Key_Super_R)
     {
-        return sad::WindowsKey;
+        return sad::KeyboardKey::WindowsKey;
     }
 
     if (key == Qt::Key_Menu)
     {
-        return sad::AppKey;
+        return sad::KeyboardKey::AppKey;
     }
 
     if (key == Qt::Key_Space)
     {
-        return sad::Space;
+        return sad::KeyboardKey::Space;
     }
 
     for(int i = 0; i < table_common_size; i++)
@@ -211,7 +211,7 @@ sad::KeyboardKey sad::qt::qtKeyToSadKey(int key, quint32 nativeScanCode)
         }
     }
 
-    return sad::KeyNone;
+    return sad::KeyboardKey::KeyNone;
 }
 
 
